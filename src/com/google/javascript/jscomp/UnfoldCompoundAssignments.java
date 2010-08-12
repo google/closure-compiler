f02faf9d0650270eeb430d368240401d0f465597
elnatan@google.com <elnatan@google.com@b0f006be-c8cd-11de-a2e8-8d36a3108c74>
2010-08-12 22:43:47 +0000

    Augment instrumentation to handle increment, decrement, and compound assignment operators by unfolding them to simple assignments
    R=johnlenz
    DELTA=314  (296 added, 0 deleted, 18 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=104005
    
    
    git-svn-id: https://closure-compiler.googlecode.com/svn/trunk@356 b0f006be-c8cd-11de-a2e8-8d36a3108c74

---
+++ src/com/google/javascript/jscomp/UnfoldCompoundAssignments.java
@@ -0,0 +1,172 @@
+/*
+ * Copyright 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.javascript.jscomp;
+
+import com.google.common.base.Preconditions;
+import com.google.javascript.jscomp.NodeTraversal.Callback;
+import com.google.javascript.rhino.Node;
+import com.google.javascript.rhino.Token;
+
+/**
+ * Transforms compound assignments into simple ones.
+ * <p>
+ * {@code x++} and {@code x--} are unfolded to {@code x = +x + 1} and {@code x =
+ * x - 1}, and assignments such as {@code x += y} are expanded to {@code x = x +
+ * y}.
+ * <p>
+ * Notice the prefix '+' when unfolding ++. This is needed because the operand
+ * is implicitly converted to a number.
+ *
+ * @author elnatan@google.com (Elnatan Reisner)
+ */
+class UnfoldCompoundAssignments implements Callback, CompilerPass {
+  private final AbstractCompiler compiler;
+
+  public UnfoldCompoundAssignments(AbstractCompiler compiler) {
+    this.compiler = compiler;
+  }
+
+  /* (non-Javadoc)
+   * @see Callback#shouldTraverse(NodeTraversal, Node, Node)
+   */
+  @Override
+  public boolean shouldTraverse(NodeTraversal nodeTraversal, Node n,
+      Node parent) {
+    return true;
+  }
+
+  /* (non-Javadoc)
+   * @see Callback#visit(NodeTraversal, Node, Node)
+   */
+  @Override
+  public void visit(NodeTraversal t, Node node, Node parent) {
+    switch (node.getType()) {
+      case Token.INC:
+      case Token.DEC:
+        unfoldIncrDecr(node, node.getType() == Token.INC);
+        break;
+      case Token.ASSIGN_BITOR:  // |=
+      case Token.ASSIGN_BITXOR: // ^=
+      case Token.ASSIGN_BITAND: // &=
+      case Token.ASSIGN_LSH:    // <<=
+      case Token.ASSIGN_RSH:    // >>=
+      case Token.ASSIGN_URSH:   // >>>=
+      case Token.ASSIGN_ADD:    // +=
+      case Token.ASSIGN_SUB:    // -=
+      case Token.ASSIGN_MUL:    // *=
+      case Token.ASSIGN_DIV:    // /=
+      case Token.ASSIGN_MOD:    // %=
+        unfoldCompoundAssignment(node);
+        break;
+    }
+  }
+
+  /**
+   * Unfolds ++ and -- operators into {@code x = +x + 1} and {@code x = x - 1}.
+   * <p>
+   * The operand gets a prefix {@code +} when unfolding an increment because
+   * {@code ++} converts its operand to a number but binary {@code +} does not.
+   * ({@code -} <em>does</em> convert its operands to numbers, so we don't need
+   * to add a prefix {@code +} when unfolding {@code --}.)
+   *
+   * @param node an increment or decrement node
+   * @param isIncrement true if the operator is ++; false if it is --
+   * @throws RuntimeException if node is a postfix expression whose value is
+   *         used (that is, node is <em>not</em> used only for its side-effect)
+   */
+  private void unfoldIncrDecr(Node node, boolean isIncrement) {
+    Preconditions.checkArgument(
+        isPrefix(node) || valueIsDiscarded(node),
+        "Unfolding postfix ++/-- requires that the result be ignored.");
+    Node lhs = node.getFirstChild().cloneTree();
+    // TODO(elnatan): We might want to use type information to only add this '+'
+    // when lhs isn't already a number.
+    if (isIncrement) {
+      lhs = new Node(Token.POS, lhs);
+    }
+    node.setType(Token.ASSIGN);
+    Node rhs = new Node(isIncrement ? Token.ADD : Token.SUB,
+        lhs, Node.newNumber(1));
+    rhs.copyInformationFromForTree(node);
+    node.addChildToBack(rhs);
+    compiler.reportCodeChange();
+  }
+
+  /**
+   * Returns true if the node's value is discarded.
+   * <p>
+   * The value is discarded if node is
+   * <ul>
+   * <li>the child of an EXPR_RESULT,
+   * <li>the first child of a COMMA, or
+   * <li>the increment of a FOR loop.
+   * </ul>
+   */
+  private boolean valueIsDiscarded(Node node) {
+    Node parent = node.getParent();
+    switch (parent.getType()) {
+      case Token.EXPR_RESULT:
+        return true;
+      case Token.COMMA:
+        return parent.getFirstChild() == node;
+      case Token.FOR:
+        Preconditions.checkArgument(!NodeUtil.isForIn(parent),
+            "Error: the child of a FOR-IN cannot be an INC or DEC");
+        return parent.getChildAtIndex(2) == node;
+    }
+    return false;
+  }
+
+  /**
+   * @param node an INC or DEC node
+   * @return true if the increment/decrement is prefix; false if postfix
+   */
+  private boolean isPrefix(Node node) {
+    Preconditions.checkArgument(
+        node.getType() == Token.INC || node.getType() == Token.DEC,
+        "isPrefix can only be called on INC and DEC nodes");
+    // According to CodeGenerator:
+    // A non-zero post-prop value indicates a post inc/dec, default of zero is a
+    // pre-inc/dec.
+    return node.getIntProp(Node.INCRDECR_PROP) == 0;
+  }
+
+  /**
+   * Unfolds a compound assignment node {@code lhs op= rhs} to {@code lhs = lhs
+   * op rhs}.
+   *
+   * @param node a compound assignment node
+   */
+  private void unfoldCompoundAssignment(Node node) {
+    Node newRhs = node.cloneTree();
+    newRhs.setType(NodeUtil.getOpFromAssignmentOp(node));
+    Node lhs = node.getFirstChild();
+    node.replaceChildAfter(lhs, newRhs);
+    node.setType(Token.ASSIGN);
+    compiler.reportCodeChange();
+  }
+
+  /* (non-Javadoc)
+   * @see CompilerPass#process(Node, Node)
+   */
+  @Override
+  public void process(Node externs, Node root) {
+    Preconditions.checkState(compiler.isNormalized(),
+        "UnfoldCompoundAssignments requires a normalized AST");
+    NodeTraversal.traverse(compiler, root, this);
+  }
+}
---
+++ test/com/google/javascript/jscomp/UnfoldCompoundAssignmentsTest.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2010 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.javascript.jscomp;
+
+/**
+ * @author elnatan@google.com (Elnatan Reisner)
+ *
+ */
+public class UnfoldCompoundAssignmentsTest extends CompilerTestCase {
+  public UnfoldCompoundAssignmentsTest() {
+    enableNormalize();
+  }
+
+  /* (non-Javadoc)
+   * @see CompilerTestCase#getProcessor(Compiler)
+   */
+  @Override
+  protected CompilerPass getProcessor(Compiler compiler) {
+    return new UnfoldCompoundAssignments(compiler);
+  }
+
+  public void testIncrement() {
+    test("x++;", "x = +x + 1;");
+    test("var x = 0; ++x;", "var x = 0; x = +x + 1;");
+  }
+
+  public void testDecrement() {
+    test("x--;", "x = x - 1;");
+    test("var x = 0; --x;", "var x = 0; x = x - 1;");
+  }
+
+  public void testCompoundAssignment() {
+    test("x <<= y;", "x = x << y;");
+  }
+
+  public void testPostfixInForLoop0() {
+    test("for (x++;;) {}", "for (x = +x + 1;;) {}");
+  }
+
+  public void testPostfixInForLoop1() {
+    try {
+      testSame("for (;x++;) {}");
+      fail("Should raise an exception");
+    } catch (RuntimeException e) {
+    }
+  }
+
+  public void testPostfixInForLoop2() {
+    test("for (;;x++) {}", "for (;;x = +x + 1) {}");
+  }
+
+  public void testPrefixWithinLargerExpression() {
+    test("--x + 7;", "(x = x - 1) + 7;");
+  }
+
+  public void testPostfixInComma() {
+    test("z++, z==8;", "z = +z + 1, z==8;");
+  }
+
+  public void testPostfixUsedValue0() {
+    try {
+      testSame("z==8, z++;");
+      fail("Should raise an exception");
+    } catch (RuntimeException e) {
+    }
+  }
+
+  public void testPostfixUsedValue1() {
+    try {
+      testSame("x-- + 7;");
+      fail("Should raise an Exception");
+    } catch (RuntimeException e) {
+    }
+  }
+
+  public void testMultiple() {
+    test("x++, 5; for (a.x++;0;x++) {}; x++;",
+        "x = +x + 1, 5; for (a.x = +a.x + 1; 0; x = +x + 1) {}; x = +x + 1;");
+  }
+}
