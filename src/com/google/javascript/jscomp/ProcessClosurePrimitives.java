9d1e18e87a41f2a610ed70f3a1d57abbce7a1cd7
mbolin <mbolin@b0f006be-c8cd-11de-a2e8-8d36a3108c74>
2010-10-13 22:48:59 +0000

    Fixed bug 261: http://code.google.com/p/closure-compiler/issues/detail?id=261 Made JUnit test failure in Ant result in Ant failure (previously Ant printed BUILD SUCCESSFUL even when a test failed, thereby masking the failure).
    git-svn-id: https://closure-compiler.googlecode.com/svn/trunk@485 b0f006be-c8cd-11de-a2e8-8d36a3108c74

--- src/com/google/javascript/jscomp/ProcessClosurePrimitives.java
+++ src/com/google/javascript/jscomp/ProcessClosurePrimitives.java
@@ -786,7 +786,7 @@ class ProcessClosurePrimitives extends AbstractPostOrderCallback
         } else {
           // In this case, the name was implicitly provided by two independent
           // modules. We need to move this code up to a common module.
-          int indexOfDot = namespace.indexOf('.');
+          int indexOfDot = namespace.lastIndexOf('.');
           if (indexOfDot == -1) {
             // Any old place is fine.
             compiler.getNodeForCodeInsertion(minimumModule)
--- test/com/google/javascript/jscomp/ProcessClosurePrimitivesTest.java
+++ test/com/google/javascript/jscomp/ProcessClosurePrimitivesTest.java
@@ -771,6 +771,21 @@ public class ProcessClosurePrimitivesTest extends CompilerTestCase {
         });
   }
 
+  public void testProvideInIndependentModules4() {
+    // Regression test for bug 261:
+    // http://code.google.com/p/closure-compiler/issues/detail?id=261
+    test(
+        createModuleStar(
+            "goog.provide('apps');",
+            "goog.provide('apps.foo.bar.B');",
+            "goog.provide('apps.foo.bar.C');"),
+        new String[] {
+            "var apps = {};apps.foo = {};apps.foo.bar = {}",
+            "apps.foo.bar.B = {};",
+            "apps.foo.bar.C = {};",
+        });
+  }
+
   public void testRequireOfBaseGoog() {
     test("goog.require('goog');",
          "", MISSING_PROVIDE_ERROR);
