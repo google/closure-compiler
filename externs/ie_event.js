834078395f9dd98e127f4c60fd8ca136e0c4dc57
nicksantos@google.com <nicksantos@google.com@b0f006be-c8cd-11de-a2e8-8d36a3108c74>
2010-12-10 22:41:19 +0000

    Automated g4 rollback.
    *** Reason for rollback ***
    
    breaks maps build
    
    *** Original change description ***
    
    Add type information to ie_event.js externs
    Import of r613
    Contributed by Chad Killingsworth
    
    Revision created by MOE tool push_codebase.
    
    DELTA=85  (1 added, 78 deleted, 6 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=120
    
    
    git-svn-id: https://closure-compiler.googlecode.com/svn/trunk@622 b0f006be-c8cd-11de-a2e8-8d36a3108c74

--- externs/ie_event.js
+++ externs/ie_event.js
@@ -13,138 +13,61 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 /**
  * @fileoverview Definitions for all the extensions over the
  *  W3C's event specification by IE in JScript. This file depends on
- *  w3c_event.js.
+ *  w3c_event.js. The whole file has NOT been fully type annotated.
  *
- * @see http://msdn.microsoft.com/en-us/library/ms535863.aspx
  * @externs
  */
 
-/** @type {string} */
 Event.prototype.Abstract;
-
-/** @type {boolean} */
 Event.prototype.altKey;
-
-/** @type {boolean} */
 Event.prototype.altLeft;
-
-/** @type {string} */
 Event.prototype.Banner;
-
-/** @type {number} */
 Event.prototype.button;
-
-/** @type {boolean} */
 Event.prototype.cancelBubble;
-
-/** @type {number} */
 Event.prototype.clientX;
-
-/** @type {number} */
 Event.prototype.clientY;
-
 /**
- * @see http://msdn.microsoft.com/en-us/library/ms535220.aspx
- * @type {(ClipboardData|undefined)}
+ * @see http://msdn.microsoft.com/en-us/library/ms535220(VS.85).aspx
+ * @type {ClipboardData|undefined}
  */
 Event.prototype.clipboardData;
-
-/** @type {boolean} */
 Event.prototype.contentOverflow;
-
-/** @type {boolean} */
 Event.prototype.ctrlKey;
-
-/** @type {boolean} */
 Event.prototype.ctrlLeft;
-
-/** @type {string} */
 Event.prototype.data;
-
-/** @type {string} */
 Event.prototype.dataFld;
-
 Event.prototype.domain;
-
-/** @type {Element} */
 Event.prototype.fromElement;
-
-/** @type {number} */
 Event.prototype.keyCode;
-
-/** @type {string} */
 Event.prototype.MoreInfo;
-
-/** @type {string} */
 Event.prototype.nextPage;
-
-/** @type {number} */
 Event.prototype.offsetX;
-
-/** @type {number} */
 Event.prototype.offsetY;
-
-/** @type {string} */
 Event.prototype.propertyName;
-
-/** @type {string} */
 Event.prototype.qualifier;
-
-/** @type {number} */
 Event.prototype.reason;
-
-/** @type {Object.<*,*>} */
 Event.prototype.recordset;
-
-/** @type {boolean} */
 Event.prototype.repeat;
-
-/** @type {(boolean|string|undefined)} */
+/**
+ * @type {boolean|string|undefined}
+ */
 Event.prototype.returnValue;
-
-/** @type {string} */
 Event.prototype.saveType;
-
 Event.prototype.scheme;
-
-/** @type {number} */
 Event.prototype.screenX;
-
-/** @type {number} */
 Event.prototype.screenY;
-
-/** @type {boolean} */
 Event.prototype.shiftKey;
-
-/** @type {boolean} */
 Event.prototype.shiftLeft;
-
-/** @type {Window} */
 Event.prototype.source;
-
-/** @type {Element} */
 Event.prototype.srcElement;
-
 Event.prototype.srcFilter;
-
-/** @type {string} */
 Event.prototype.srcUrn;
-
-/** @type {Element} */
 Event.prototype.toElement;
-
+Event.prototype.type;
 Event.prototype.userName;
-
-/** @type {number} */
 Event.prototype.wheelDelta;
-
-/** @type {number} */
 Event.prototype.x;
-
-/** @type {number} */
 Event.prototype.y;
-
