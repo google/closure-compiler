a68c23091f93552148a26f45318f93dd3a17321e
lukem@google.com <lukem@google.com@b0f006be-c8cd-11de-a2e8-8d36a3108c74>
2011-03-02 04:17:13 +0000

    Adding 3.4 externs
    R=cbro
    DELTA=3868  (3867 added, 0 deleted, 1 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=718
    
    
    git-svn-id: https://closure-compiler.googlecode.com/svn/trunk@841 b0f006be-c8cd-11de-a2e8-8d36a3108c74

--- contrib/externs/maps/google_maps_api_v3_4.js
+++ contrib/externs/maps/google_maps_api_v3_4.js
@@ -15,7 +15,7 @@
  */
 
 /**
- * @fileoverview Externs for the Google Maps V3 API.
+ * @fileoverview Externs for the Google Maps V3.4 API.
  * @see http://code.google.com/apis/maps/documentation/javascript/reference.html
  * @externs
  */
