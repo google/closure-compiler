552148a7cdf57d0adfab8ca14c99d5f600627512
nicksantos@google.com <nicksantos@google.com@b0f006be-c8cd-11de-a2e8-8d36a3108c74>
2011-04-29 20:04:43 +0000

    Improve the error message for the new global name check, so that it contains two locations (where both the owner and property are defined)
    R=acleung
    DELTA=6  (4 added, 0 deleted, 2 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=1679
    
    
    git-svn-id: https://closure-compiler.googlecode.com/svn/trunk@1048 b0f006be-c8cd-11de-a2e8-8d36a3108c74

--- src/com/google/javascript/jscomp/CheckGlobalNames.java
+++ src/com/google/javascript/jscomp/CheckGlobalNames.java
@@ -43,7 +43,7 @@ class CheckGlobalNames implements CompilerPass {
   static final DiagnosticType NAME_DEFINED_LATE_WARNING =
       DiagnosticType.warning(
           "JSC_NAME_DEFINED_LATE",
-          "{0} is not defined yet, so properties cannot be referenced on it");
+          "{0} defined before its owner. {1} is defined at {2}:{3}");
 
   static final DiagnosticType STRICT_MODULE_DEP_QNAME =
       DiagnosticType.disabled(
@@ -131,7 +131,11 @@ class CheckGlobalNames implements CompilerPass {
           parent.declaration.preOrderIndex > ref.preOrderIndex) {
         compiler.report(
             JSError.make(ref.source.getName(), ref.node,
-                NAME_DEFINED_LATE_WARNING, parent.fullName()));
+                NAME_DEFINED_LATE_WARNING,
+                name.fullName(),
+                parent.fullName(),
+                parent.declaration.source.getName(),
+                String.valueOf(parent.declaration.node.getLineno())));
       }
     }
   }
