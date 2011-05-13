3ded788d90107f37725bd2670159cb7bb0d1306c
nicksantos@google.com <nicksantos@google.com@b0f006be-c8cd-11de-a2e8-8d36a3108c74>
2011-05-13 02:01:03 +0000

    fix up the license on CaseCanonicalize
    R=msamuel
    DELTA=3  (2 added, 0 deleted, 1 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=1863
    
    
    git-svn-id: https://closure-compiler.googlecode.com/svn/trunk@1092 b0f006be-c8cd-11de-a2e8-8d36a3108c74

--- src/com/google/javascript/jscomp/regex/CaseCanonicalize.java
+++ src/com/google/javascript/jscomp/regex/CaseCanonicalize.java
@@ -1,4 +1,5 @@
-/* Copyright 2011 The Closure Compiler Authors.
+/*
+ * Copyright 2011 The Closure Compiler Authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
