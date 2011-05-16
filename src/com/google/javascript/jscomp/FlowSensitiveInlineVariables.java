4553519e4f2e27200fca0610893a169356d5ce0f
acleung@google.com <acleung@google.com@b0f006be-c8cd-11de-a2e8-8d36a3108c74>
2011-05-16 22:03:01 +0000

    Sort some of the checks by computation complexity so it bails out earlier. (30s -> 27.5s in bullet.js)
    R=johnlenz
    DELTA=46  (22 added, 20 deleted, 4 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=1921
    
    
    git-svn-id: https://closure-compiler.googlecode.com/svn/trunk@1109 b0f006be-c8cd-11de-a2e8-8d36a3108c74

--- src/com/google/javascript/jscomp/FlowSensitiveInlineVariables.java
+++ src/com/google/javascript/jscomp/FlowSensitiveInlineVariables.java
@@ -276,26 +276,6 @@ class FlowSensitiveInlineVariables extends AbstractPostOrderCallback
         return false;
       }
 
-      // We can skip the side effect check along the paths of two nodes if
-      // they are just next to each other.
-      if (NodeUtil.isStatementBlock(defCfgNode.getParent()) &&
-          defCfgNode.getNext() != useCfgNode) {
-        // Similar side effect check as above but this time the side effect is
-        // else where along the path.
-        // x = readProp(b); while(modifyProp(b)) {}; print(x);
-        CheckPathsBetweenNodes<Node, ControlFlowGraph.Branch>
-          pathCheck = new CheckPathsBetweenNodes<Node, ControlFlowGraph.Branch>(
-                 cfg,
-                 cfg.getDirectedGraphNode(defCfgNode),
-                 cfg.getDirectedGraphNode(useCfgNode),
-                 SIDE_EFFECT_PREDICATE,
-                 Predicates.
-                     <DiGraphEdge<Node, ControlFlowGraph.Branch>>alwaysTrue(),
-                 false);
-        if (pathCheck.somePathsSatisfyPredicate()) {
-          return false;
-        }
-      }
 
       // TODO(user): Side-effect is ok sometimes. As long as there are no
       // side-effect function down all paths to the use. Once we have all the
@@ -315,6 +295,13 @@ class FlowSensitiveInlineVariables extends AbstractPostOrderCallback
 
       // Make sure that the name is not within a loop
       if (NodeUtil.isWithinLoop(use)) {
+        return false;
+      }
+
+
+      Collection<Node> uses = reachingUses.getUses(varName, defCfgNode);
+
+      if (uses.size() != 1) {
         return false;
       }
 
@@ -351,10 +338,25 @@ class FlowSensitiveInlineVariables extends AbstractPostOrderCallback
         return false;
       }
 
-      Collection<Node> uses = reachingUses.getUses(varName, defCfgNode);
-
-      if (uses.size() != 1) {
-        return false;
+      // We can skip the side effect check along the paths of two nodes if
+      // they are just next to each other.
+      if (NodeUtil.isStatementBlock(defCfgNode.getParent()) &&
+          defCfgNode.getNext() != useCfgNode) {
+        // Similar side effect check as above but this time the side effect is
+        // else where along the path.
+        // x = readProp(b); while(modifyProp(b)) {}; print(x);
+        CheckPathsBetweenNodes<Node, ControlFlowGraph.Branch>
+          pathCheck = new CheckPathsBetweenNodes<Node, ControlFlowGraph.Branch>(
+                 cfg,
+                 cfg.getDirectedGraphNode(defCfgNode),
+                 cfg.getDirectedGraphNode(useCfgNode),
+                 SIDE_EFFECT_PREDICATE,
+                 Predicates.
+                     <DiGraphEdge<Node, ControlFlowGraph.Branch>>alwaysTrue(),
+                 false);
+        if (pathCheck.somePathsSatisfyPredicate()) {
+          return false;
+        }
       }
 
       return true;
