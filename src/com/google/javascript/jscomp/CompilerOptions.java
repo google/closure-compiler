dda65eadf8c89487850c2093a9f3bc0efef26cc2
johnlenz@google.com <johnlenz@google.com@b0f006be-c8cd-11de-a2e8-8d36a3108c74>
2011-11-30 18:21:49 +0000

    Remove unused CompilerOption "checkTypedPropertyCalls"
    R=tylerg
    DELTA=4  (0 added, 4 deleted, 0 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=3883
    
    
    git-svn-id: https://closure-compiler.googlecode.com/svn/trunk@1652 b0f006be-c8cd-11de-a2e8-8d36a3108c74

--- src/com/google/javascript/jscomp/CompilerOptions.java
+++ src/com/google/javascript/jscomp/CompilerOptions.java
@@ -167,9 +167,6 @@ public class CompilerOptions implements Serializable, Cloneable {
   /** Tightens types based on a global analysis. Experimental. */
   public boolean tightenTypes;
 
-  /** Checks for inexistant property calls */
-  public boolean checkTypedPropertyCalls;
-
   public CheckLevel reportMissingOverride;
 
   /**
@@ -750,7 +747,6 @@ public class CompilerOptions implements Serializable, Cloneable {
     checkControlStructures = false;
     checkTypes = false;
     tightenTypes = false;
-    checkTypedPropertyCalls = false;
     reportMissingOverride = CheckLevel.OFF;
     reportUnknownTypes = CheckLevel.OFF;
     checkRequires = CheckLevel.OFF;
