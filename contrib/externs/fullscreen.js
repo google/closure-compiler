7e73db05e57c4f57eba1ea173116b4ea6570098f
nicksantos@google.com <nicksantos@google.com@b0f006be-c8cd-11de-a2e8-8d36a3108c74>
2012-01-27 16:25:09 +0000

    Externs definition for HTML5 fullscreen API Contributed by edo999 Fixes issue 647
    R=johnlenz
    DELTA=66  (66 added, 0 deleted, 0 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=4143
    
    
    git-svn-id: https://closure-compiler.googlecode.com/svn/trunk@1749 b0f006be-c8cd-11de-a2e8-8d36a3108c74

---
+++ contrib/externs/fullscreen.js
@@ -0,0 +1,65 @@
+/*
+ * Copyright 2012 The Closure Compiler Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/**
+ * @fileoverview Definitions for the fullscreen API specification of HTML5.
+ * @see http://dvcs.w3.org/hg/fullscreen/raw-file/tip/Overview.html
+ *
+ * @externs
+ */
+
+// Externs as defined by the specification.
+Element.prototype.requestFullscreen = function() {};
+
+/** @type {boolean} */
+Document.prototype.fullscreenEnabled;
+
+/** @type {Element} */
+Document.prototype.fullscreenElement;
+
+Document.prototype.exitFullscreen = function() {};
+
+// Externs definitions of browser current implementations.
+// Firefox 10 implementation.
+Element.prototype.mozRequestFullScreen = function() {};
+
+/** @type {boolean} */
+Document.prototype.mozFullScreen;
+
+Document.prototype.mozCancelFullScreen = function() {};
+
+/** @type {Element} */
+Document.prototype.mozFullScreenElement;
+
+/** @type {boolean} */
+Document.prototype.mozFullScreenEnabled;
+
+// Chrome 18 implementation.
+Element.prototype.webkitRequestFullScreen = function() {};
+
+/** @type {boolean} */
+Document.prototype.webkitIsFullScreen;
+
+Document.prototype.webkitCancelFullScreen = function() {};
+
+/** @type {Element} */
+Document.prototype.webkitCurrentFullScreenElement;
+
+/** @type {boolean} */
+Document.prototype.webkitFullScreenKeyboardInputAllowed;
+
+/** @type {number} */
+Element.ALLOW_KEYBOARD_INPUT;
