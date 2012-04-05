b31b4de025de3beefcd5bf164bb64e13f2ce0691
nicksantos@google.com <nicksantos@google.com@b0f006be-c8cd-11de-a2e8-8d36a3108c74>
2012-04-05 21:58:21 +0000

    widen the RAI API a bit more. fixes issue 696
    R=acleung
    DELTA=3  (0 added, 0 deleted, 3 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=4483
    
    
    git-svn-id: https://closure-compiler.googlecode.com/svn/trunk@1870 b0f006be-c8cd-11de-a2e8-8d36a3108c74

--- src/com/google/javascript/jscomp/type/ChainableReverseAbstractInterpreter.java
+++ src/com/google/javascript/jscomp/type/ChainableReverseAbstractInterpreter.java
@@ -113,7 +113,7 @@ public abstract class ChainableReverseAbstractInterpreter
    * name whose type is capable of being refined.
    * @return The current type of the node if it can be refined, null otherwise.
    */
-  JSType getTypeIfRefinable(Node node, FlowScope scope) {
+  protected JSType getTypeIfRefinable(Node node, FlowScope scope) {
     switch (node.getType()) {
       case Token.NAME:
         StaticSlot<JSType> nameVar = scope.getSlot(node.getString());
@@ -590,14 +590,14 @@ public abstract class ChainableReverseAbstractInterpreter
   /**
    * Returns a version of type where undefined is not present.
    */
-  final JSType getRestrictedWithoutUndefined(JSType type) {
+  protected final JSType getRestrictedWithoutUndefined(JSType type) {
     return type == null ? null : type.visit(restrictUndefinedVisitor);
   }
 
   /**
    * Returns a version of type where null is not present.
    */
-  final JSType getRestrictedWithoutNull(JSType type) {
+  protected final JSType getRestrictedWithoutNull(JSType type) {
     return type == null ? null : type.visit(restrictNullVisitor);
   }
 
