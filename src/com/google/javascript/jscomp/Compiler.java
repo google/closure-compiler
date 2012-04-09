0a670cb51fd56c12c19f98884c9513792ebc3f76
nicksantos@google.com <nicksantos@google.com@b0f006be-c8cd-11de-a2e8-8d36a3108c74>
2012-04-09 22:00:50 +0000

    add dependency management in whitespace-only mode contributed by Chris Peisert fixes issue 703
    R=acleung
    DELTA=16  (15 added, 1 deleted, 0 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=4496
    
    
    git-svn-id: https://closure-compiler.googlecode.com/svn/trunk@1879 b0f006be-c8cd-11de-a2e8-8d36a3108c74

--- src/com/google/javascript/jscomp/Compiler.java
+++ src/com/google/javascript/jscomp/Compiler.java
@@ -1282,7 +1282,6 @@ public class Compiler extends AbstractCompiler {
 
       // Check if the sources need to be re-ordered.
       if (options.dependencyOptions.needsManagement() &&
-          !options.skipAllPasses &&
           options.closurePass) {
         for (CompilerInput input : inputs) {
           // Forward-declare all the provided types, so that they
--- test/com/google/javascript/jscomp/CommandLineRunnerTest.java
+++ test/com/google/javascript/jscomp/CommandLineRunnerTest.java
@@ -621,6 +621,20 @@ public class CommandLineRunnerTest extends TestCase {
          });
   }
 
+  public void testDependencySortingWhitespaceMode() {
+    args.add("--manage_closure_dependencies");
+    args.add("--compilation_level=WHITESPACE_ONLY");
+    test(new String[] {
+          "goog.require('beer');",
+          "goog.provide('beer');\ngoog.require('hops');",
+          "goog.provide('hops');",
+         },
+         new String[] {
+          "goog.provide('hops');",
+          "goog.provide('beer');\ngoog.require('hops');",
+          "goog.require('beer');"
+         });
+  }
 
   public void testForwardDeclareDroppedTypes() {
     args.add("--manage_closure_dependencies=true");
