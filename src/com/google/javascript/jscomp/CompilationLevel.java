7f1732c82d9d4feb9336d9ab94f7c28f4e90a74e
johnlenz@google.com <johnlenz@google.com@b0f006be-c8cd-11de-a2e8-8d36a3108c74>
2012-04-26 17:43:02 +0000

    Enable assumeClosuresOnlyCaptureReferences by default.
    R=nicksantos
    DELTA=10  (10 added, 0 deleted, 0 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=4620
    
    
    git-svn-id: https://closure-compiler.googlecode.com/svn/trunk@1924 b0f006be-c8cd-11de-a2e8-8d36a3108c74

--- src/com/google/javascript/jscomp/CompilationLevel.java
+++ src/com/google/javascript/jscomp/CompilationLevel.java
@@ -106,6 +106,7 @@ public enum CompilationLevel {
     options.setInlineVariables(Reach.LOCAL_ONLY);
     options.flowSensitiveInlineVariables = true;
     options.setInlineFunctions(Reach.LOCAL_ONLY);
+    options.setAssumeClosuresOnlyCaptureReferences(true);
     options.checkGlobalThisLevel = CheckLevel.OFF;
     options.foldConstants = true;
     options.coalesceVariableNames = true;
@@ -165,6 +166,7 @@ public enum CompilationLevel {
     options.smartNameRemoval = true;
     options.inlineConstantVars = true;
     options.setInlineFunctions(Reach.ALL);
+    options.setAssumeClosuresOnlyCaptureReferences(true);
     options.inlineGetters = true;
     options.setInlineVariables(Reach.ALL);
     options.flowSensitiveInlineVariables = true;
