ebf229b05f4ee71bc05d70830f5dd5683271d661
nicksantos@google.com <nicksantos@google.com@b0f006be-c8cd-11de-a2e8-8d36a3108c74>
2012-06-21 22:45:36 +0000

    fix a bad String() optimization fixes issue 759
    R=johnlenz
    DELTA=18  (16 added, 0 deleted, 2 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=5055
    
    
    git-svn-id: https://closure-compiler.googlecode.com/svn/trunk@2061 b0f006be-c8cd-11de-a2e8-8d36a3108c74

--- src/com/google/javascript/jscomp/PeepholeSubstituteAlternateSyntax.java
+++ src/com/google/javascript/jscomp/PeepholeSubstituteAlternateSyntax.java
@@ -210,9 +210,15 @@ class PeepholeSubstituteAlternateSyntax
     Node callTarget = n.getFirstChild();
     if (callTarget != null && callTarget.isName() &&
           callTarget.getString().equals("String")) {
-      // Fold String(a) to ''+(a) - which allows further optimizations
+      // Fold String(a) to '' + (a) on immutable literals,
+      // which allows further optimizations
+      //
+      // We can't do this in the general case, because String(a) has
+      // slightly different semantics than '' + (a). See
+      // http://code.google.com/p/closure-compiler/issues/detail?id=759
       Node value = callTarget.getNext();
-      if (value != null) {
+      if (value != null && value.getNext() == null &&
+          NodeUtil.isImmutableValue(value)) {
         Node addition = IR.add(
             IR.string("").srcref(callTarget),
             value.detachFromParent());
--- test/com/google/javascript/jscomp/PeepholeSubstituteAlternateSyntaxTest.java
+++ test/com/google/javascript/jscomp/PeepholeSubstituteAlternateSyntaxTest.java
@@ -1028,6 +1028,8 @@ public class PeepholeSubstituteAlternateSyntaxTest extends CompilerTestCase {
   public void testSimpleFunctionCall() {
     test("var a = String(23)", "var a = '' + 23");
     test("var a = String('hello')", "var a = '' + 'hello'");
+    testSame("var a = String('hello', bar());");
+    testSame("var a = String({valueOf: function() { return 1; }});");
   }
 
   private static class StringCompareTestCase extends CompilerTestCase {
