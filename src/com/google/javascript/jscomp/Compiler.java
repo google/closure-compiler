ea119a2d06328852763f9ee447c3bf74fa1f83dc
nicksantos@google.com <nicksantos@google.com@b0f006be-c8cd-11de-a2e8-8d36a3108c74>
2012-07-03 04:44:08 +0000

    Allow dependency sorting even when closure pass is off. Fixes issue 768
    R=johnlenz
    DELTA=23  (21 added, 1 deleted, 1 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=5100
    
    
    git-svn-id: https://closure-compiler.googlecode.com/svn/trunk@2073 b0f006be-c8cd-11de-a2e8-8d36a3108c74

--- src/com/google/javascript/jscomp/Compiler.java
+++ src/com/google/javascript/jscomp/Compiler.java
@@ -1285,8 +1285,7 @@ public class Compiler extends AbstractCompiler {
 
       // Check if the sources need to be re-ordered.
       boolean staleInputs = false;
-      if (options.dependencyOptions.needsManagement() &&
-          options.closurePass) {
+      if (options.dependencyOptions.needsManagement()) {
         for (CompilerInput input : inputs) {
           // Forward-declare all the provided types, so that they
           // are not flagged even if they are dropped from the process.
--- test/com/google/javascript/jscomp/IntegrationTest.java
+++ test/com/google/javascript/jscomp/IntegrationTest.java
@@ -2099,6 +2099,27 @@ public class IntegrationTest extends IntegrationTestCase {
         "var x = {};");
   }
 
+  public void testDependencySorting() throws Exception {
+    CompilerOptions options = createCompilerOptions();
+    options.setDependencyOptions(
+        new DependencyOptions()
+        .setDependencySorting(true));
+    test(
+        options,
+        new String[] {
+          "goog.require('x');",
+          "goog.provide('x');",
+        },
+        new String[] {
+          "goog.provide('x');",
+          "goog.require('x');",
+
+          // For complicated reasons involving modules,
+          // the compiler creates a synthetic source file.
+          "",
+        });
+  }
+
   public void testStrictWarningsGuard() throws Exception {
     CompilerOptions options = createCompilerOptions();
     options.checkTypes = true;
