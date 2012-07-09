33ab4df84afbdb36216b8a1ea3a526c438615e26
nicksantos@google.com <nicksantos@google.com@b0f006be-c8cd-11de-a2e8-8d36a3108c74>
2012-07-09 16:00:23 +0000

    When declaring @const vars initialized to a type-casted value, prefer the type-cast as the declared type. fixes issue 688
    R=johnlenz
    DELTA=26  (25 added, 0 deleted, 1 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=5125
    
    
    git-svn-id: https://closure-compiler.googlecode.com/svn/trunk@2076 b0f006be-c8cd-11de-a2e8-8d36a3108c74

--- src/com/google/javascript/jscomp/TypedScopeCreator.java
+++ src/com/google/javascript/jscomp/TypedScopeCreator.java
@@ -1288,8 +1288,14 @@ final class TypedScopeCreator implements ScopeCreator {
           if (info.isConstant()) {
             JSType knownType = null;
             if (rValue != null) {
-              if (rValue.getJSType() != null
+              JSDocInfo rValueInfo = rValue.getJSDocInfo();
+              if (rValueInfo != null && rValueInfo.hasType()) {
+                // If rValue has a type-cast, we use the type in the type-cast.
+                return rValueInfo.getType().evaluate(scope, typeRegistry);
+              } else if (rValue.getJSType() != null
                   && !rValue.getJSType().isUnknownType()) {
+                // If rValue's type was already computed during scope creation,
+                // then we can safely use that.
                 return rValue.getJSType();
               } else if (rValue.isOr()) {
                 // Check for a very specific JS idiom:
--- test/com/google/javascript/jscomp/TypeCheckTest.java
+++ test/com/google/javascript/jscomp/TypeCheckTest.java
@@ -5902,6 +5902,25 @@ public class TypeCheckTest extends CompilerTypeTestCase {
          "}");
   }
 
+  public void testIssue688() throws Exception {
+    testTypes(
+        "/** @const */ var SOME_DEFAULT =\n" +
+        "    /** @type {TwoNumbers} */ ({first: 1, second: 2});\n" +
+        "/**\n" +
+        "* Class defining an interface with two numbers.\n" +
+        "* @interface\n" +
+        "*/\n" +
+        "function TwoNumbers() {}\n" +
+        "/** @type number */\n" +
+        "TwoNumbers.prototype.first;\n" +
+        "/** @type number */\n" +
+        "TwoNumbers.prototype.second;\n" +
+        "/** @return {number} */ function f() { return SOME_DEFAULT; }",
+        "inconsistent return type\n" +
+        "found   : (TwoNumbers|null)\n" +
+        "required: number");
+  }
+
   public void testIssue700() throws Exception {
     testTypes(
         "/**\n" +
