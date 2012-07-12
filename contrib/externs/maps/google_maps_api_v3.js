23386f99fb475cb7d2ed969fe5761bc60d4ba571
zwarts@google.com <zwarts@google.com@b0f006be-c8cd-11de-a2e8-8d36a3108c74>
2012-07-12 05:22:15 +0000

    Externs for Maps API v3.9.9.
    R=enochlau
    DELTA=111  (110 added, 0 deleted, 1 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=5141
    
    
    git-svn-id: https://closure-compiler.googlecode.com/svn/trunk@2089 b0f006be-c8cd-11de-a2e8-8d36a3108c74

--- contrib/externs/maps/google_maps_api_v3.js
+++ contrib/externs/maps/google_maps_api_v3.js
@@ -3952,7 +3952,17 @@ google.maps.TransitVehicle = function() {};
 /**
  * @type {string}
  */
+google.maps.TransitVehicle.prototype.icon;
+
+/**
+ * @type {string}
+ */
 google.maps.TransitVehicle.prototype.local_icon;
+
+/**
+ * @type {string}
+ */
+google.maps.TransitVehicle.prototype.name;
 
 /**
  * @enum {number|string}
@@ -4761,6 +4771,11 @@ google.maps.places.PlaceSearchRequest.prototype.name;
 google.maps.places.PlaceSearchRequest.prototype.radius;
 
 /**
+ * @type {google.maps.places.RankBy}
+ */
+google.maps.places.PlaceSearchRequest.prototype.rankBy;
+
+/**
  * @type {Array.<string>}
  */
 google.maps.places.PlaceSearchRequest.prototype.types;
@@ -4787,6 +4802,13 @@ google.maps.places.PlacesService.prototype.getDetails = function(request, callba
 google.maps.places.PlacesService.prototype.search = function(request, callback) {};
 
 /**
+ * @param {google.maps.places.TextSearchRequest|Object.<string>} request
+ * @param {function(Array.<google.maps.places.PlaceResult>, google.maps.places.PlacesServiceStatus)} callback
+ * @return {undefined}
+ */
+google.maps.places.PlacesService.prototype.textSearch = function(request, callback) {};
+
+/**
  * @enum {number|string}
  */
 google.maps.places.PlacesServiceStatus = {
@@ -4797,6 +4819,39 @@ google.maps.places.PlacesServiceStatus = {
   UNKNOWN_ERROR: '',
   ZERO_RESULTS: ''
 };
+
+/**
+ * @enum {number|string}
+ */
+google.maps.places.RankBy = {
+  DISTANCE: '',
+  PROMINENCE: ''
+};
+
+/**
+ * @constructor
+ */
+google.maps.places.TextSearchRequest = function() {};
+
+/**
+ * @type {google.maps.LatLngBounds}
+ */
+google.maps.places.TextSearchRequest.prototype.bounds;
+
+/**
+ * @type {google.maps.LatLng}
+ */
+google.maps.places.TextSearchRequest.prototype.location;
+
+/**
+ * @type {string}
+ */
+google.maps.places.TextSearchRequest.prototype.query;
+
+/**
+ * @type {number}
+ */
+google.maps.places.TextSearchRequest.prototype.radius;
 
 // Namespace
 google.maps.visualization = {};
