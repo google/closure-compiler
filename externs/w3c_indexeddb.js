dec2f2c33a77a0c19a9d8270ef3f6b2576bc7380
chadkillingsworth@missouristate.edu <chadkillingsworth@missouristate.edu@b0f006be-c8cd-11de-a2e8-8d36a3108c74>
2012-10-02 19:18:11 +0000

    Correct a type annotation in w3c_indexeddb.js
    R=nicksantos
    
    git-svn-id: https://closure-compiler.googlecode.com/svn/trunk@2230 b0f006be-c8cd-11de-a2e8-8d36a3108c74

--- externs/w3c_indexeddb.js
+++ externs/w3c_indexeddb.js
@@ -346,7 +346,7 @@ IDBDatabase.prototype.description;
 IDBDatabase.prototype.version;
 
 /**
- * @type {Array.<string>}
+ * @type {DOMStringList}
  * @const
  */
 IDBDatabase.prototype.objectStoreNames;
