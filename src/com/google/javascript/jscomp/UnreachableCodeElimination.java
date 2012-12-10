93252fd6ea544d95224e3758576bc1d1ab580b5c
acleung@google.com <acleung@google.com@b0f006be-c8cd-11de-a2e8-8d36a3108c74>
2012-12-10 20:23:31 +0000

    Don't keep a stack of CFGs during UnreachableCodeElimination.
    R=blickly
    DELTA=259  (85 added, 77 deleted, 97 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=5950
    
    
    git-svn-id: https://closure-compiler.googlecode.com/svn/trunk@2377 b0f006be-c8cd-11de-a2e8-8d36a3108c74

--- src/com/google/javascript/jscomp/UnreachableCodeElimination.java
+++ src/com/google/javascript/jscomp/UnreachableCodeElimination.java
@@ -19,6 +19,7 @@ package com.google.javascript.jscomp;
 import com.google.common.base.Preconditions;
 import com.google.javascript.jscomp.ControlFlowGraph.Branch;
 import com.google.javascript.jscomp.NodeTraversal.AbstractPostOrderCallback;
+import com.google.javascript.jscomp.NodeTraversal.AbstractShallowCallback;
 import com.google.javascript.jscomp.NodeTraversal.ScopedCallback;
 import com.google.javascript.jscomp.graph.GraphReachability;
 import com.google.javascript.jscomp.graph.DiGraph.DiGraphEdge;
@@ -26,8 +27,6 @@ import com.google.javascript.jscomp.graph.DiGraph.DiGraphNode;
 import com.google.javascript.rhino.Node;
 import com.google.javascript.rhino.Token;
 
-import java.util.Deque;
-import java.util.LinkedList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -52,11 +51,6 @@ class UnreachableCodeElimination extends AbstractPostOrderCallback
   private final AbstractCompiler compiler;
   private final boolean removeNoOpStatements;
 
-  Deque<ControlFlowGraph<Node>> cfgStack =
-      new LinkedList<ControlFlowGraph<Node>>();
-
-  ControlFlowGraph<Node> curCfg = null;
-
   UnreachableCodeElimination(AbstractCompiler compiler,
       boolean removeNoOpStatements) {
     this.compiler = compiler;
@@ -64,22 +58,23 @@ class UnreachableCodeElimination extends AbstractPostOrderCallback
   }
 
   @Override
-  public void enterScope(NodeTraversal t) {
+  public void exitScope(NodeTraversal t) {
     Scope scope = t.getScope();
 
     // Computes the control flow graph.
     ControlFlowAnalysis cfa = new ControlFlowAnalysis(compiler, false, false);
     cfa.process(null, scope.getRootNode());
-    cfgStack.push(curCfg);
-    curCfg = cfa.getCfg();
+    ControlFlowGraph<Node> cfg = cfa.getCfg();
 
-    new GraphReachability<Node, ControlFlowGraph.Branch>(curCfg)
-        .compute(curCfg.getEntry().getValue());
-  }
+    new GraphReachability<Node, ControlFlowGraph.Branch>(cfg)
+        .compute(cfg.getEntry().getValue());
 
-  @Override
-  public void exitScope(NodeTraversal t) {
-    curCfg = cfgStack.pop();
+    Node root = scope.getRootNode();
+    if (scope.isLocal()) {
+      root = root.getLastChild();
+    }
+    NodeTraversal.traverse(
+        compiler, root, new EliminationPass(cfg));
   }
 
   @Override
@@ -87,166 +82,179 @@ class UnreachableCodeElimination extends AbstractPostOrderCallback
     NodeTraversal.traverse(compiler, root, this);
   }
 
-  @Override
-  public void visit(NodeTraversal t, Node n, Node parent) {
-    if (parent == null) {
-      return;
-    }
-    if (n.isFunction() || n.isScript()) {
-      return;
+  private class EliminationPass extends AbstractShallowCallback {
+    private final ControlFlowGraph<Node> cfg;
+    private EliminationPass(ControlFlowGraph<Node> cfg) {
+      this.cfg = cfg;
     }
 
-    DiGraphNode<Node, Branch> gNode = curCfg.getDirectedGraphNode(n);
-    if (gNode == null) { // Not in CFG.
-      return;
-    }
-    if (gNode.getAnnotation() != GraphReachability.REACHABLE ||
-        (removeNoOpStatements && !NodeUtil.mayHaveSideEffects(n, compiler))) {
-      removeDeadExprStatementSafely(n);
-      return;
+    @Override
+    public void visit(NodeTraversal t, Node n, Node parent) {
+      if (parent == null) {
+        return;
+      }
+      if (n.isFunction() || n.isScript()) {
+        return;
+      }
+
+      DiGraphNode<Node, Branch> gNode = cfg.getDirectedGraphNode(n);
+      if (gNode == null) { // Not in CFG.
+        return;
+      }
+      if (gNode.getAnnotation() != GraphReachability.REACHABLE ||
+          (removeNoOpStatements && !NodeUtil.mayHaveSideEffects(n, compiler))) {
+        removeDeadExprStatementSafely(n);
+        return;
+      }
+
+      tryRemoveUnconditionalBranching(n);
     }
 
-    tryRemoveUnconditionalBranching(n);
-  }
-
-  /**
-   * Tries to remove n if it is an unconditional branch node (break, continue,
-   * or return) and the target of n is the same as the the follow of n.
-   * That is, if removing n preserves the control flow. Also if n targets
-   * another unconditional branch, this function will recursively try to remove
-   * the target branch as well. The reason why we want to cascade this removal
-   * is because we only run this pass once. If we have code such as
-   *
-   * break -> break -> break
-   *
-   * where all 3 breaks are useless, then the order of removal matters. When we
-   * first look at the first break, we see that it branches to the 2nd break.
-   * However, if we remove the last break, the 2nd break becomes useless and
-   * finally the first break becomes useless as well.
-   *
-   * @return The target of this jump. If the target is also useless jump,
-   *     the target of that useless jump recursively.
-   */
-  @SuppressWarnings("fallthrough")
-  private Node tryRemoveUnconditionalBranching(Node n) {
-    /*
-     * For each unconditional branching control flow node, check to see
-     * if the ControlFlowAnalysis.computeFollowNode of that node is same as
-     * the branching target. If it is, the branch node is safe to be removed.
+    /**
+     * Tries to remove n if it is an unconditional branch node (break, continue,
+     * or return) and the target of n is the same as the the follow of n.
+     * That is, if removing n preserves the control flow. Also if n targets
+     * another unconditional branch, this function will recursively try to remove
+     * the target branch as well. The reason why we want to cascade this removal
+     * is because we only run this pass once. If we have code such as
      *
-     * This is not as clever as MinimizeExitPoints because it doesn't do any
-     * if-else conversion but it handles more complicated switch statements
-     * much more nicely.
+     * break -> break -> break
+     *
+     * where all 3 breaks are useless, then the order of removal matters. When we
+     * first look at the first break, we see that it branches to the 2nd break.
+     * However, if we remove the last break, the 2nd break becomes useless and
+     * finally the first break becomes useless as well.
+     *
+     * @return The target of this jump. If the target is also useless jump,
+     *     the target of that useless jump recursively.
      */
+    @SuppressWarnings("fallthrough")
+    private Node tryRemoveUnconditionalBranching(Node n) {
+      /*
+       * For each unconditional branching control flow node, check to see
+       * if the ControlFlowAnalysis.computeFollowNode of that node is same as
+       * the branching target. If it is, the branch node is safe to be removed.
+       *
+       * This is not as clever as MinimizeExitPoints because it doesn't do any
+       * if-else conversion but it handles more complicated switch statements
+       * much more nicely.
+       */
 
-    // If n is null the target is the end of the function, nothing to do.
-    if (n == null) {
-       return n;
-    }
+      // If n is null the target is the end of the function, nothing to do.
+      if (n == null) {
+         return n;
+      }
 
-    DiGraphNode<Node, Branch> gNode = curCfg.getDirectedGraphNode(n);
+      DiGraphNode<Node, Branch> gNode = cfg.getDirectedGraphNode(n);
 
-    if (gNode == null) {
+      if (gNode == null) {
+        return n;
+      }
+
+      switch (n.getType()) {
+        case Token.RETURN:
+          if (n.hasChildren()) {
+            break;
+          }
+        case Token.BREAK:
+        case Token.CONTINUE:
+
+          // We are looking for a control flow changing statement that always
+          // branches to the same node. If after removing it control still
+          // branches to the same node, it is safe to remove.
+          List<DiGraphEdge<Node,Branch>> outEdges = gNode.getOutEdges();
+          if (outEdges.size() == 1 &&
+              // If there is a next node, there is no chance this jump is useless.
+              (n.getNext() == null || n.getNext().isFunction())) {
+
+            Preconditions.checkState(outEdges.get(0).getValue() == Branch.UNCOND);
+            Node fallThrough = computeFollowing(n);
+            Node nextCfgNode = outEdges.get(0).getDestination().getValue();
+            if (nextCfgNode == fallThrough) {
+              removeDeadExprStatementSafely(n);
+              return fallThrough;
+            }
+          }
+      }
       return n;
     }
 
-    switch (n.getType()) {
-      case Token.RETURN:
-        if (n.hasChildren()) {
-          break;
+    private Node computeFollowing(Node n) {
+      Node next = ControlFlowAnalysis.computeFollowNode(n);
+      while (next != null && next.isBlock()) {
+        if (next.hasChildren()) {
+          next = next.getFirstChild();
+        } else {
+          next = computeFollowing(next);
         }
-      case Token.BREAK:
-      case Token.CONTINUE:
-
-        // We are looking for a control flow changing statement that always
-        // branches to the same node. If after removing it control still
-        // branches to the same node, it is safe to remove.
-        List<DiGraphEdge<Node,Branch>> outEdges = gNode.getOutEdges();
-        if (outEdges.size() == 1 &&
-            // If there is a next node, there is no chance this jump is useless.
-            (n.getNext() == null || n.getNext().isFunction())) {
-
-          Preconditions.checkState(outEdges.get(0).getValue() == Branch.UNCOND);
-          Node fallThrough = computeFollowing(n);
-          Node nextCfgNode = outEdges.get(0).getDestination().getValue();
-          if (nextCfgNode == fallThrough) {
-            removeDeadExprStatementSafely(n);
-            return fallThrough;
-          }
-        }
-    }
-    return n;
-  }
-
-  private Node computeFollowing(Node n) {
-    Node next = ControlFlowAnalysis.computeFollowNode(n);
-    while (next != null && next.isBlock()) {
-      if (next.hasChildren()) {
-        next = next.getFirstChild();
-      } else {
-        next = computeFollowing(next);
       }
-    }
-    return next;
-  }
-
-  private void removeDeadExprStatementSafely(Node n) {
-    Node parent = n.getParent();
-    if (n.isEmpty() ||
-        (n.isBlock() && !n.hasChildren())) {
-      // Not always trivial to remove, let FoldConstants work its magic later.
-      return;
+      return next;
     }
 
-    // TODO(user): This is a problem with removeNoOpStatements.
-    // Every expression in a FOR-IN header looks side effect free on its own.
-    if (NodeUtil.isForIn(parent)) {
-      return;
-    }
-
-    switch (n.getType()) {
-      // Removing an unreachable DO node is messy because it means we still have
-      // to execute one iteration. If the DO's body has breaks in the middle, it
-      // can get even more tricky and code size might actually increase.
-      case Token.DO:
+    private void removeDeadExprStatementSafely(Node n) {
+      Node parent = n.getParent();
+      if (n.isEmpty() ||
+          (n.isBlock() && !n.hasChildren())) {
+        // Not always trivial to remove, let FoldConstants work its magic later.
         return;
+      }
 
-      case Token.BLOCK:
-        // BLOCKs are used in several ways including wrapping CATCH
-        // blocks in TRYs
-        if (parent.isTry()) {
-          if (NodeUtil.isTryCatchNodeContainer(n)) {
-            return;
+      // TODO(user): This is a problem with removeNoOpStatements.
+      // Every expression in a FOR-IN header looks side effect free on its own.
+      if (NodeUtil.isForIn(parent)) {
+        return;
+      }
+
+      switch (n.getType()) {
+        // Removing an unreachable DO node is messy because it means we still have
+        // to execute one iteration. If the DO's body has breaks in the middle, it
+        // can get even more tricky and code size might actually increase.
+        case Token.DO:
+          return;
+
+        case Token.BLOCK:
+          // BLOCKs are used in several ways including wrapping CATCH
+          // blocks in TRYs
+          if (parent.isTry()) {
+            if (NodeUtil.isTryCatchNodeContainer(n)) {
+              return;
+            }
           }
-        }
-        break;
+          break;
 
-      case Token.CATCH:
-        Node tryNode = parent.getParent();
-        NodeUtil.maybeAddFinally(tryNode);
-        break;
+        case Token.CATCH:
+          Node tryNode = parent.getParent();
+          NodeUtil.maybeAddFinally(tryNode);
+          break;
+      }
+
+
+      if (n.isVar() && !n.getFirstChild().hasChildren()) {
+        // Very Edge case, Consider this:
+        // File 1: {throw 1}
+        // File 2: {var x}
+        // The node var x is unreachable in the global scope.
+        // Before we remove the node, redeclareVarsInsideBranch
+        // would basically move var x to the beginning of File 2,
+        // which resulted in zero changes to the AST but triggered
+        // reportCodeChange().
+        // Instead, we should just ignore dead variable declarations.
+        return;
+      }
+
+      NodeUtil.redeclareVarsInsideBranch(n);
+      compiler.reportCodeChange();
+      if (logger.isLoggable(Level.FINE)) {
+        logger.fine("Removing " + n.toString());
+      }
+
+      NodeUtil.removeChild(n.getParent(), n);
     }
-
-
-    if (n.isVar() && !n.getFirstChild().hasChildren()) {
-      // Very Edge case, Consider this:
-      // File 1: {throw 1}
-      // File 2: {var x}
-      // The node var x is unreachable in the global scope.
-      // Before we remove the node, redeclareVarsInsideBranch
-      // would basically move var x to the beginning of File 2,
-      // which resulted in zero changes to the AST but triggered
-      // reportCodeChange().
-      // Instead, we should just ignore dead variable declarations.
-      return;
-    }
-
-    NodeUtil.redeclareVarsInsideBranch(n);
-    compiler.reportCodeChange();
-    if (logger.isLoggable(Level.FINE)) {
-      logger.fine("Removing " + n.toString());
-    }
-
-    NodeUtil.removeChild(n.getParent(), n);
   }
+
+  @Override
+  public void visit(NodeTraversal t, Node n, Node parent) {}
+
+  @Override
+  public void enterScope(NodeTraversal t) {}
 }
