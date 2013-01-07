82776475362a3fd598ed656e17d40a411e99f643
jmoon@google.com <jmoon@google.com@b0f006be-c8cd-11de-a2e8-8d36a3108c74>
2013-01-07 23:39:53 +0000

    This particular CL contains the changes to KeyRange and the corresponding changes to callers to prevent new compiler errors.
    This has the potential to introduce compiler type-check errors
    
    R=nicksantos
    DELTA=50 (0 added, 0 deleted, 50 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=6088
    
    
    git-svn-id: https://closure-compiler.googlecode.com/svn/trunk@2445 b0f006be-c8cd-11de-a2e8-8d36a3108c74

--- externs/w3c_indexeddb.js
+++ externs/w3c_indexeddb.js
@@ -839,59 +839,59 @@ IDBKeyRange.prototype.lowerOpen;
 IDBKeyRange.prototype.upperOpen;
 
 /**
- * @param {*} value The single key value of this range.
+ * @param {IDBKeyType} value The single key value of this range.
  * @return {!IDBKeyRange} The key range.
  */
 IDBKeyRange.only = function(value) {};
 
 /**
- * @param {*} value The single key value of this range.
+ * @param {IDBKeyType} value The single key value of this range.
  * @return {!IDBKeyRange} The key range.
  */
 webkitIDBKeyRange.only = function(value) {};
 
 /**
- * @param {*} bound Creates a lower bound key range.
+ * @param {IDBKeyType} bound Creates a lower bound key range.
  * @param {boolean=} open Open the key range.
  * @return {!IDBKeyRange} The key range.
  */
 IDBKeyRange.lowerBound = function(bound, open) {};
 
 /**
- * @param {*} bound Creates a lower bound key range.
+ * @param {IDBKeyType} bound Creates a lower bound key range.
  * @param {boolean=} open Open the key range.
  * @return {!IDBKeyRange} The key range.
  */
 webkitIDBKeyRange.lowerBound = function(bound, open) {};
 
 /**
- * @param {*} bound Creates an upper bound key range.
+ * @param {IDBKeyType} bound Creates an upper bound key range.
  * @param {boolean=} open Open the key range.
  * @return {!IDBKeyRange} The key range.
  */
 IDBKeyRange.upperBound = function(bound, open) {};
 
 /**
- * @param {*} bound Creates an upper bound key range.
+ * @param {IDBKeyType} bound Creates an upper bound key range.
  * @param {boolean=} open Open the key range.
  * @return {!IDBKeyRange} The key range.
  */
 webkitIDBKeyRange.upperBound = function(bound, open) {};
 
 /**
- * @param {*} left The left bound value of openLeft is true.
- * @param {*} right The right bound value of openRight is true.
- * @param {boolean=} openLeft Whether to open a left bound range.
- * @param {boolean=} openRight Whether to open a right bound range.
+ * @param {IDBKeyType} left The left bound value.
+ * @param {IDBKeyType} right The right bound value.
+ * @param {boolean=} openLeft Whether the left bound value should be excluded.
+ * @param {boolean=} openRight Whether the right bound value should be excluded.
  * @return {!IDBKeyRange} The key range.
  */
 IDBKeyRange.bound = function(left, right, openLeft, openRight) {};
 
 /**
- * @param {*} left The left bound value of openLeft is true.
- * @param {*} right The right bound value of openRight is true.
- * @param {boolean=} openLeft Whether to open a left bound range.
- * @param {boolean=} openRight Whether to open a right bound range.
+ * @param {IDBKeyType} left The left bound value.
+ * @param {IDBKeyType} right The right bound value.
+ * @param {boolean=} openLeft Whether the left bound value should be excluded.
+ * @param {boolean=} openRight Whether the right bound value should be excluded.
  * @return {!IDBKeyRange} The key range.
  */
 webkitIDBKeyRange.bound = function(left, right, openLeft, openRight) {};
