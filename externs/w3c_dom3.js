2f853df3d9c0c0ed8c79a990f0e087f7a7d1391f
dpapad@google.com <dpapad@google.com@b0f006be-c8cd-11de-a2e8-8d36a3108c74>
2013-01-31 02:36:05 +0000

    Adding the "name" property to DOMError interface.
    R=nicksantos
    DELTA=6 (6 added, 0 deleted, 0 changed)
    
    
    Revision created by MOE tool push_codebase.
    MOE_MIGRATION=6227
    
    
    git-svn-id: https://closure-compiler.googlecode.com/svn/trunk@2500 b0f006be-c8cd-11de-a2e8-8d36a3108c74

--- externs/w3c_dom3.js
+++ externs/w3c_dom3.js
@@ -727,6 +727,12 @@ DOMError.prototype.severity;
 DOMError.prototype.type;
 
 /**
+ * @type {string}
+ * @see http://www.w3.org/TR/dom/#domerror
+ */
+DOMError.prototype.name;
+
+/**
  * @constructor
  * @see http://www.w3.org/TR/DOM-Level-3-Core/core.html#ERROR-Interfaces-DOMErrorHandler
  */
