237e6ba91721b3dda4adc2c57c0f8d3faee88773
Ben Lickly <blickly@google.com>
2013-05-20 16:11:04 -0700

    Updating externs based on latest Quota Management API specs.
    See https://dvcs.w3.org/hg/quota/raw-file/tip/Overview.html#storagequota-interface.
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=46876782

--- externs/fileapi.js
+++ externs/fileapi.js
@@ -925,3 +925,24 @@ StorageInfo.prototype.queryUsageAndQuota = function(type, successCallback,
  * @type {!StorageInfo}
  */
 Window.prototype.webkitStorageInfo;
+
+/**
+ * @see https://dvcs.w3.org/hg/quota/raw-file/tip/Overview.html#storagequota-interface.
+ * @constructor
+ */
+function StorageQuota() {}
+
+/**
+ * @param {number} size
+ * @param {function(number)=} opt_successCallback
+ * @param {function(!DOMException)=} opt_errorCallback
+ */
+StorageQuota.prototype.requestQuota = function(size, opt_successCallback,
+    opt_errorCallback) {};
+
+/**
+ * @param {function(number, number)} successCallback
+ * @param {function(!DOMException)=} opt_errorCallback
+ */
+StorageQuota.prototype.queryUsageAndQuota = function(successCallback,
+    opt_errorCallback) {};
