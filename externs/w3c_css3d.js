624d38f931bfce8257443cc1923cf676a3a3e73f
Ben Lickly <blickly@google.com>
2013-06-06 16:13:05 -0700

    Update matrix definition to allow for optional parameters to translate, scale, rotate, and rotateAxisAngle methods and correctly reflect optional return of inverse if the matrix is non-invertable. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=47625336

--- externs/w3c_css3d.js
+++ externs/w3c_css3d.js
@@ -17,7 +17,7 @@
 /**
  * @fileoverview Definitions for W3C's CSS 3D Transforms specification.
  *  The whole file has been fully type annotated. Created from
- *  http://www.w3.org/TR/css3-3d-transforms/
+ *  https://dvcs.w3.org/hg/FXTF/raw-file/tip/matrix/index.html
  *
  * @externs
  */
@@ -25,162 +25,162 @@
 /**
  * @constructor
  * @param {string=} opt_matrix
- * @see http://www.w3.org/TR/css3-3d-transforms/#cssmatrix-interface
+ * @see https://dvcs.w3.org/hg/FXTF/raw-file/tip/matrix/index.html#the-cssmatrix-interface
  */
 function CSSMatrix(opt_matrix) {}
 
 /**
  * @type {number}
- * @see http://www.w3.org/TR/css3-3d-transforms/#cssmatrix-interface
+ * @see https://dvcs.w3.org/hg/FXTF/raw-file/tip/matrix/index.html#three-dimensional-attributes
  */
 CSSMatrix.prototype.m11;
 
 /**
  * @type {number}
- * @see http://www.w3.org/TR/css3-3d-transforms/#cssmatrix-interface
+ * @see https://dvcs.w3.org/hg/FXTF/raw-file/tip/matrix/index.html#three-dimensional-attributes
  */
 CSSMatrix.prototype.m12;
 
 /**
  * @type {number}
- * @see http://www.w3.org/TR/css3-3d-transforms/#cssmatrix-interface
+ * @see https://dvcs.w3.org/hg/FXTF/raw-file/tip/matrix/index.html#three-dimensional-attributes
  */
 CSSMatrix.prototype.m13;
 
 /**
  * @type {number}
- * @see http://www.w3.org/TR/css3-3d-transforms/#cssmatrix-interface
+ * @see https://dvcs.w3.org/hg/FXTF/raw-file/tip/matrix/index.html#three-dimensional-attributes
  */
 CSSMatrix.prototype.m14;
 
 /**
  * @type {number}
- * @see http://www.w3.org/TR/css3-3d-transforms/#cssmatrix-interface
+ * @see https://dvcs.w3.org/hg/FXTF/raw-file/tip/matrix/index.html#three-dimensional-attributes
  */
 CSSMatrix.prototype.m21;
 
 /**
  * @type {number}
- * @see http://www.w3.org/TR/css3-3d-transforms/#cssmatrix-interface
+ * @see https://dvcs.w3.org/hg/FXTF/raw-file/tip/matrix/index.html#three-dimensional-attributes
  */
 CSSMatrix.prototype.m22;
 
 /**
  * @type {number}
- * @see http://www.w3.org/TR/css3-3d-transforms/#cssmatrix-interface
+ * @see https://dvcs.w3.org/hg/FXTF/raw-file/tip/matrix/index.html#three-dimensional-attributes
  */
 CSSMatrix.prototype.m23;
 
 /**
  * @type {number}
- * @see http://www.w3.org/TR/css3-3d-transforms/#cssmatrix-interface
+ * @see https://dvcs.w3.org/hg/FXTF/raw-file/tip/matrix/index.html#three-dimensional-attributes
  */
 CSSMatrix.prototype.m24;
 
 /**
  * @type {number}
- * @see http://www.w3.org/TR/css3-3d-transforms/#cssmatrix-interface
+ * @see https://dvcs.w3.org/hg/FXTF/raw-file/tip/matrix/index.html#three-dimensional-attributes
  */
 CSSMatrix.prototype.m31;
 
 /**
  * @type {number}
- * @see http://www.w3.org/TR/css3-3d-transforms/#cssmatrix-interface
+ * @see https://dvcs.w3.org/hg/FXTF/raw-file/tip/matrix/index.html#three-dimensional-attributes
  */
 CSSMatrix.prototype.m32;
 
 /**
  * @type {number}
- * @see http://www.w3.org/TR/css3-3d-transforms/#cssmatrix-interface
+ * @see https://dvcs.w3.org/hg/FXTF/raw-file/tip/matrix/index.html#three-dimensional-attributes
  */
 CSSMatrix.prototype.m33;
 
 /**
  * @type {number}
- * @see http://www.w3.org/TR/css3-3d-transforms/#cssmatrix-interface
+ * @see https://dvcs.w3.org/hg/FXTF/raw-file/tip/matrix/index.html#three-dimensional-attributes
  */
 CSSMatrix.prototype.m34;
 
 /**
  * @type {number}
- * @see http://www.w3.org/TR/css3-3d-transforms/#cssmatrix-interface
+ * @see https://dvcs.w3.org/hg/FXTF/raw-file/tip/matrix/index.html#three-dimensional-attributes
  */
 CSSMatrix.prototype.m41;
 
 /**
  * @type {number}
- * @see http://www.w3.org/TR/css3-3d-transforms/#cssmatrix-interface
+ * @see https://dvcs.w3.org/hg/FXTF/raw-file/tip/matrix/index.html#three-dimensional-attributes
  */
 CSSMatrix.prototype.m42;
 
 /**
  * @type {number}
- * @see http://www.w3.org/TR/css3-3d-transforms/#cssmatrix-interface
+ * @see https://dvcs.w3.org/hg/FXTF/raw-file/tip/matrix/index.html#three-dimensional-attributes
  */
 CSSMatrix.prototype.m43;
 
 /**
  * @type {number}
- * @see http://www.w3.org/TR/css3-3d-transforms/#cssmatrix-interface
+ * @see https://dvcs.w3.org/hg/FXTF/raw-file/tip/matrix/index.html#three-dimensional-attributes
  */
 CSSMatrix.prototype.m44;
 
 /**
  * @param {string} string
  * @return {void}
- * @see http://www.w3.org/TR/css3-3d-transforms/#cssmatrix-interface
  */
 CSSMatrix.prototype.setMatrixValue = function(string) {};
 
 /**
  * @param {!CSSMatrix} secondMatrix
  * @return {!CSSMatrix}
- * @see http://www.w3.org/TR/css3-3d-transforms/#cssmatrix-interface
+ * @see https://dvcs.w3.org/hg/FXTF/raw-file/tip/matrix/index.html#widl-CSSMatrix-multiply-CSSMatrix-CSSMatrix-other
  */
 CSSMatrix.prototype.multiply = function(secondMatrix) {};
 
 /**
- * @return {!CSSMatrix}
- * @see http://www.w3.org/TR/css3-3d-transforms/#cssmatrix-interface
+ * @return {CSSMatrix} Returns void if the matrix is non-invertable.
+ * @see https://dvcs.w3.org/hg/FXTF/raw-file/tip/matrix/index.html#widl-CSSMatrix-inverse-CSSMatrix
  */
 CSSMatrix.prototype.inverse = function() {};
 
 /**
- * @param {number} x
- * @param {number} y
- * @param {number} z
+ * @param {number=} opt_x Defaults to 0.
+ * @param {number=} opt_y Defaults to 0.
+ * @param {number=} opt_z Defaults to 0.
  * @return {!CSSMatrix}
- * @see http://www.w3.org/TR/css3-3d-transforms/#cssmatrix-interface
+ * @see https://dvcs.w3.org/hg/FXTF/raw-file/tip/matrix/index.html#widl-CSSMatrix-translate-CSSMatrix-unrestricted-double-tx-unrestricted-double-ty-unrestricted-double-tz
  */
-CSSMatrix.prototype.translate = function(x, y, z) {};
+CSSMatrix.prototype.translate = function(opt_x, opt_y, opt_z) {};
 
 /**
- * @param {number} scaleX
- * @param {number} scaleY
- * @param {number} scaleZ
+ * @param {number=} opt_scaleX Defaults to 1.
+ * @param {number=} opt_scaleY Defaults to scaleX.
+ * @param {number=} opt_scaleZ Defaults to 1.
  * @return {!CSSMatrix}
- * @see http://www.w3.org/TR/css3-3d-transforms/#cssmatrix-interface
+ * @see https://dvcs.w3.org/hg/FXTF/raw-file/tip/matrix/index.html#widl-CSSMatrix-scale-CSSMatrix-unrestricted-double-scale-unrestricted-double-originX-unrestricted-double-originY
  */
-CSSMatrix.prototype.scale = function(scaleX, scaleY, scaleZ) {};
+CSSMatrix.prototype.scale = function(opt_scaleX, opt_scaleY, opt_scaleZ) {};
 
 /**
- * @param {number} rotX
- * @param {number} rotY
- * @param {number} rotZ
+ * @param {number=} opt_rotX Defaults to 0.
+ * @param {number=} opt_rotY Defaults to 0.
+ * @param {number=} opt_rotZ Defaults to rotX if rotY is not defined, else 0.
  * @return {!CSSMatrix}
- * @see http://www.w3.org/TR/css3-3d-transforms/#cssmatrix-interface
+ * @see https://dvcs.w3.org/hg/FXTF/raw-file/tip/matrix/index.html#widl-CSSMatrix-rotate-CSSMatrix-unrestricted-double-angle-unrestricted-double-originX-unrestricted-double-originY
  */
-CSSMatrix.prototype.rotate = function(rotX, rotY, rotZ) {};
+CSSMatrix.prototype.rotate = function(opt_rotX, opt_rotY, opt_rotZ) {};
 
 /**
- * @param {number} x
- * @param {number} y
- * @param {number} z
- * @param {number} angle
+ * @param {number=} opt_x Defaults to 0.
+ * @param {number=} opt_y Defaults to 0.
+ * @param {number=} opt_z Defaults to 0.
+ * @param {number=} opt_angle Defaults to 0.
  * @return {!CSSMatrix}
- * @see http://www.w3.org/TR/css3-3d-transforms/#cssmatrix-interface
+ * @see https://dvcs.w3.org/hg/FXTF/raw-file/tip/matrix/index.html#widl-CSSMatrix-rotateAxisAngle-CSSMatrix-unrestricted-double-x-unrestricted-double-y-unrestricted-double-z-unrestricted-double-angle
  */
-CSSMatrix.prototype.rotateAxisAngle = function(x, y, z, angle) {};
+CSSMatrix.prototype.rotateAxisAngle =
+    function(opt_x, opt_y, opt_z, opt_angle) {};
 
 /**
  * @constructor
