4a000338cbc7384fff903e630e4ee9d7c71f8c95
Ben Lickly <blickly@google.com>
2013-07-23 16:04:42 -0700

    Update externs to match valid CustomEvent instantiation.
    https://developer.mozilla.org/en-US/docs/Web/API/CustomEvent
    
    var event = new CustomEvent("cat", {"detail":{"hazcheeseburger":true}})
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=49741676

--- externs/w3c_event.js
+++ externs/w3c_event.js
@@ -129,11 +129,13 @@ Event.prototype.preventDefault = function() {};
 Event.prototype.initEvent = function(eventTypeArg, canBubbleArg, cancelableArg) {};
 
 /**
+ * @param {string} eventType
+ * @param {Object=} opt_details
  * @constructor
  * @extends {Event}
  * @see http://www.w3.org/TR/DOM-Level-3-Events/#interface-CustomEvent
  */
-function CustomEvent() {}
+function CustomEvent(eventType, opt_details) {}
 
 /**
  * @param {string} eventType
