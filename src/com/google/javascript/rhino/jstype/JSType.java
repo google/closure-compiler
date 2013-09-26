1d8928bfd2250947ee08dc9d5082ce1bf8ee0424
Ben Lickly <blickly@google.com>
2013-09-26 16:05:25 -0700

    Fix the resolution of template types in inherited methods and similiar. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=53272242

--- src/com/google/javascript/rhino/jstype/JSType.java
+++ src/com/google/javascript/rhino/jstype/JSType.java
@@ -617,6 +617,10 @@ public abstract class JSType implements Serializable {
           that.toObjectType().getReferenceName());
     }
 
+    if (isTemplateType() && that.isTemplateType()) {
+      return this == that;
+    }
+
     // Unbox other proxies.
     if (this instanceof ProxyObjectType) {
       return ((ProxyObjectType) this)
