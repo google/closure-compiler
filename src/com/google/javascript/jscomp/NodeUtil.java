9e8078b82389ea64cdfece38ea844dc05bbc357f
Ben Lickly <blickly@google.com>
2013-11-08 12:12:02 -0800

    Remove unused NodeUtil method. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=56190217

--- src/com/google/javascript/jscomp/NodeUtil.java
+++ src/com/google/javascript/jscomp/NodeUtil.java
@@ -1188,15 +1188,6 @@ public final class NodeUtil {
    * 14 call, member () [] .
    */
   static int precedence(int type) {
-    int precedence = precedenceWithDefault(type);
-    if (precedence != -1) {
-      return precedence;
-    }
-    throw new Error("Unknown precedence for " +
-        Token.name(type) + " (type " + type + ")");
-  }
-
-  static int precedenceWithDefault(int type) {
     switch (type) {
       case Token.COMMA:  return 0;
       case Token.ASSIGN_BITOR:
@@ -1268,8 +1259,8 @@ public final class NodeUtil {
         return 16;
 
       default:
-        // Statements are lower precedence than expressions.
-        return -1;
+        throw new IllegalStateException("Unknown precedence for " +
+            Token.name(type) + " (type " + type + ")");
     }
   }
 
