e5c86ea61874d3ad39e24813be789ce2f6fe0d8c
Ben Lickly <blickly@google.com>
2014-02-11 16:13:37 -0800

    Automated g4 rollback of changelist 60502250.
    *** Reason for rollback ***
    
    fixed
    
    *** Original change description ***
    
    Automated g4 rollback of changelist 60488763.
    
    *** Reason for rollback ***
    
    Breakages
    
    *** Original change description ***
    
    Now that the compiler always parse the externs in ES5 mode, it isn't necessary to quote ES3 reserved word properties
    
    ***
    
    ***
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=61351536

--- externs/w3c_indexeddb.js
+++ externs/w3c_indexeddb.js
@@ -486,7 +486,7 @@ IDBObjectStore.prototype.add = function(value, key) {};
  * @param {IDBKeyType} key The key of this value.
  * @return {!IDBRequest} The IDBRequest object.
  */
-IDBObjectStore.prototype['delete'] = function(key) {};
+IDBObjectStore.prototype.delete = function(key) {};
 
 /**
  * @param {IDBKeyType|!IDBKeyRange} key The key of the document to retrieve.
@@ -691,7 +691,7 @@ IDBCursor.prototype.update = function(value) {};
  * @param {IDBKeyType=} key Continue enumerating the cursor from the specified
  *     key (or next).
  */
-IDBCursor.prototype['continue'] = function(key) {};
+IDBCursor.prototype.continue = function(key) {};
 
 /**
  * @param {number} count Number of times to iterate the cursor.
@@ -702,7 +702,7 @@ IDBCursor.prototype.advance = function(count) {};
  * Note: Must be quoted to avoid parse error.
  * @return {!IDBRequest} The IDBRequest object.
  */
-IDBCursor.prototype['delete'] = function() {};
+IDBCursor.prototype.delete = function() {};
 
 /** @type {function(new:IDBTransaction)} */
 Window.prototype.IDBTransaction;
