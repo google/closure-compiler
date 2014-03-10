f2e6eb1f47f7704858386ed95b904727f163f822
Ben Lickly <blickly@google.com>
2014-03-10 16:09:25 -0700

    Add new generateExports option to API for Closure Compiler web services. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=62885502

--- src/com/google/javascript/jscomp/webservice/common/Protocol.java
+++ src/com/google/javascript/jscomp/webservice/common/Protocol.java
@@ -64,6 +64,7 @@ public class Protocol {
     LANGUAGE("language"),
     USE_TYPES_FOR_OPTIMIZATIONS("use_types_for_optimization"),
     ANGULAR_PASS("angular_pass"),
+    GENERATE_EXPORTS("generate_exports"),
 
     // Old ROBOCOMP urls.
     RAWJS("rawjs"),
