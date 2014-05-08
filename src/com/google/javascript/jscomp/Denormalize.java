96a92e3d265b440fedf9266f93735c4bfc91ba81
Ben Lickly <blickly@google.com>
2014-05-08 16:02:50 -0700

    Remove some unused code in Denormalize. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=66555043

--- src/com/google/javascript/jscomp/Denormalize.java
+++ src/com/google/javascript/jscomp/Denormalize.java
@@ -17,7 +17,6 @@
 package com.google.javascript.jscomp;
 
 import com.google.common.base.Preconditions;
-import com.google.javascript.jscomp.NodeTraversal.AbstractPostOrderCallback;
 import com.google.javascript.jscomp.NodeTraversal.Callback;
 import com.google.javascript.rhino.Node;
 import com.google.javascript.rhino.Token;
@@ -123,29 +122,6 @@ class Denormalize implements CompilerPass, Callback {
       forNode.replaceChild(oldInitializer, newInitializer);
 
       compiler.reportCodeChange();
-    }
-  }
-
-  static class StripConstantAnnotations
-      extends AbstractPostOrderCallback
-      implements CompilerPass {
-    private AbstractCompiler compiler;
-
-    StripConstantAnnotations(AbstractCompiler compiler) {
-      this.compiler = compiler;
-    }
-
-    @Override
-    public void process(Node externs, Node js) {
-      NodeTraversal.traverse(compiler, externs, this);
-      NodeTraversal.traverse(compiler, js, this);
-    }
-
-    @Override
-    public void visit(NodeTraversal t, Node node, Node parent) {
-      if (node.isName() || node.isString() || node.isStringKey()) {
-        node.removeProp(Node.IS_CONSTANT_NAME);
-      }
     }
   }
 }
