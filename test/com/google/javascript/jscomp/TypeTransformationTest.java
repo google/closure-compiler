8541544b2cb3000891f0cd64e4be47b0c8fa261a
Ben Lickly <blickly@google.com>
2014-07-28 15:21:25 -0700

    Reformatted the tests of the evaluation of type transformation expressions ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=72094154

--- test/com/google/javascript/jscomp/TypeTransformationTest.java
+++ test/com/google/javascript/jscomp/TypeTransformationTest.java
@@ -17,282 +17,146 @@
 package com.google.javascript.jscomp;
 
 import com.google.common.collect.ImmutableMap;
-import com.google.javascript.jscomp.parsing.TypeTransformationParser.Keywords;
-import com.google.javascript.rhino.IR;
 import com.google.javascript.rhino.Node;
 import com.google.javascript.rhino.jstype.JSType;
 
 public class TypeTransformationTest extends CompilerTypeTestCase {
 
+  private ImmutableMap<String, JSType> typeVars;
+
+  @Override
+  public void setUp() {
+    super.setUp();
+    typeVars = ImmutableMap.of(
+        "S", STRING_TYPE, "N", NUMBER_TYPE);
+  }
+
   public void testTransformationWithValidBasicTypePredicate() {
-    assertTypeEquals(NUMBER_TYPE,
-        transform(buildTypePredicate("number")));
+    testTTL(NUMBER_TYPE, "type('number')");
   }
 
   public void testTransformationWithBasicTypePredicateWithInvalidTypename() {
-    assertTypeEquals(UNKNOWN_TYPE,
-        transform(buildTypePredicate("foo")));
+    testTTL(UNKNOWN_TYPE, "type('foo')");
   }
 
   public void testTransformationWithSingleTypeVar() {
-    ImmutableMap<String, JSType> typeVars = ImmutableMap.of("T", STRING_TYPE);
-    assertTypeEquals(STRING_TYPE, transform(buildTypeVariable("T"), typeVars));
+    testTTL(STRING_TYPE, "S");
   }
 
   public void testTransformationWithMulipleTypeVars() {
-    ImmutableMap<String, JSType> typeVars = ImmutableMap.of(
-        "T", STRING_TYPE, "R", STRING_OBJECT_TYPE);
-    assertTypeEquals(STRING_TYPE,
-        transform(buildTypeVariable("T"), typeVars));
-    assertTypeEquals(STRING_OBJECT_TYPE,
-        transform(buildTypeVariable("R"), typeVars));
+    testTTL(STRING_TYPE, "S");
+    testTTL(NUMBER_TYPE, "N");
   }
 
   public void testTransformationWithValidUnionTypeOnlyVars() {
-    // Builds union(T, R) returns (number|string)
-    Node ttlExp = buildUnionType(
-        buildTypeVariable("T"), buildTypeVariable("R"));
-    ImmutableMap<String, JSType> typeVars =
-        ImmutableMap.of("T", NUMBER_TYPE, "R", STRING_TYPE);
-    assertTypeEquals(createUnionType(NUMBER_TYPE, STRING_TYPE),
-        transform(ttlExp, typeVars));
+    testTTL(union(NUMBER_TYPE, STRING_TYPE), "union(N, S)");
   }
 
+
   public void testTransformationWithValidUnionTypeOnlyTypePredicates() {
-    // Builds union(type('number'), type('string')) returns (number|string)
-    Node ttlExp = buildUnionType(
-        buildTypePredicate("number"), buildTypePredicate("string"));
-    assertTypeEquals(createUnionType(NUMBER_TYPE, STRING_TYPE),
-        transform(ttlExp));
+    testTTL(union(NUMBER_TYPE, STRING_TYPE),
+        "union(type('number'), type('string'))");
   }
 
   public void testTransformationWithValidUnionTypeMixed() {
-    // Builds union(T, type('number')) returns (number|string)
-    Node ttlExp = buildUnionType(
-        buildTypeVariable("T"), buildTypePredicate("number"));
-    ImmutableMap<String, JSType> typeVars = ImmutableMap.of("T", STRING_TYPE);
-    assertTypeEquals(createUnionType(NUMBER_TYPE, STRING_TYPE),
-        transform(ttlExp, typeVars));
+    testTTL(union(NUMBER_TYPE, STRING_TYPE), "union(S, type('number'))");
   }
 
   public void testTransformationWithUnknownParameter() {
-    // Builds union(T, type('number')) returns ? because T is not defined
-    Node ttlExp = buildUnionType(
-        buildTypeVariable("T"), buildTypePredicate("number"));
-    assertTypeEquals(UNKNOWN_TYPE, transform(ttlExp));
+    // Returns ? because foo is not defined
+    testTTL(UNKNOWN_TYPE, "union(foo, type('number'))");
   }
 
   public void testTransformationWithUnknownParameter2() {
-    // Builds union(T, type('foo')) returns ? because foo is not defined
-    Node ttlExp = buildUnionType(
-        buildTypeVariable("T"), buildTypePredicate("foo"));
-    ImmutableMap<String, JSType> typeVars = ImmutableMap.of("T", STRING_TYPE);
-    assertTypeEquals(UNKNOWN_TYPE, transform(ttlExp, typeVars));
+    // Returns ? because foo is not defined
+    testTTL(UNKNOWN_TYPE, "union(N, type('foo'))");
   }
 
   public void testTransformationWithNestedUnionInFirstParameter() {
-    // Builds union(union(T, type('null')), R) returns (number|null|string)
-    Node ttlExp = buildUnionType(
-        buildUnionType(buildTypeVariable("T"), buildTypePredicate("null")),
-        buildTypeVariable("R"));
-    ImmutableMap<String, JSType> typeVars =
-        ImmutableMap.of("T", NUMBER_TYPE, "R", STRING_TYPE);
-    assertTypeEquals(createUnionType(NUMBER_TYPE, NULL_TYPE, STRING_TYPE),
-        transform(ttlExp, typeVars));
+    testTTL(union(NUMBER_TYPE, NULL_TYPE, STRING_TYPE),
+        "union(union(N, type('null')), S)");
   }
 
   public void testTransformationWithNestedUnionInSecondParameter() {
-    // Builds union(union(T, (type('null'), R)) returns (number|null|string)
-    Node ttlExp = buildUnionType(
-        buildTypeVariable("T"),
-        buildUnionType(buildTypePredicate("null"), buildTypeVariable("R")));
-    ImmutableMap<String, JSType> typeVars =
-        ImmutableMap.of("T", NUMBER_TYPE, "R", STRING_TYPE);
-    assertTypeEquals(createUnionType(NUMBER_TYPE, NULL_TYPE, STRING_TYPE),
-        transform(ttlExp, typeVars));
+    testTTL(union(NUMBER_TYPE, NULL_TYPE, STRING_TYPE),
+        "union(N, union(type('null'), S))");
   }
 
   public void testTransformationWithRepeatedTypePredicate() {
-    // Builds union(type('number'), type('number')) returns number
-    Node ttlExp = buildUnionType(
-        buildTypePredicate("number"), buildTypePredicate("number"));
-    assertTypeEquals(NUMBER_TYPE, transform(ttlExp));
+    testTTL(NUMBER_TYPE, "union(type('number'), type('number'))");
   }
 
   public void testTransformationWithUndefinedTypeVar() {
-    assertTypeEquals(UNKNOWN_TYPE, transform(buildTypeVariable("T")));
+    testTTL(UNKNOWN_TYPE, "foo");
   }
 
   public void testTransformationWithTrueEqtypeConditional() {
-    // Builds cond(eq(T, T), type('string'), type('number')) returns string
-    Node ttlExp = buildConditional(
-        buildEqtypePredicate(buildTypeVariable("T"), buildTypeVariable("T")),
-        buildTypePredicate("string"),
-        buildTypePredicate("number"));
-    ImmutableMap<String, JSType> typeVars =
-        ImmutableMap.of("T", NUMBER_TYPE);
-    assertTypeEquals(STRING_TYPE, transform(ttlExp, typeVars));
+    testTTL(STRING_TYPE, "cond(eq(N, N), type('string'), type('number'))");
   }
 
   public void testTransformationWithFalseEqtypeConditional() {
-    // Builds cond(eq(T, R), type('string'), type('number')) returns number
-    Node ttlExp = buildConditional(
-        buildEqtypePredicate(buildTypeVariable("T"), buildTypeVariable("R")),
-        buildTypePredicate("string"),
-        buildTypePredicate("number"));
-    ImmutableMap<String, JSType> typeVars =
-        ImmutableMap.of("T", NUMBER_TYPE, "R", STRING_TYPE);
-    assertTypeEquals(NUMBER_TYPE, transform(ttlExp, typeVars));
+    testTTL(NUMBER_TYPE, "cond(eq(N, S), type('string'), type('number'))");
   }
 
   public void testTransformationWithTrueSubtypeConditional() {
-    // Builds cond( sub(type('Number'), type('Object')),
-    //              type('string'),
-    //              type('number'))
-    // returns string
-    Node ttlExp = buildConditional(
-        buildSubtypePredicate(buildTypePredicate("Number"),
-            buildTypePredicate("Object")),
-        buildTypePredicate("string"),
-        buildTypePredicate("number"));
-    assertTypeEquals(STRING_TYPE, transform(ttlExp));
+    testTTL(STRING_TYPE,
+        "cond( sub(type('Number'), type('Object')),"
+            + "type('string'),"
+            + "type('number'))");
   }
 
   public void testTransformationWithFalseSubtypeConditional() {
-    // Builds cond( sub(type('Number'), type('String')),
-    //              type('string'),
-    //              type('number'))
-    // returns number
-    Node ttlExp = buildConditional(
-        buildSubtypePredicate(buildTypePredicate("Number"),
-            buildTypePredicate("String")),
-        buildTypePredicate("string"),
-        buildTypePredicate("number"));
-    assertTypeEquals(NUMBER_TYPE, transform(ttlExp));
+    testTTL(NUMBER_TYPE,
+        "cond( sub(type('Number'), type('String')),"
+            + "type('string'),"
+            + "type('number'))");
   }
 
   public void testTransformationWithNestedExpressionInBooleanFirstParam() {
-    // Builds cond( eq( cond(eq(T, T), type('string'), type('number')),
-    //                  type('string')
-    //                ),
-    //              type('string'),
-    //              type('number'))
-    // returns string
-    Node nested = buildConditional(
-        buildEqtypePredicate(buildTypeVariable("T"), buildTypeVariable("T")),
-        buildTypePredicate("string"),
-        buildTypePredicate("number"));
-    Node ttlExp = buildConditional(
-        buildEqtypePredicate(nested, buildTypePredicate("string")),
-        buildTypePredicate("string"),
-        buildTypePredicate("number"));
-    ImmutableMap<String, JSType> typeVars =
-        ImmutableMap.of("T", NUMBER_TYPE);
-    assertTypeEquals(STRING_TYPE, transform(ttlExp, typeVars));
+    testTTL(STRING_TYPE,
+        "cond( eq( cond(eq(N, N), type('string'), type('number')),"
+            +     "type('string')"
+            +    "),"
+            + "type('string'),"
+            + "type('number'))");
   }
 
   public void testTransformationWithNestedExpressionInBooleanSecondParam() {
-    // Builds cond( eq( type('string'),
-    //                  cond(eq(T, T), type('string'), type('number'))
-    //                ),
-    //              type('string'),
-    //              type('number'))
-    // returns string
-    Node nested = buildConditional(
-        buildEqtypePredicate(buildTypeVariable("T"), buildTypeVariable("T")),
-        buildTypePredicate("string"),
-        buildTypePredicate("number"));
-    Node ttlExp = buildConditional(
-        buildEqtypePredicate(buildTypePredicate("string"), nested),
-        buildTypePredicate("string"),
-        buildTypePredicate("number"));
-    ImmutableMap<String, JSType> typeVars =
-        ImmutableMap.of("T", NUMBER_TYPE);
-    assertTypeEquals(STRING_TYPE, transform(ttlExp, typeVars));
+    testTTL(STRING_TYPE,
+        "cond( eq( type('string'),"
+        +          "cond(eq(N, N), type('string'), type('number'))"
+        +        "),"
+        +     "type('string'),"
+        +     "type('number'))");
   }
 
   public void testTransformationWithNestedExpressionInIfBranch() {
-    // Builds cond( eq(T, T),
-    //              cond(eq(T, R), type('string'), type('String')),
-    //              type('number'))
-    // returns String
-    Node nested = buildConditional(
-        buildEqtypePredicate(buildTypeVariable("T"), buildTypeVariable("R")),
-        buildTypePredicate("string"),
-        buildTypePredicate("String"));
-    Node ttlExp = buildConditional(
-        buildEqtypePredicate(buildTypeVariable("T"), buildTypeVariable("T")),
-        nested,
-        buildTypePredicate("number"));
-    ImmutableMap<String, JSType> typeVars =
-        ImmutableMap.of("T", NUMBER_TYPE);
-    assertTypeEquals(STRING_OBJECT_TYPE, transform(ttlExp, typeVars));
+    testTTL(STRING_OBJECT_TYPE,
+        "cond( eq(N, N),"
+        +     "cond(eq(N, S), type('string'), type('String')),"
+        +     "type('number'))");
   }
 
   public void testTransformationWithNestedExpressionInElseBranch() {
-    // Builds cond( eq(T, R),
-    //              type('number'),
-    //              cond(eq(T, R), type('string'), type('String')))
-    // returns String
-    Node nested = buildConditional(
-        buildEqtypePredicate(buildTypeVariable("T"), buildTypeVariable("R")),
-        buildTypePredicate("string"),
-        buildTypePredicate("String"));
-    Node ttlExp = buildConditional(
-        buildEqtypePredicate(buildTypeVariable("T"), buildTypeVariable("R")),
-        buildTypePredicate("number"),
-        nested);
-    ImmutableMap<String, JSType> typeVars =
-        ImmutableMap.of("T", NUMBER_TYPE);
-    assertTypeEquals(STRING_OBJECT_TYPE, transform(ttlExp, typeVars));
+    testTTL(STRING_OBJECT_TYPE,
+        "cond( eq(N, S),"
+        +     "type('number'),"
+        +     "cond(eq(N, S), type('string'), type('String')))");
   }
 
-  /**
-   * Builds a Node representing a type variable
-   * @param name The name of the variable
-   */
-  private Node buildTypeVariable(String name) {
-    return IR.name(name);
+  private JSType union(JSType... variants) {
+    return createUnionType(variants);
   }
 
-  /**
-   * Builds a Node representing a type expression such as type('typename')
-   * @param typename
-   */
-  private Node buildTypePredicate(String typename) {
-    return IR.call(
-        IR.name(Keywords.TYPE.name),
-        IR.name(typename));
-  }
-
-  private Node buildUnionType(Node... params) {
-    return IR.call(IR.name(Keywords.UNION.name),
-        params);
-   }
-
-  private Node buildEqtypePredicate(Node param0, Node param1) {
-    return buildBooleanPredicate(Keywords.EQTYPE, param0, param1);
-  }
-
-  private Node buildSubtypePredicate(Node param0, Node param1) {
-    return buildBooleanPredicate(Keywords.SUBTYPE, param0, param1);
-  }
-
-  private Node buildBooleanPredicate(Keywords predicate, Node param0,
-      Node param1) {
-    return IR.call(IR.name(predicate.name), param0, param1);
-  }
-
-  private Node buildConditional(Node boolExp, Node ifBranch, Node elseBranch) {
-    return IR.call(IR.name(Keywords.COND.name), boolExp, ifBranch, elseBranch);
-  }
-
-  private JSType transform(Node ttlExp) {
-    return transform(ttlExp, ImmutableMap.<String, JSType>of());
-  }
-
-  private JSType transform(Node ttlExp, ImmutableMap<String, JSType> typeVars) {
+  private void testTTL(JSType expectedType, String ttlExp) {
+    // TODO(lpino): Validate the AST using the TypeTransformationParser
+    Node ast = compiler.parseTestCode(ttlExp);
+    // Cut the SCRIPT and EXPR_RESULT nodes
+    ast = ast.getFirstChild().getFirstChild();
+    // Evaluate the type transformation
     TypeTransformation typeTransformation = new TypeTransformation(compiler);
-    return typeTransformation.eval(ttlExp, typeVars);
+    JSType resultType = typeTransformation.eval(ast, typeVars);
+    assertTypeEquals(expectedType, resultType);
   }
+
 }
