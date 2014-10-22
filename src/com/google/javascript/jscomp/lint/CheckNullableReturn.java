1c759000ff21dce6c34f3f619894ae94e252969d
blickly <blickly@google.com>
2014-10-21 17:37:52 -0700

    Allow CheckNullableReturn check to be run from outside the pass. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=78219496

--- src/com/google/javascript/jscomp/lint/CheckNullableReturn.java
+++ src/com/google/javascript/jscomp/lint/CheckNullableReturn.java
@@ -69,13 +69,18 @@ public class CheckNullableReturn implements HotSwapCompilerPass, NodeTraversal.C
     this.compiler = compiler;
   }
 
+  public static boolean hasReturnDeclaredNullable(Node n) {
+    return n.isBlock() && n.hasChildren() && isReturnTypeNullable(n.getParent())
+        && !hasSingleThrow(n);
+  }
+
   @Override
   public void visit(NodeTraversal t, Node n, Node parent) {
     // Do the checks when 'n' is the block node and 'parent' is the function
     // node, so that getControlFlowGraph will return the graph inside
     // the function, rather than the graph of the enclosing scope.
-    if (n.isBlock() && n.hasChildren() && isReturnTypeNullable(parent)
-        && !hasSingleThrow(n) && !canReturnNull(t.getControlFlowGraph())) {
+    if (hasReturnDeclaredNullable(n)
+        && !canReturnNull(t.getControlFlowGraph())) {
       String fnName = NodeUtil.getNearestFunctionName(parent);
       if (fnName != null && !fnName.isEmpty()) {
         compiler.report(t.makeError(parent, NULLABLE_RETURN_WITH_NAME, fnName));
@@ -122,7 +127,7 @@ public class CheckNullableReturn implements HotSwapCompilerPass, NodeTraversal.C
   /**
    * @return True if the given ControlFlowGraph could return null.
    */
-  private static boolean canReturnNull(ControlFlowGraph<Node> graph) {
+  public static boolean canReturnNull(ControlFlowGraph<Node> graph) {
     CheckPathsBetweenNodes<Node, ControlFlowGraph.Branch> test =
         new CheckPathsBetweenNodes<Node, ControlFlowGraph.Branch>(
             graph,
