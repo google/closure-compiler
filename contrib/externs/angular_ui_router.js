google/closure-compiler

commit aea6e011c13bb183d1bbc74b1f30d240e976ccb7

Automated g4 rollback of changelist 82444611.
*** Reason for rollback ***

Typo in ui.router.state.$state.GoOptions_ causing breakages.

*** Original change description ***

Merge pull request #744 from dlochrie/master

Added more properties/types to the angular_ui_router.js externs.

-------------
Created by MOE: http://code.google.com/p/moe-java

***
-------------
Created by MOE: http://code.google.com/p/moe-java
MOE_MIGRATED_REVID=82449441

CONTRIBUTORS
@@ -36,7 +36,6 @@ benjamin.j.mccann [gmail.com]
 bitwiseman [gmail.com] (Liam Newman)
 cpeisert [gmail.com] (Christopher Peisert)
 chadkillingsworth [missouristate.edu] (Chad Killingsworth)
-dlochrie (Daniel Lochrie)
 dunbarb2 [gmail.com]
 edo999 [gmail.com]
 eric.lemoine [gmail.com]

contrib/externs/angular_ui_router.js
@@ -36,6 +36,7 @@ var ui = {};
 ui.router = {};
 
 
+// TODO: Provide stronger types for properties on $state.
 /**
  * @typedef {{
  *   params: Object,
@@ -51,93 +52,6 @@ ui.router = {};
  * }}
  */
 ui.router.$state;
-
-
-/**
- * @type {ui.router.State}
- */
-ui.router.$state.current;
-
-
-/**
- * @type {Object}
- */
-ui.router.$state.params;
-
-
-/**
- * @type {?angular.$q.Promise}
- */
-ui.router.$state.transition;
-
-
-/**
- * @param {?string|Object=} stateOrName
- * @param {?string|Object=} context
- * @return {Object|Array}
- */
-ui.router.$state.get = function(stateOrName, context) {};
-
-
-/**
- * @param {string} to
- * @param {Object=} params
- * @param {(ui.router.state.$state.GoOptions_|Object)=} options
- * @returns {angular.$q.Promise}
- */
-ui.router.$state.go = function(to, params, options) {};
-
-
-/**
- * @param {?string|Object} stateOrName
- * @param {Object=} params
- * @param {Object=} options
- * @returns {string} compiled state url
- */
-ui.router.$state.href = function(stateOrName, params, options) {};
-
-
-/**
- * @param {?string} stateOrName
- * @param {Object=} params
- * @param {Object=} options
- */
-ui.router.$state.includes = function(stateOrName, params, options) {};
-
-
-/**
- * @param {?string|Object} stateOrName
- * @param {Object=} params
- * @param {Object=} options
- * @returns {boolean}
- */
-ui.router.$state.is = function(stateOrName, params, options) {};
-
-
-/**
- * @returns {angular.$q.Promise}
- */
-ui.router.$state.reload = function() {};
-
-
-/**
- * @param {string} to
- * @param {Object=} toParams
- * @param {Object=} options
- */
-ui.router.$state.transitionTo = function(to, toParams, options) {};
-
-
-/**
- * @typedef {{
- *   location: (boolean|string|undefined),
- *   inherit: (boolean|undefined),
- *   relative: (Object|undefined),
- *   notify: (boolean|undefined),
- *   reload: (boolean|undefined)
- * }}
- */
-ui.router.state.$state.GoOptions_;
 
 
 /**
@@ -223,4 +137,5 @@ ui.router.$stateProvider = function(
  * @param {Object} definition
  * @return {!ui.router.$stateProvider}
  */
-ui.router.$stateProvider.prototype.state = function(name, definition) {};
+ui.router.$stateProvider.prototype.state = function(
+    name, definition) {};

commit 2b8ffe141cb0b9491daf4002208b33668de5f7b3

Fix spelling errors which caused rollback in aea6e011c13bb183d1bbc74b1f30d240e976ccb7

CONTRIBUTORS
@@ -36,6 +36,7 @@ benjamin.j.mccann [gmail.com]
 bitwiseman [gmail.com] (Liam Newman)
 cpeisert [gmail.com] (Christopher Peisert)
 chadkillingsworth [missouristate.edu] (Chad Killingsworth)
+dlochrie (Daniel Lochrie)
 dunbarb2 [gmail.com]
 edo999 [gmail.com]
 eric.lemoine [gmail.com]

contrib/externs/angular_ui_router.js
@@ -36,7 +36,6 @@ var ui = {};
 ui.router = {};
 
 
-// TODO: Provide stronger types for properties on $state.
 /**
  * @typedef {{
  *   params: Object,
@@ -52,6 +51,93 @@ ui.router = {};
  * }}
  */
 ui.router.$state;
+
+
+/**
+ * @type {ui.router.State}
+ */
+ui.router.$state.current;
+
+
+/**
+ * @type {Object}
+ */
+ui.router.$state.params;
+
+
+/**
+ * @type {?angular.$q.Promise}
+ */
+ui.router.$state.transition;
+
+
+/**
+ * @param {?string|Object=} stateOrName
+ * @param {?string|Object=} context
+ * @return {Object|Array}
+ */
+ui.router.$state.get = function(stateOrName, context) {};
+
+
+/**
+ * @param {string} to
+ * @param {Object=} params
+ * @param {(ui.router.$state.GoOptions_|Object)=} options
+ * @returns {angular.$q.Promise}
+ */
+ui.router.$state.go = function(to, params, options) {};
+
+
+/**
+ * @param {?string|Object} stateOrName
+ * @param {Object=} params
+ * @param {Object=} options
+ * @returns {string} compiled state url
+ */
+ui.router.$state.href = function(stateOrName, params, options) {};
+
+
+/**
+ * @param {?string} stateOrName
+ * @param {Object=} params
+ * @param {Object=} options
+ */
+ui.router.$state.includes = function(stateOrName, params, options) {};
+
+
+/**
+ * @param {?string|Object} stateOrName
+ * @param {Object=} params
+ * @param {Object=} options
+ * @returns {boolean}
+ */
+ui.router.$state.is = function(stateOrName, params, options) {};
+
+
+/**
+ * @returns {angular.$q.Promise}
+ */
+ui.router.$state.reload = function() {};
+
+
+/**
+ * @param {string} to
+ * @param {Object=} toParams
+ * @param {Object=} options
+ */
+ui.router.$state.transitionTo = function(to, toParams, options) {};
+
+
+/**
+ * @typedef {{
+ *   location: (boolean|string|undefined),
+ *   inherit: (boolean|undefined),
+ *   relative: (Object|undefined),
+ *   notify: (boolean|undefined),
+ *   reload: (boolean|undefined)
+ * }}
+ */
+ui.router.$state.GoOptions_;
 
 
 /**
@@ -137,5 +223,4 @@ ui.router.$stateProvider = function(
  * @param {Object} definition
  * @return {!ui.router.$stateProvider}
  */
-ui.router.$stateProvider.prototype.state = function(
-    name, definition) {};
+ui.router.$stateProvider.prototype.state = function(name, definition) {};