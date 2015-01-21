df31880fecdc803b78538354a3028d6f1f3e5fa4
blickly <blickly@google.com>
2015-01-21 09:01:56 -0800

    Remove some more code that was part of "initial module specialization" ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=84439436

--- src/com/google/javascript/jscomp/AnalyzePrototypeProperties.java
+++ src/com/google/javascript/jscomp/AnalyzePrototypeProperties.java
@@ -604,17 +604,6 @@ class AnalyzePrototypeProperties implements CompilerPass {
     public JSModule getModule() {
       return module;
     }
-
-    public Node getFunctionNode() {
-      Node parent = nameNode.getParent();
-
-      if (parent.isFunction()) {
-        return parent;
-      } else {
-        // we are the name of a var node, so the function is name's second child
-        return nameNode.getChildAtIndex(1);
-      }
-    }
   }
 
   /**
