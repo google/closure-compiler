28553b64597ee27bb367530321db0ffe26210ba7
blickly <blickly@google.com>
2015-01-23 18:52:13 -0800

    Add a check that no implicitly nullable names are used in JSDoc
    Fixes #772
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=84671678

---
+++ src/com/google/javascript/jscomp/ImplicitNullabilityCheck.java
@@ -0,0 +1,99 @@
+/*
+ * Copyright 2015 The Closure Compiler Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.javascript.jscomp;
+
+import com.google.common.base.Predicates;
+import com.google.javascript.jscomp.NodeTraversal.AbstractPostOrderCallback;
+import com.google.javascript.rhino.JSDocInfo;
+import com.google.javascript.rhino.Node;
+import com.google.javascript.rhino.Token;
+import com.google.javascript.rhino.jstype.JSType;
+import com.google.javascript.rhino.jstype.JSTypeRegistry;
+
+/**
+ * Warn about types in JSDoc that are implicitly nullable.
+ * @author blickly@google.com (Ben Lickly)
+ */
+public final class ImplicitNullabilityCheck extends AbstractPostOrderCallback
+    implements CompilerPass {
+
+  static final DiagnosticType IMPLICITLY_NULLABLE_JSDOC =
+    DiagnosticType.warning(
+        "JSC_IMPLICITLY_NULLABLE_JSDOC",
+        "Name {0} in JSDoc is implicitly nullable.\n"
+        + "Please add a '!' to make it non-nullable,"
+        + " or a '?' to make it explicitly nullable.");
+
+  private final AbstractCompiler compiler;
+
+  public ImplicitNullabilityCheck(AbstractCompiler compiler) {
+    this.compiler = compiler;
+  }
+
+  @Override
+  public void process(Node externs, Node root) {
+    NodeTraversal.traverseRoots(compiler, this, externs, root);
+  }
+
+  /**
+   * Crawls the JSDoc of the given node to find any names in JSDoc
+   * that are implicitly null.
+   */
+  @Override
+  public void visit(final NodeTraversal t, final Node n, final Node p) {
+    final JSDocInfo info = n.getJSDocInfo();
+    if (info == null) {
+      return;
+    }
+    final JSTypeRegistry registry = compiler.getTypeRegistry();
+
+    for (Node typeRoot : info.getTypeNodes()) {
+      NodeUtil.visitPreOrder(typeRoot, new NodeUtil.Visitor() {
+        public void visit(Node node) {
+          if (!node.isString()) {
+            return;
+          }
+          Node parent = node.getParent();
+          if (parent != null) {
+            switch (parent.getType()) {
+              case Token.BANG:
+              case Token.QMARK:
+              case Token.THIS:  // The names inside function(this:Foo) and
+              case Token.NEW:   // function(new:Bar) are already non-null.
+                return;
+            }
+          }
+          String typeName = node.getString();
+          if (typeName.equals("null") || registry.getType(typeName) == null) {
+            return;
+          }
+          JSType type = registry.createFromTypeNodes(node, "[internal]", null);
+          if (type.isNullable()) {
+            reportWarning(t, node, typeName);
+          }
+        }
+      }, Predicates.<Node>alwaysTrue());
+    }
+  }
+
+  /**
+   * Reports an implicitly nullable name in JSDoc warning.
+   */
+  void reportWarning(NodeTraversal t, Node n, String name) {
+    compiler.report(t.makeError(n, IMPLICITLY_NULLABLE_JSDOC, name));
+  }
+}
---
+++ test/com/google/javascript/jscomp/ImplicitNullabilityCheckTest.java
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2015 The Closure Compiler Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.javascript.jscomp;
+
+/** Unit tests for {@link ImplicitNullabilityCheck}. */
+public class ImplicitNullabilityCheckTest extends CompilerTestCase {
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    super.enableTypeCheck(CheckLevel.WARNING);
+  }
+
+  @Override
+  public CompilerPass getProcessor(Compiler compiler) {
+    return new ImplicitNullabilityCheck(compiler);
+  }
+
+  @Override
+  protected int getNumRepetitions() {
+    return 1;
+  }
+
+  public void testExplicitJsdocDoesntWarn() {
+    noWarning("/** @type {boolean} */ var x;");
+    noWarning("/** @type {null} */ var x;");
+    noWarning("/** @type {!Object} */ var x;");
+    noWarning("/** @type {?Object} */ var x;");
+    noWarning("/** @type {function(new:Object)} */ function f(){}");
+    noWarning("/** @type {function(this:Object)} */ function f(){}");
+    noWarning("/** @typedef {!Object} */ var Obj; var /** Obj */ x;");
+  }
+
+  public void testJsdocPositions() {
+    warnImplicitlyNullable("/** @type {Object} */ var x;");
+    warnImplicitlyNullable("var /** Object */ x;");
+    warnImplicitlyNullable("/** @typedef {Object} */ var x;");
+    warnImplicitlyNullable("/** @param {Object} x */ function f(x){}");
+    warnImplicitlyNullable(
+        "/** @return {Object} */ function f(x){ return {}; }");
+  }
+
+  public void testNullableTypedef() {
+    warnImplicitlyNullable(
+        "/** @typedef {?number} */ var Num; var /** Num */ x;");
+  }
+
+  public void testUnkownTypenameDoesntWarn() {
+    testSame("/** @type {gibberish} */ var x;",
+        RhinoErrorReporter.TYPE_PARSE_ERROR);
+  }
+
+  private void warnImplicitlyNullable(String js) {
+    testSame(js, ImplicitNullabilityCheck.IMPLICITLY_NULLABLE_JSDOC);
+  }
+
+  private void noWarning(String js) {
+    testSame(js);
+  }
+}
