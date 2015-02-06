9fc02469522f77d08bd814f383c3dbf59977b39b
tbreisacher <tbreisacher@google.com>
2015-02-06 10:10:56 -0800

    Add some g3docs for the old and new type systems. ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=85726721

--- src/com/google/javascript/jscomp/SourceMapInput.java
+++ src/com/google/javascript/jscomp/SourceMapInput.java
@@ -32,7 +32,7 @@ public class SourceMapInput {
       Logger.getLogger(SourceMapInput.class.getName());
 
   private SourceFile sourceFile;
-  private volatile SourceMapConsumerV3 parsedSourceMap = null;
+  private SourceMapConsumerV3 parsedSourceMap = null;
 
   public SourceMapInput(SourceFile sourceFile) {
     this.sourceFile = sourceFile;
