5a853a60f93e09c446d458673bc7a2f6bb26742c
blickly <blickly@google.com>
2015-06-17 21:26:49 -0700

    Move TypeDeclarationsIRFactory into JsdocToEs6TypedConverter ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=96273054

--- src/com/google/javascript/jscomp/JsdocToEs6TypedConverter.java
+++ src/com/google/javascript/jscomp/JsdocToEs6TypedConverter.java
@@ -16,14 +16,34 @@
 
 package com.google.javascript.jscomp;
 
-import static com.google.javascript.jscomp.parsing.TypeDeclarationsIRFactory.convert;
+import static com.google.javascript.rhino.TypeDeclarationsIR.anyType;
+import static com.google.javascript.rhino.TypeDeclarationsIR.arrayType;
+import static com.google.javascript.rhino.TypeDeclarationsIR.booleanType;
+import static com.google.javascript.rhino.TypeDeclarationsIR.functionType;
+import static com.google.javascript.rhino.TypeDeclarationsIR.namedType;
+import static com.google.javascript.rhino.TypeDeclarationsIR.numberType;
+import static com.google.javascript.rhino.TypeDeclarationsIR.optionalParameter;
+import static com.google.javascript.rhino.TypeDeclarationsIR.parameterizedType;
+import static com.google.javascript.rhino.TypeDeclarationsIR.recordType;
+import static com.google.javascript.rhino.TypeDeclarationsIR.stringType;
+import static com.google.javascript.rhino.TypeDeclarationsIR.undefinedType;
+import static com.google.javascript.rhino.TypeDeclarationsIR.unionType;
 
+import com.google.common.base.Function;
+import com.google.common.base.Predicates;
+import com.google.common.collect.FluentIterable;
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Iterables;
 import com.google.javascript.jscomp.NodeTraversal.AbstractPostOrderCallback;
 import com.google.javascript.rhino.JSDocInfo;
 import com.google.javascript.rhino.JSTypeExpression;
 import com.google.javascript.rhino.Node;
 import com.google.javascript.rhino.Node.TypeDeclarationNode;
 import com.google.javascript.rhino.Token;
+
+import java.util.LinkedHashMap;
+
+import javax.annotation.Nullable;
 
 /**
  * Converts JS with types in jsdocs to an extended JS syntax that includes types.
@@ -91,10 +111,168 @@ public final class JsdocToEs6TypedConverter
   }
 
   private void setTypeExpression(Node n, JSTypeExpression type) {
-    TypeDeclarationNode node = convert(type);
+    TypeDeclarationNode node = TypeDeclarationsIRFactory.convert(type);
     if (node != null) {
       n.setDeclaredTypeExpression(node);
       compiler.reportCodeChange();
+    }
+  }
+
+  /**
+   * Converts root nodes of JSTypeExpressions into TypeDeclaration ASTs.
+   *
+   * @author alexeagle@google.com (Alex Eagle)
+   */
+  public static final class TypeDeclarationsIRFactory {
+
+    // Allow functional-style Iterables.transform over collections of nodes.
+    private static final Function<Node, TypeDeclarationNode> CONVERT_TYPE_NODE =
+        new Function<Node, TypeDeclarationNode>() {
+          @Override
+          public TypeDeclarationNode apply(Node node) {
+            return convertTypeNodeAST(node);
+          }
+        };
+
+    @Nullable
+    public static TypeDeclarationNode convert(@Nullable JSTypeExpression typeExpr) {
+      if (typeExpr == null) {
+        return null;
+      }
+      return convertTypeNodeAST(typeExpr.getRoot());
+    }
+
+    /**
+     * The root of a JSTypeExpression is very different from an AST node, even
+     * though we use the same Java class to represent them.
+     * This function converts root nodes of JSTypeExpressions into TypeDeclaration ASTs,
+     * to make them more similar to ordinary AST nodes.
+     *
+     * @return the root node of a TypeDeclaration AST, or null if no type is
+     *         available for the node.
+     */
+    // TODO(dimvar): Eventually, we want to just parse types to the new
+    // representation directly, and delete this function.
+    @Nullable
+    public static TypeDeclarationNode convertTypeNodeAST(Node n) {
+      int token = n.getType();
+      switch (token) {
+        case Token.STAR:
+        case Token.EMPTY: // for function types that don't declare a return type
+          return anyType();
+        case Token.VOID:
+          return undefinedType();
+        case Token.BANG:
+          // TODO(alexeagle): non-nullable is assumed to be the default
+          return convertTypeNodeAST(n.getFirstChild());
+        case Token.STRING:
+          String typeName = n.getString();
+          switch (typeName) {
+            case "boolean":
+              return booleanType();
+            case "number":
+              return numberType();
+            case "string":
+              return stringType();
+            case "null":
+            case "undefined":
+            case "void":
+              return null;
+            default:
+              TypeDeclarationNode root = namedType(typeName);
+              if (n.getChildCount() > 0 && n.getFirstChild().isBlock()) {
+                Node block = n.getFirstChild();
+                if ("Array".equals(typeName)) {
+                  return arrayType(convertTypeNodeAST(block.getFirstChild()));
+                }
+                return parameterizedType(root,
+                    Iterables.filter(
+                        Iterables.transform(block.children(), CONVERT_TYPE_NODE),
+                        Predicates.notNull()));
+              }
+              return root;
+          }
+        case Token.QMARK:
+          Node child = n.getFirstChild();
+          return child == null
+              ? anyType()
+              // For now, our ES6_TYPED language doesn't support nullable
+              // so we drop it before building the tree.
+              // : nullable(convertTypeNodeAST(child));
+              : convertTypeNodeAST(child);
+        case Token.LC:
+          LinkedHashMap<String, TypeDeclarationNode> properties = new LinkedHashMap<>();
+          for (Node field : n.getFirstChild().children()) {
+            boolean isFieldTypeDeclared = field.getType() == Token.COLON;
+            Node fieldNameNode = isFieldTypeDeclared ? field.getFirstChild() : field;
+            String fieldName = fieldNameNode.getString();
+            if (fieldName.startsWith("'") || fieldName.startsWith("\"")) {
+              fieldName = fieldName.substring(1, fieldName.length() - 1);
+            }
+            TypeDeclarationNode fieldType = isFieldTypeDeclared
+                ? convertTypeNodeAST(field.getLastChild()) : null;
+            properties.put(fieldName, fieldType);
+          }
+          return recordType(properties);
+        case Token.ELLIPSIS:
+          return arrayType(convertTypeNodeAST(n.getFirstChild()));
+        case Token.PIPE:
+          ImmutableList<TypeDeclarationNode> types = FluentIterable
+              .from(n.children()).transform(CONVERT_TYPE_NODE)
+              .filter(Predicates.notNull()).toList();
+          switch (types.size()) {
+            case 0:
+              return null;
+            case 1:
+              return types.get(0);
+            default:
+              return unionType(types);
+          }
+        case Token.FUNCTION:
+          Node returnType = anyType();
+          LinkedHashMap<String, TypeDeclarationNode> requiredParams = new LinkedHashMap<>();
+          LinkedHashMap<String, TypeDeclarationNode> optionalParams = new LinkedHashMap<>();
+          String restName = null;
+          TypeDeclarationNode restType = null;
+          for (Node child2 : n.children()) {
+            if (child2.isParamList()) {
+              int paramIdx = 1;
+              for (Node param : child2.children()) {
+                String paramName = "p" + paramIdx++;
+                if (param.getType() == Token.ELLIPSIS) {
+                  if (param.getFirstChild() != null) {
+                    restType = arrayType(convertTypeNodeAST(param.getFirstChild()));
+                  }
+                  restName = paramName;
+                } else {
+                  TypeDeclarationNode paramNode = convertTypeNodeAST(param);
+                  if (paramNode.getType() == Token.OPTIONAL_PARAMETER) {
+                    optionalParams.put(paramName,
+                        (TypeDeclarationNode) paramNode.removeFirstChild());
+                  } else {
+                    requiredParams.put(paramName, convertTypeNodeAST(param));
+                  }
+                }
+              }
+            } else if (child2.isNew()) {
+              // TODO(alexeagle): keep the constructor signatures on the tree, and emit them following
+              // the syntax in TypeScript 1.4 spec, section 3.7.8 Constructor Type Literals
+            } else if (child2.isThis()) {
+              // Not expressible in TypeScript syntax, so we omit them from the tree.
+              // They could be added as properties on the result node.
+            } else {
+              returnType = convertTypeNodeAST(child2);
+            }
+          }
+          return functionType(returnType, requiredParams, optionalParams, restName, restType);
+        case Token.EQUALS:
+          TypeDeclarationNode optionalParam = convertTypeNodeAST(n.getFirstChild());
+          return optionalParam == null ? null : optionalParameter(optionalParam);
+        default:
+          throw new IllegalArgumentException(
+              "Unsupported node type: " + Token.name(n.getType())
+                  + " " + n.toStringTree());
+      }
     }
   }
 }
--- test/com/google/javascript/jscomp/JsdocToEs6TypedConverterTest.java
+++ test/com/google/javascript/jscomp/JsdocToEs6TypedConverterTest.java
@@ -16,7 +16,38 @@
 
 package com.google.javascript.jscomp;
 
+import static com.google.common.truth.Truth.THROW_ASSERTION_ERROR;
+import static com.google.javascript.jscomp.testing.NodeSubject.assertNode;
+import static com.google.javascript.rhino.Token.ANY_TYPE;
+import static com.google.javascript.rhino.Token.ARRAY_TYPE;
+import static com.google.javascript.rhino.Token.BOOLEAN_TYPE;
+import static com.google.javascript.rhino.Token.FUNCTION_TYPE;
+import static com.google.javascript.rhino.Token.NAMED_TYPE;
+import static com.google.javascript.rhino.Token.NUMBER_TYPE;
+import static com.google.javascript.rhino.Token.PARAMETERIZED_TYPE;
+import static com.google.javascript.rhino.Token.RECORD_TYPE;
+import static com.google.javascript.rhino.Token.STRING_TYPE;
+import static com.google.javascript.rhino.TypeDeclarationsIR.anyType;
+import static com.google.javascript.rhino.TypeDeclarationsIR.arrayType;
+import static com.google.javascript.rhino.TypeDeclarationsIR.booleanType;
+import static com.google.javascript.rhino.TypeDeclarationsIR.functionType;
+import static com.google.javascript.rhino.TypeDeclarationsIR.namedType;
+import static com.google.javascript.rhino.TypeDeclarationsIR.numberType;
+import static com.google.javascript.rhino.TypeDeclarationsIR.parameterizedType;
+import static com.google.javascript.rhino.TypeDeclarationsIR.recordType;
+import static com.google.javascript.rhino.TypeDeclarationsIR.stringType;
+import static com.google.javascript.rhino.TypeDeclarationsIR.unionType;
+import static java.util.Arrays.asList;
+
 import com.google.javascript.jscomp.CompilerOptions.LanguageMode;
+import com.google.javascript.jscomp.JsdocToEs6TypedConverter.TypeDeclarationsIRFactory;
+import com.google.javascript.jscomp.parsing.JsDocInfoParser;
+import com.google.javascript.jscomp.testing.NodeSubject;
+import com.google.javascript.rhino.IR;
+import com.google.javascript.rhino.Node;
+import com.google.javascript.rhino.Node.TypeDeclarationNode;
+
+import java.util.LinkedHashMap;
 
 /**
  * Tests the conversion of closure-style type declarations in JSDoc
@@ -90,5 +121,135 @@ public final class JsdocToEs6TypedConverterTest extends CompilerTestCase {
   // so our transpilation is lossy here.
   public void testUndefinedType() throws Exception {
     test("/** @type {undefined} */ var n;", "var n;");
+  }
+
+  public void testConvertSimpleTypes() {
+    assertParseTypeAndConvert("?").hasType(ANY_TYPE);
+    assertParseTypeAndConvert("*").hasType(ANY_TYPE);
+    assertParseTypeAndConvert("boolean").hasType(BOOLEAN_TYPE);
+    assertParseTypeAndConvert("number").hasType(NUMBER_TYPE);
+    assertParseTypeAndConvert("string").hasType(STRING_TYPE);
+  }
+
+  public void testConvertNamedTypes() throws Exception {
+    assertParseTypeAndConvert("Window")
+        .isEqualTo(namedType("Window"));
+    assertParseTypeAndConvert("goog.ui.Menu")
+        .isEqualTo(namedType("goog.ui.Menu"));
+
+    assertNode(namedType("goog.ui.Menu"))
+        .isEqualTo(new TypeDeclarationNode(NAMED_TYPE,
+            IR.getprop(IR.getprop(IR.name("goog"), IR.string("ui")), IR.string("Menu"))));
+  }
+
+  public void testConvertTypeApplication() throws Exception {
+    assertParseTypeAndConvert("Array.<string>")
+        .isEqualTo(arrayType(stringType()));
+    assertParseTypeAndConvert("Object.<string, number>")
+        .isEqualTo(parameterizedType(namedType("Object"), asList(stringType(), numberType())));
+
+    assertNode(parameterizedType(namedType("Array"), asList(stringType())))
+        .isEqualTo(new TypeDeclarationNode(PARAMETERIZED_TYPE,
+            new TypeDeclarationNode(NAMED_TYPE, IR.name("Array")),
+            new TypeDeclarationNode(STRING_TYPE)));
+  }
+
+  public void testConvertTypeUnion() throws Exception {
+    assertParseTypeAndConvert("(number|boolean)")
+        .isEqualTo(unionType(numberType(), booleanType()));
+  }
+
+  public void testConvertRecordType() throws Exception {
+    LinkedHashMap<String, TypeDeclarationNode> properties = new LinkedHashMap<>();
+    properties.put("myNum", numberType());
+    properties.put("myObject", null);
+
+    assertParseTypeAndConvert("{myNum: number, myObject}")
+        .isEqualTo(recordType(properties));
+  }
+
+  public void testCreateRecordType() throws Exception {
+    LinkedHashMap<String, TypeDeclarationNode> properties = new LinkedHashMap<>();
+    properties.put("myNum", numberType());
+    properties.put("myObject", null);
+    TypeDeclarationNode node = recordType(properties);
+
+    Node prop1 = IR.stringKey("myNum");
+    prop1.addChildToFront(new TypeDeclarationNode(NUMBER_TYPE));
+    Node prop2 = IR.stringKey("myObject");
+
+    assertNode(node)
+        .isEqualTo(new TypeDeclarationNode(RECORD_TYPE, prop1, prop2));
+  }
+
+  public void testConvertRecordTypeWithTypeApplication() throws Exception {
+    Node prop1 = IR.stringKey("length");
+    assertParseTypeAndConvert("Array.<{length}>")
+        .isEqualTo(new TypeDeclarationNode(ARRAY_TYPE,
+            new TypeDeclarationNode(RECORD_TYPE, prop1)));
+  }
+
+  public void testConvertNullableType() throws Exception {
+    assertParseTypeAndConvert("?number")
+        .isEqualTo(numberType());
+  }
+
+  // TODO(alexeagle): change this test once we can capture nullability constraints in TypeScript
+  public void testConvertNonNullableType() throws Exception {
+    assertParseTypeAndConvert("!Object")
+        .isEqualTo(namedType("Object"));
+  }
+
+  public void testConvertFunctionType() throws Exception {
+    Node p1 = IR.name("p1");
+    p1.setDeclaredTypeExpression(stringType());
+    Node p2 = IR.name("p2");
+    p2.setDeclaredTypeExpression(booleanType());
+    assertParseTypeAndConvert("function(string, boolean)")
+        .isEqualTo(new TypeDeclarationNode(FUNCTION_TYPE, anyType(), p1, p2));
+  }
+
+  public void testConvertFunctionReturnType() throws Exception {
+    assertParseTypeAndConvert("function(): number")
+        .isEqualTo(new TypeDeclarationNode(FUNCTION_TYPE, numberType()));
+  }
+
+  public void testConvertFunctionThisType() throws Exception {
+    Node p1 = IR.name("p1");
+    p1.setDeclaredTypeExpression(stringType());
+    assertParseTypeAndConvert("function(this:goog.ui.Menu, string)")
+        .isEqualTo(new TypeDeclarationNode(FUNCTION_TYPE, anyType(), p1));
+  }
+
+  public void testConvertFunctionNewType() throws Exception {
+    Node p1 = IR.name("p1");
+    p1.setDeclaredTypeExpression(stringType());
+    assertParseTypeAndConvert("function(new:goog.ui.Menu, string)")
+        .isEqualTo(new TypeDeclarationNode(FUNCTION_TYPE, anyType(), p1));
+  }
+
+  public void testConvertVariableParameters() throws Exception {
+    Node p1 = IR.name("p1");
+    p1.setDeclaredTypeExpression(stringType());
+    Node p2 = IR.rest("p2");
+    p2.setDeclaredTypeExpression(arrayType(numberType()));
+    assertParseTypeAndConvert("function(string, ...number): number")
+        .isEqualTo(new TypeDeclarationNode(FUNCTION_TYPE, numberType(), p1, p2));
+  }
+
+  public void testConvertOptionalFunctionParameters() throws Exception {
+    LinkedHashMap<String, TypeDeclarationNode> requiredParams = new LinkedHashMap<>();
+    LinkedHashMap<String, TypeDeclarationNode> optionalParams = new LinkedHashMap<>();
+    optionalParams.put("p1", stringType());
+    optionalParams.put("p2", numberType());
+    assertParseTypeAndConvert("function(?string=, number=)")
+        .isEqualTo(functionType(anyType(), requiredParams, optionalParams, null, null));
+  }
+
+  private NodeSubject assertParseTypeAndConvert(final String typeExpr) {
+    Node oldAST = JsDocInfoParser.parseTypeString(typeExpr);
+    assertNotNull(typeExpr + " did not produce a parsed AST", oldAST);
+    return new NodeSubject(THROW_ASSERTION_ERROR,
+        TypeDeclarationsIRFactory.convertTypeNodeAST(oldAST));
   }
 }
