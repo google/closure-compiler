927eea21b218ec9dc403be2bef550ac28124e826
blickly <blickly@google.com>
2015-06-24 16:55:49 -0700

    Add Chad's new CheckJSDoc pass to the (per-file) linter binary ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=96825470

--- src/com/google/javascript/jscomp/CheckJSDoc.java
+++ src/com/google/javascript/jscomp/CheckJSDoc.java
@@ -26,7 +26,7 @@ import com.google.javascript.rhino.Token;
  *
  * @author chadkillingsworth@gmail.com (Chad Killingsworth)
  */
-final class CheckJSDoc implements CompilerPass {
+final class CheckJSDoc extends AbstractPostOrderCallback implements CompilerPass {
 
   public static final DiagnosticType MISPLACED_MSG_ANNOTATION =
       DiagnosticType.disabled("JSC_MISPLACED_MSG_ANNOTATION",
@@ -49,207 +49,202 @@ final class CheckJSDoc implements CompilerPass {
 
   @Override
   public void process(Node externs, Node root) {
-    NodeTraversal.traverse(compiler, externs, new ProcessNodes());
-    NodeTraversal.traverse(compiler, root, new ProcessNodes());
+    NodeTraversal.traverse(compiler, externs, this);
+    NodeTraversal.traverse(compiler, root, this);
   }
 
-  final class ProcessNodes extends AbstractPostOrderCallback {
+  @Override
+  public void visit(NodeTraversal t, Node n, Node parent) {
+    JSDocInfo info = n.getJSDocInfo();
+    validateTypeAnnotations(t, n, info);
+    validateFunctionJsDoc(t, n, info);
+    validateMsgJsDoc(t, n, info);
+    validateDeprecatedJsDoc(t, n, info);
+    validateNoCollapse(t, n, info);
+  }
 
-    @Override
-    public void visit(NodeTraversal t, Node n, Node parent) {
-      JSDocInfo info = n.getJSDocInfo();
-      validateTypeAnnotations(t, n, info);
-      validateFunctionJsDoc(t, n, info);
-      validateMsgJsDoc(t, n, info);
-      validateDeprecatedJsDoc(t, n, info);
-      validateNoCollapse(t, n, info);
+  /**
+   * Checks that deprecated annotations such as @expose are not present
+   */
+  private void validateDeprecatedJsDoc(NodeTraversal t, Node n,
+      JSDocInfo info) {
+    if (info != null && info.isExpose()) {
+      t.getCompiler().report(
+          t.makeError(n, ANNOTATION_DEPRECATED, "@expose",
+              "Use @nocollapse or @export instead."));
     }
+  }
 
-    /**
-     * Checks that deprecated annotations such as @expose are not present
-     */
-    private void validateDeprecatedJsDoc(NodeTraversal t, Node n,
-        JSDocInfo info) {
-      if (info != null && info.isExpose()) {
-        t.getCompiler().report(
-            t.makeError(n, ANNOTATION_DEPRECATED, "@expose",
-                "Use @nocollapse or @export instead."));
-      }
-    }
-
-    /**
-     * Warns when nocollapse annotations are pressent on nodes
-     * which are not eligible for property collapsing .
-     */
-    private void validateNoCollapse(NodeTraversal t, Node n,
-        JSDocInfo info) {
-      if (n.isFromExterns()) {
-        if (info != null && info.isNoCollapse()) {
-          // @nocollapse has no effect in externs
-          t.getCompiler().report(t.makeError(n, MISPLACED_ANNOTATION,
-              "@nocollapse", "This JSDoc has no effect in externs."));
-        }
-        return;
-      }
-      JSDocInfo bestInfo = NodeUtil.getBestJSDocInfo(n);
-      if (bestInfo != null && bestInfo.isNoCollapse()
-          && (n.getType() == Token.GETELEM || n.getType() == Token.GETPROP)
-          && NodeUtil.isPrototypeProperty(n)) {
+  /**
+   * Warns when nocollapse annotations are pressent on nodes
+   * which are not eligible for property collapsing .
+   */
+  private void validateNoCollapse(NodeTraversal t, Node n,
+      JSDocInfo info) {
+    if (n.isFromExterns()) {
+      if (info != null && info.isNoCollapse()) {
+        // @nocollapse has no effect in externs
         t.getCompiler().report(t.makeError(n, MISPLACED_ANNOTATION,
-            "@nocollapse", "This JSDoc has no effect on prototype"
-                + "properties."));
+            "@nocollapse", "This JSDoc has no effect in externs."));
       }
+      return;
+    }
+    JSDocInfo bestInfo = NodeUtil.getBestJSDocInfo(n);
+    if (bestInfo != null && bestInfo.isNoCollapse()
+        && (n.getType() == Token.GETELEM || n.getType() == Token.GETPROP)
+        && NodeUtil.isPrototypeProperty(n)) {
+      t.getCompiler().report(t.makeError(n, MISPLACED_ANNOTATION,
+          "@nocollapse", "This JSDoc has no effect on prototype"
+              + "properties."));
+    }
+  }
+
+  /**
+   * Checks that JSDoc intended for a function is actually attached to a
+   * function.
+   */
+  private void validateFunctionJsDoc(NodeTraversal t, Node n, JSDocInfo info) {
+    if (info == null) {
+      return;
     }
 
-    /**
-     * Checks that JSDoc intended for a function is actually attached to a
-     * function.
-     */
-    private void validateFunctionJsDoc(NodeTraversal t, Node n,
-        JSDocInfo info) {
-      if (info == null) {
-        return;
-      }
-
-      if (info.containsFunctionDeclaration() && !info.hasType()) {
-        // This JSDoc should be attached to a FUNCTION node, or an assignment
-        // with a function as the RHS, etc.
-        switch (n.getType()) {
-          case Token.FUNCTION:
-          case Token.VAR:
-          case Token.LET:
-          case Token.CONST:
-          case Token.GETTER_DEF:
-          case Token.SETTER_DEF:
-          case Token.MEMBER_FUNCTION_DEF:
-          case Token.STRING_KEY:
-          case Token.EXPORT:
-            return;
-          case Token.GETELEM:
-          case Token.GETPROP:
-            if (n.getFirstChild().isQualifiedName()) {
-              return;
-            }
-            break;
-          case Token.ASSIGN: {
-            // TODO(tbreisacher): Check that the RHS of the assignment is a
-            // function. Note that it can be a FUNCTION node, but it can also be
-            // a call to goog.abstractMethod, goog.functions.constant, etc.
+    if (info.containsFunctionDeclaration() && !info.hasType()) {
+      // This JSDoc should be attached to a FUNCTION node, or an assignment
+      // with a function as the RHS, etc.
+      switch (n.getType()) {
+        case Token.FUNCTION:
+        case Token.VAR:
+        case Token.LET:
+        case Token.CONST:
+        case Token.GETTER_DEF:
+        case Token.SETTER_DEF:
+        case Token.MEMBER_FUNCTION_DEF:
+        case Token.STRING_KEY:
+        case Token.EXPORT:
+          return;
+        case Token.GETELEM:
+        case Token.GETPROP:
+          if (n.getFirstChild().isQualifiedName()) {
             return;
           }
+          break;
+        case Token.ASSIGN: {
+          // TODO(tbreisacher): Check that the RHS of the assignment is a
+          // function. Note that it can be a FUNCTION node, but it can also be
+          // a call to goog.abstractMethod, goog.functions.constant, etc.
+          return;
         }
+      }
+      t.getCompiler().report(t.makeError(n, MISPLACED_ANNOTATION,
+          "function", "This JSDoc is not attached to a function node. "
+              + "Are you missing parentheses?"));
+    }
+  }
+
+  /**
+   * Checks that annotations for messages ({@code @desc}, {@code @hidden},
+   * and {@code @meaning})
+   * are in the proper place, namely on names starting with MSG_ which
+   * indicates they should be
+   * extracted for translation. A later pass checks that the right side is
+   * a call to goog.getMsg.
+   */
+  private void validateMsgJsDoc(NodeTraversal t, Node n,
+      JSDocInfo info) {
+    if (info == null) {
+      return;
+    }
+
+    if (info.getDescription() != null || info.isHidden() || info.getMeaning() != null) {
+      boolean descOkay = false;
+      switch (n.getType()) {
+        case Token.ASSIGN: {
+          Node lhs = n.getFirstChild();
+          if (lhs.isName()) {
+            descOkay = lhs.getString().startsWith("MSG_");
+          } else if (lhs.isQualifiedName()) {
+            descOkay = lhs.getLastChild().getString().startsWith("MSG_");
+          }
+          break;
+        }
+        case Token.VAR:
+        case Token.LET:
+        case Token.CONST:
+          descOkay = n.getFirstChild().getString().startsWith("MSG_");
+          break;
+        case Token.STRING_KEY:
+          descOkay = n.getString().startsWith("MSG_");
+          break;
+      }
+      if (!descOkay) {
+        t.getCompiler().report(t.makeError(n, MISPLACED_MSG_ANNOTATION));
+      }
+    }
+  }
+
+  /**
+   * Check that JSDoc with a {@code @type} annotation is in a valid place.
+   */
+  private void validateTypeAnnotations(NodeTraversal t, Node n, JSDocInfo info) {
+    if (info != null && info.hasType()) {
+      boolean valid = false;
+      switch (n.getType()) {
+        // Casts, variable declarations, and exports are valid.
+        case Token.CAST:
+        case Token.VAR:
+        case Token.LET:
+        case Token.CONST:
+        case Token.EXPORT:
+          valid = true;
+          break;
+        // Function declarations are valid
+        case Token.FUNCTION:
+          valid = NodeUtil.isFunctionDeclaration(n);
+          break;
+        // Object literal properties, catch declarations and variable
+        // initializers are valid.
+        case Token.NAME:
+        case Token.DEFAULT_VALUE:
+          Node parent = n.getParent();
+          switch (parent.getType()) {
+            case Token.STRING_KEY:
+            case Token.GETTER_DEF:
+            case Token.SETTER_DEF:
+            case Token.CATCH:
+            case Token.FUNCTION:
+            case Token.VAR:
+            case Token.LET:
+            case Token.CONST:
+            case Token.PARAM_LIST:
+              valid = true;
+              break;
+          }
+          break;
+        // Object literal properties are valid
+        case Token.STRING_KEY:
+        case Token.GETTER_DEF:
+        case Token.SETTER_DEF:
+          valid = true;
+          break;
+        // Property assignments are valid, if at the root of an expression.
+        case Token.ASSIGN:
+          valid = n.getParent().isExprResult()
+              && (n.getFirstChild().isGetProp() || n.getFirstChild().isGetElem());
+          break;
+        case Token.GETPROP:
+          valid = n.getParent().isExprResult() && n.isQualifiedName();
+          break;
+        case Token.CALL:
+          valid = info.isDefine();
+          break;
+        default:
+          break;
+      }
+
+      if (!valid) {
         t.getCompiler().report(t.makeError(n, MISPLACED_ANNOTATION,
-            "function", "This JSDoc is not attached to a function node. "
+            "type", "Type annotations are not allowed here. "
                 + "Are you missing parentheses?"));
-      }
-    }
-
-    /**
-     * Checks that annotations for messages ({@code @desc}, {@code @hidden},
-     * and {@code @meaning})
-     * are in the proper place, namely on names starting with MSG_ which
-     * indicates they should be
-     * extracted for translation. A later pass checks that the right side is
-     * a call to goog.getMsg.
-     */
-    private void validateMsgJsDoc(NodeTraversal t, Node n,
-        JSDocInfo info) {
-      if (info == null) {
-        return;
-      }
-
-      if (info.getDescription() != null || info.isHidden() || info.getMeaning() != null) {
-        boolean descOkay = false;
-        switch (n.getType()) {
-          case Token.ASSIGN: {
-            Node lhs = n.getFirstChild();
-            if (lhs.isName()) {
-              descOkay = lhs.getString().startsWith("MSG_");
-            } else if (lhs.isQualifiedName()) {
-              descOkay = lhs.getLastChild().getString().startsWith("MSG_");
-            }
-            break;
-          }
-          case Token.VAR:
-          case Token.LET:
-          case Token.CONST:
-            descOkay = n.getFirstChild().getString().startsWith("MSG_");
-            break;
-          case Token.STRING_KEY:
-            descOkay = n.getString().startsWith("MSG_");
-            break;
-        }
-        if (!descOkay) {
-          t.getCompiler().report(t.makeError(n, MISPLACED_MSG_ANNOTATION));
-        }
-      }
-    }
-
-    /**
-     * Check that JSDoc with a {@code @type} annotation is in a valid place.
-     */
-    private void validateTypeAnnotations(NodeTraversal t, Node n,
-        JSDocInfo info) {
-      if (info != null && info.hasType()) {
-        boolean valid = false;
-        switch (n.getType()) {
-          // Casts, variable declarations, and exports are valid.
-          case Token.CAST:
-          case Token.VAR:
-          case Token.LET:
-          case Token.CONST:
-          case Token.EXPORT:
-            valid = true;
-            break;
-          // Function declarations are valid
-          case Token.FUNCTION:
-            valid = NodeUtil.isFunctionDeclaration(n);
-            break;
-          // Object literal properties, catch declarations and variable
-          // initializers are valid.
-          case Token.NAME:
-          case Token.DEFAULT_VALUE:
-            Node parent = n.getParent();
-            switch (parent.getType()) {
-              case Token.STRING_KEY:
-              case Token.GETTER_DEF:
-              case Token.SETTER_DEF:
-              case Token.CATCH:
-              case Token.FUNCTION:
-              case Token.VAR:
-              case Token.LET:
-              case Token.CONST:
-              case Token.PARAM_LIST:
-                valid = true;
-                break;
-            }
-            break;
-          // Object literal properties are valid
-          case Token.STRING_KEY:
-          case Token.GETTER_DEF:
-          case Token.SETTER_DEF:
-            valid = true;
-            break;
-          // Property assignments are valid, if at the root of an expression.
-          case Token.ASSIGN:
-            valid = n.getParent().isExprResult()
-                && (n.getFirstChild().isGetProp() || n.getFirstChild().isGetElem());
-            break;
-          case Token.GETPROP:
-            valid = n.getParent().isExprResult() && n.isQualifiedName();
-            break;
-          case Token.CALL:
-            valid = info.isDefine();
-            break;
-          default:
-            break;
-        }
-
-        if (!valid) {
-          t.getCompiler().report(t.makeError(n, MISPLACED_ANNOTATION,
-              "type", "Type annotations are not allowed here. "
-                  + "Are you missing parentheses?"));
-        }
       }
     }
   }
