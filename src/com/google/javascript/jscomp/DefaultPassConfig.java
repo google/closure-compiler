a537d8f7d1d9854a55aed0ffd78e99c0a6ce69fe
Ben Lickly <blickly@google.com>
2015-07-21 16:01:46 -0700

    Move angularPass to before the later transpilation step ------------- Created by MOE: http://code.google.com/p/moe-java MOE_MIGRATED_REVID=98783196

--- src/com/google/javascript/jscomp/DefaultPassConfig.java
+++ src/com/google/javascript/jscomp/DefaultPassConfig.java
@@ -250,6 +250,10 @@ public final class DefaultPassConfig extends PassConfig {
       checks.add(jqueryAliases);
     }
 
+    if (options.angularPass && !options.skipNonTranspilationPasses) {
+      checks.add(angularPass);
+    }
+
     if (options.generateExports && !options.skipNonTranspilationPasses) {
       checks.add(generateExports);
     }
@@ -281,10 +285,6 @@ public final class DefaultPassConfig extends PassConfig {
     checks.add(convertStaticInheritance);
 
     // End of ES6 transpilation passes.
-
-    if (options.angularPass) {
-      checks.add(angularPass);
-    }
 
     if (options.exportTestFunctions) {
       checks.add(exportTestFunctions);
