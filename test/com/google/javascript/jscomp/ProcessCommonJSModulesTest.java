70857b76b6debcce4cd2b1e9befcc9ee574e0b10
blickly <blickly@google.com>
2015-07-22 09:54:11 -0700

    Fix failing test in ProcessCommonJSModulesTest
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=98842913

--- test/com/google/javascript/jscomp/ProcessCommonJSModulesTest.java
+++ test/com/google/javascript/jscomp/ProcessCommonJSModulesTest.java
@@ -177,19 +177,19 @@ public final class ProcessCommonJSModulesTest extends CompilerTestCase {
 
   public void testModuleExportsScope() {
     setFilename("test");
-    test(
+    testModules(
         "var foo = function (module) {module.exports = {};};" +
         "module.exports = foo;",
         "goog.provide('module$test');" +
         "var foo$$module$test=function(module){module.exports={}};" +
         "var module$test=foo$$module$test");
-    test(
+    testModules(
         "var foo = function () {var module = {};module.exports = {};};" +
         "module.exports = foo;",
         "goog.provide('module$test');" +
         "var foo$$module$test=function(){var module={};module.exports={}};" +
         "var module$test=foo$$module$test");
-    test(
+    testModules(
         "var foo = function () {if (true) var module = {};" +
         "module.exports = {};};" +
         "module.exports = foo;",
