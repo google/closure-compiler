5a1822b39964ad6a26922a824aa2f84622665087
blickly <blickly@google.com>
2015-07-28 21:27:41 -0700

    Automated g4 rollback of changelist 95649598.
    *** Reason for rollback ***
    
    Temporary rollback heuristic property renaming in order to do a compiler release.
    
    *** Original change description ***
    
    Reroll of heuristic property renaming removal now that release has been cut.
    
    ***
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=99345344

--- src/com/google/javascript/jscomp/DefaultPassConfig.java
+++ src/com/google/javascript/jscomp/DefaultPassConfig.java
@@ -659,7 +659,12 @@ public final class DefaultPassConfig extends PassConfig {
     // The mapped name anonymous function pass makes use of information that
     // the extract prototype member declarations pass removes so the former
     // happens before the latter.
-    if (options.extractPrototypeMemberDeclarations != ExtractPrototypeMemberDeclarationsMode.OFF) {
+    //
+    // Extracting prototype properties screws up the heuristic renaming
+    // policies, so never run it when those policies are requested.
+    if (options.extractPrototypeMemberDeclarations != ExtractPrototypeMemberDeclarationsMode.OFF
+        && (options.propertyRenaming != PropertyRenamingPolicy.HEURISTIC
+            && options.propertyRenaming != PropertyRenamingPolicy.AGGRESSIVE_HEURISTIC)) {
       passes.add(extractPrototypeMemberDeclarations);
     }
 
@@ -668,7 +673,7 @@ public final class DefaultPassConfig extends PassConfig {
       passes.add(ambiguateProperties);
     }
 
-    if (options.propertyRenaming == PropertyRenamingPolicy.ALL_UNQUOTED) {
+    if (options.propertyRenaming != PropertyRenamingPolicy.OFF) {
       passes.add(renameProperties);
     }
 
@@ -822,7 +827,7 @@ public final class DefaultPassConfig extends PassConfig {
   /** Creates several passes aimed at removing code. */
   private List<PassFactory> getCodeRemovingPasses() {
     List<PassFactory> passes = new ArrayList<>();
-    if (options.collapseObjectLiterals) {
+    if (options.collapseObjectLiterals && !isInliningForbidden()) {
       passes.add(collapseObjectLiterals);
     }
 
@@ -846,7 +851,7 @@ public final class DefaultPassConfig extends PassConfig {
       passes.add(removeUnusedPrototypeProperties);
     }
 
-    if (options.removeUnusedClassProperties) {
+    if (options.removeUnusedClassProperties && !isInliningForbidden()) {
       passes.add(removeUnusedClassProperties);
     }
 
@@ -1823,7 +1828,7 @@ public final class DefaultPassConfig extends PassConfig {
       new PassFactory("collapseProperties", true) {
         @Override
         protected CompilerPass create(AbstractCompiler compiler) {
-          return new CollapseProperties(compiler);
+          return new CollapseProperties(compiler, !isInliningForbidden());
         }
       };
 
@@ -1941,15 +1946,23 @@ public final class DefaultPassConfig extends PassConfig {
       new PassFactory("inlineVariables", false) {
         @Override
         protected CompilerPass create(AbstractCompiler compiler) {
-          InlineVariables.Mode mode;
-          if (options.inlineVariables) {
-            mode = InlineVariables.Mode.ALL;
-          } else if (options.inlineLocalVariables) {
-            mode = InlineVariables.Mode.LOCALS_ONLY;
+          if (isInliningForbidden()) {
+            // In old renaming schemes, inlining a variable can change whether
+            // or not a property is renamed. This is bad, and those old renaming
+            // schemes need to die.
+            return new ErrorPass(compiler, CANNOT_USE_PROTOTYPE_AND_VAR);
           } else {
-            throw new IllegalStateException("No variable inlining option set.");
+            InlineVariables.Mode mode;
+            if (options.inlineVariables) {
+              mode = InlineVariables.Mode.ALL;
+            } else if (options.inlineLocalVariables) {
+              mode = InlineVariables.Mode.LOCALS_ONLY;
+            } else {
+              throw new IllegalStateException("No variable inlining option set.");
+            }
+
+            return new InlineVariables(compiler, mode, true);
           }
-          return new InlineVariables(compiler, mode, true);
         }
       };
 
@@ -2090,12 +2103,13 @@ public final class DefaultPassConfig extends PassConfig {
       new PassFactory("inlineFunctions", false) {
         @Override
         protected CompilerPass create(AbstractCompiler compiler) {
+          boolean enableBlockInlining = !isInliningForbidden();
           return new InlineFunctions(
               compiler,
               compiler.getUniqueNameIdSupplier(),
               options.inlineFunctions,
               options.inlineLocalFunctions,
-              true,
+              enableBlockInlining,
               options.assumeStrictThis()
                   || options.getLanguageIn() == LanguageMode.ECMASCRIPT5_STRICT,
               options.assumeClosuresOnlyCaptureReferences,
@@ -2362,21 +2376,43 @@ public final class DefaultPassConfig extends PassConfig {
       new PassFactory("renameProperties", true) {
         @Override
         protected CompilerPass create(final AbstractCompiler compiler) {
-          Preconditions.checkState(options.propertyRenaming == PropertyRenamingPolicy.ALL_UNQUOTED);
           final VariableMap prevPropertyMap = options.inputPropertyMap;
           return new CompilerPass() {
             @Override
             public void process(Node externs, Node root) {
-              char[] reservedChars = options.anonymousFunctionNaming.getReservedCharacters();
-              RenameProperties rprop =
-                  new RenameProperties(
-                      compiler, options.generatePseudoNames, prevPropertyMap, reservedChars);
-              rprop.process(externs, root);
-              propertyMap = rprop.getPropertyMap();
+              propertyMap = runPropertyRenaming(compiler, prevPropertyMap, externs, root);
             }
           };
         }
       };
+
+  private VariableMap runPropertyRenaming(
+      AbstractCompiler compiler, VariableMap prevPropertyMap, Node externs, Node root) {
+    char[] reservedChars = options.anonymousFunctionNaming.getReservedCharacters();
+    switch (options.propertyRenaming) {
+      case HEURISTIC:
+        RenamePrototypes rproto =
+            new RenamePrototypes(compiler, false, reservedChars, prevPropertyMap);
+        rproto.process(externs, root);
+        return rproto.getPropertyMap();
+
+      case AGGRESSIVE_HEURISTIC:
+        RenamePrototypes rproto2 =
+            new RenamePrototypes(compiler, true, reservedChars, prevPropertyMap);
+        rproto2.process(externs, root);
+        return rproto2.getPropertyMap();
+
+      case ALL_UNQUOTED:
+        RenameProperties rprop =
+            new RenameProperties(
+                compiler, options.generatePseudoNames, prevPropertyMap, reservedChars);
+        rprop.process(externs, root);
+        return rprop.getPropertyMap();
+
+      default:
+        throw new IllegalStateException("Unrecognized property renaming policy");
+    }
+  }
 
   /** Renames variables. */
   private final PassFactory renameVars = new PassFactory("renameVars", true) {
@@ -2523,6 +2559,15 @@ public final class DefaultPassConfig extends PassConfig {
         return runInSerial(options.customPasses.get(executionTime));
       }
     };
+  }
+
+  /**
+   * All inlining is forbidden in heuristic renaming mode, because inlining
+   * will ruin the invariants that it depends on.
+   */
+  private boolean isInliningForbidden() {
+    return options.propertyRenaming == PropertyRenamingPolicy.HEURISTIC
+        || options.propertyRenaming == PropertyRenamingPolicy.AGGRESSIVE_HEURISTIC;
   }
 
   /** Create a compiler pass that runs the given passes in serial. */
