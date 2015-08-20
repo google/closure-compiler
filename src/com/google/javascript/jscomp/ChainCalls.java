72615cb658dca3776077f5a1f3c7372b2b76d20d
moz <moz@google.com>
2015-08-19 17:36:52 -0700

    Switch ChainCalls to use Es6SyntacticScopeCreator
    This does not cause a size regression (on maps at least).
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=101080236

--- src/com/google/javascript/jscomp/ChainCalls.java
+++ src/com/google/javascript/jscomp/ChainCalls.java
@@ -49,7 +49,7 @@ class ChainCalls implements CompilerPass {
     defFinder = new SimpleDefinitionFinder(compiler);
     defFinder.process(externs, root);
 
-    NodeTraversal.traverse(compiler, root, new GatherCallSites());
+    NodeTraversal.traverseEs6(compiler, root, new GatherCallSites());
 
     for (CallSite callSite : callSites) {
       callSite.parent.removeChild(callSite.n);
@@ -133,10 +133,9 @@ class ChainCalls implements CompilerPass {
           return;
         }
         if (!goodFunctionNodes.contains(rValue)) {
-          // TODO(moz): In ES6, t.getScope() might return a scope that is not
-          // a valid cfg root, might need something like t.getCfgScope().
-          new NodeTraversal(compiler, gatherFunctions).traverseInnerNode(
-              rValue, rValue.getParent(), t.getScope());
+          new NodeTraversal(compiler, gatherFunctions, new Es6SyntacticScopeCreator(compiler))
+              .traverseInnerNode(
+                  rValue, rValue.getParent(), t.getScope().getClosestHoistScope().getParent());
           if (badFunctionNodes.contains(rValue)) {
             return;
           }
