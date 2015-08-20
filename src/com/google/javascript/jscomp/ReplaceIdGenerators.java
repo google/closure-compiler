08fb955d838c598c40a92073a4d5b0adb0ed4e69
moz <moz@google.com>
2015-08-20 14:42:27 -0700

    Switch ReplaceIdGenerators to use Es6SyntacticScopeCreator
    This does not seem to introduce a code size regression.
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=101161493

--- src/com/google/javascript/jscomp/ReplaceIdGenerators.java
+++ src/com/google/javascript/jscomp/ReplaceIdGenerators.java
@@ -307,9 +307,9 @@ class ReplaceIdGenerators implements CompilerPass {
 
   @Override
   public void process(Node externs, Node root) {
-    NodeTraversal.traverse(compiler, root, new GatherGenerators());
+    NodeTraversal.traverseEs6(compiler, root, new GatherGenerators());
     if (!nameGenerators.isEmpty()) {
-      NodeTraversal.traverse(compiler, root, new ReplaceGenerators());
+      NodeTraversal.traverseEs6(compiler, root, new ReplaceGenerators());
     }
   }
 
@@ -326,7 +326,7 @@ class ReplaceIdGenerators implements CompilerPass {
         return;
       }
 
-      if (!t.inGlobalScope()
+      if (!t.getScope().getClosestHoistScope().isGlobal()
           && nameGenerator.getRenameStrategy() == RenameStrategy.INCONSISTENT) {
         // Warn about calls not in the global scope.
         compiler.report(t.makeError(n, NON_GLOBAL_ID_GENERATOR_CALL));
