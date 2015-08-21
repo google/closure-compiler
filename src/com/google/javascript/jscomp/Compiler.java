b8df41ab3b8154ec13d07a444c528d2f10db50f6
supersteves <steve@visokio.com>
2015-08-21 10:54:49 +0100

    Adding BEFORE_ALWAYS custom pass execution time
    Necessary to allow custom passes in WHITESPACE_ONLY mode, such as to
    process goog.module

--- src/com/google/javascript/jscomp/Compiler.java
+++ src/com/google/javascript/jscomp/Compiler.java
@@ -670,6 +670,8 @@ public class Compiler extends AbstractCompiler {
       return;
     }
 
+    runCustomPasses(CustomPassExecutionTime.BEFORE_ALWAYS);
+    
     if (!options.skipNonTranspilationPasses || options.lowerFromEs6()) {
       check();
       if (hasErrors()) {
