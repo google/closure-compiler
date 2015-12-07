11b2dc6cb756eab64037bb27188e0c1eac3b01e0
tbreisacher <tbreisacher@google.com>
2015-12-07 15:39:16 -0800

    Fix stupid typo in Linter (languageIn / languageOut) ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=109629478

--- src/com/google/javascript/jscomp/Linter.java
+++ src/com/google/javascript/jscomp/Linter.java
@@ -43,7 +43,7 @@ public class Linter {
     Compiler compiler = new Compiler();
     CompilerOptions options = new CompilerOptions();
     options.setLanguageIn(LanguageMode.ECMASCRIPT6_STRICT);
-    options.setLanguageIn(LanguageMode.ECMASCRIPT5);
+    options.setLanguageOut(LanguageMode.ECMASCRIPT5);
     options.setCodingConvention(new GoogleCodingConvention());
     options.setWarningLevel(DiagnosticGroups.MISSING_REQUIRE, CheckLevel.WARNING);
     options.setWarningLevel(DiagnosticGroups.EXTRA_REQUIRE, CheckLevel.WARNING);
