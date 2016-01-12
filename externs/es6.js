dbf021d49dfc65b9164ca3bf68a3cb1497198962
blickly <blickly@google.com>
2016-01-12 11:57:48 -0800

    Fix misformatted type annotation (parens around union types) ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=111973213

--- externs/es6.js
+++ externs/es6.js
@@ -1178,8 +1178,8 @@ Array.of = function(var_args) {};
 /**
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
  * @param {string|!Array<T>|!IArrayLike<T>|!Iterable<T>} arrayLike
- * @param {?function(this:S, string|T, number,
- *     string|!Array<T>|!IArrayLike<T>|!Iterable<T>): R} opt_mapFn
+ * @param {?function(this:S, (string|T), number,
+ *     (string|!Array<T>|!IArrayLike<T>|!Iterable<T>)): R} opt_mapFn
  * @param {S=} opt_this
  * @return {!Array<R>}
  * @template T,S,R
