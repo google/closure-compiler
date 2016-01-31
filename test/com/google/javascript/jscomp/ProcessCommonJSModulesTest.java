7d5df41b3b7f3271ab0b2e40f93d10977927ea2d
Chad Killingsworth <chadkillingsworth@gmail.com>
2016-01-31 16:28:10 -0600

    Force rewriting of ES6 or CommonJS modules which are imported, but don't export any symbols

--- test/com/google/javascript/jscomp/ProcessCommonJSModulesTest.java
+++ test/com/google/javascript/jscomp/ProcessCommonJSModulesTest.java
@@ -277,4 +277,14 @@ public final class ProcessCommonJSModulesTest extends CompilerTestCase {
             "  }",
             "};"));
   }
+
+  public void testRequireResultUnused() {
+    setFilename("test");
+    testModules(
+        "require('./other');",
+        LINE_JOINER.join(
+            "goog.provide('module$test');",
+            "goog.require('module$other');")
+    );
+  }
 }
