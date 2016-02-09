362bcbc03c3a2eb7efb6892b5e2338ede5f67685
tbreisacher <tbreisacher@google.com>
2016-02-09 12:37:54 -0800

    Re-enable the extraneous semicolon check ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=114248090

--- src/com/google/javascript/jscomp/LintPassConfig.java
+++ src/com/google/javascript/jscomp/LintPassConfig.java
@@ -18,6 +18,7 @@ package com.google.javascript.jscomp;
 import com.google.common.collect.ImmutableList;
 import com.google.javascript.jscomp.NodeTraversal.Callback;
 import com.google.javascript.jscomp.lint.CheckArguments;
+import com.google.javascript.jscomp.lint.CheckEmptyStatements;
 import com.google.javascript.jscomp.lint.CheckEnums;
 import com.google.javascript.jscomp.lint.CheckInterfaces;
 import com.google.javascript.jscomp.lint.CheckJSDocStyle;
@@ -106,6 +107,7 @@ class LintPassConfig extends PassConfig.PassConfigDelegate {
               compiler,
               ImmutableList.<Callback>of(
                   new CheckArguments(compiler),
+                  new CheckEmptyStatements(compiler),
                   new CheckEnums(compiler),
                   new CheckInterfaces(compiler),
                   new CheckPrototypeProperties(compiler),
