ce75181344d2d8f2cabf99fa09bcbe4c68b03561
ewinslow <ewinslow@google.com>
2016-02-17 14:15:15 -0800

    Make Array.from's optional mapFn actually optional, and non-nullable.
    Fixes https://github.com/google/closure-compiler/issues/1529
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=114898228

--- externs/es6.js
+++ externs/es6.js
@@ -1394,8 +1394,8 @@ Array.of = function(var_args) {};
 /**
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/from
  * @param {string|!IArrayLike<T>|!Iterable<T>} arrayLike
- * @param {?function(this:S, (string|T), number,
- *     (string|!IArrayLike<T>|!Iterable<T>)): R} opt_mapFn
+ * @param {function(this:S, (string|T), number,
+ *     (string|!IArrayLike<T>|!Iterable<T>)): R=} opt_mapFn
  * @param {S=} opt_this
  * @return {!Array<R>}
  * @template T,S,R
