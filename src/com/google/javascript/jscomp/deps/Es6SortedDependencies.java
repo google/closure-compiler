31ecfc7dd49176f32cb6356aba7b29a5fdb60cc1
Chad Killingsworth <chadkillingsworth@gmail.com>
2016-02-20 08:08:20 -0600

    Add missing import

--- src/com/google/javascript/jscomp/deps/Es6SortedDependencies.java
+++ src/com/google/javascript/jscomp/deps/Es6SortedDependencies.java
@@ -30,6 +30,7 @@ import java.util.Collections;
 import java.util.Deque;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
