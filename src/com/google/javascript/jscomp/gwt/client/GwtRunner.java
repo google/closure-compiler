302625a837b787b7165e1624fbaee39055f42f9a
Michael Zhou <zhoumotongxue008@gmail.com>
2016-03-02 22:36:21 -0500

    Add GwtRunner, the GWT equivalent of CommandlineRunner
    Changed the GWT ResourceLoader to include most of the built-in externs.
    
    Changed JsComp.gwt.xml to use the SingleScriptLinker so that the GWT
    compiler only outputs a single JavaScript file (jscomp.nocache.js).
    
    Changed JsCompGwtMain to GwtRunner with "JSCompiler" exported as the
    JavaScript name through JsInterop. Currently, GwtRunner supports a
    limited set of compiler flags, including compilationLevel,
    warningLevel, languageIn and languageOut among others.
    
    To use the GWT version of the compiler, do a Maven build and include
    "jscomp.nocache.js" from
    target/closure-compiler-gwt-1.0-SNAPSHOT/jscomp/, Eg.:
    
    <script src="jscomp.nocache.js"></script>
    <script>
      var output = new JSCompiler().compile("let x;", {
          compilationLevel: 'ADVANCED',
          warningLevel: 'VERBOSE',
          languageIn: 'ECMASCRIPT6_STRICT',
          languageOut: 'ECMASCRIPT5_STRICT'
      });
      console.log(output);
    </script>
    
    As an aside, the Maven build for closure-compiler-gwt is much faster
    now that I removed most dependencies to gwt-user.jar.

---
+++ src/com/google/javascript/jscomp/gwt/client/GwtRunner.java
@@ -0,0 +1,237 @@
+/*
+ * Copyright 2015 The Closure Compiler Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.javascript.jscomp.gwt.client;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableMap;
+import com.google.gwt.core.client.EntryPoint;
+import com.google.javascript.jscomp.CompilationLevel;
+import com.google.javascript.jscomp.Compiler;
+import com.google.javascript.jscomp.CompilerOptions;
+import com.google.javascript.jscomp.CompilerOptions.LanguageMode;
+import com.google.javascript.jscomp.ResourceLoader;
+import com.google.javascript.jscomp.SourceFile;
+import com.google.javascript.jscomp.WarningLevel;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import jsinterop.annotations.JsConstructor;
+import jsinterop.annotations.JsMethod;
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsProperty;
+import jsinterop.annotations.JsType;
+
+/**
+ * Runner for the GWT-compiled JSCompilerc
+ *
+ * @author moz@google.com (Michael Zhou)
+ */
+@JsType(namespace = JsPackage.GLOBAL, name = "JSCompiler")
+public final class GwtRunner implements EntryPoint {
+
+  // The core language externs in sorted order.
+  private static final List<String> BUILTIN_EXTERNS_LANG = ImmutableList.of(
+      "es3.js",
+      "es5.js",
+      "es6.js",
+      "es6_collections.js");
+
+  // The browser externs in sorted order.
+  private static final List<String> BUILTIN_EXTERNS_BROWSER_DEP_ORDER = ImmutableList.of(
+      "browser/intl.js",
+      "browser/w3c_event.js",
+      "browser/w3c_event3.js",
+      "browser/gecko_event.js",
+      "browser/ie_event.js",
+      "browser/webkit_event.js",
+      "browser/w3c_device_sensor_event.js",
+      "browser/w3c_dom1.js",
+      "browser/w3c_dom2.js",
+      "browser/w3c_dom3.js",
+      "browser/gecko_dom.js",
+      "browser/ie_dom.js",
+      "browser/webkit_dom.js",
+      "browser/w3c_css.js",
+      "browser/gecko_css.js",
+      "browser/ie_css.js",
+      "browser/webkit_css.js",
+      "browser/w3c_touch_event.js");
+
+  // Extra browser externs.
+  private static final List<String> BUILTIN_EXTERNS_BROWSER_EXTRA = ImmutableList.of(
+      "browser/fileapi.js",
+      "browser/html5.js",
+      "browser/page_visibility.js",
+      "browser/w3c_batterystatus.js",
+      "browser/w3c_range.js",
+      "browser/w3c_xml.js");
+
+  private static final Map<String, CompilationLevel> COMPILATION_LEVEL_MAP =
+      ImmutableMap.of(
+          "WHITESPACE_ONLY",
+          CompilationLevel.WHITESPACE_ONLY,
+          "SIMPLE",
+          CompilationLevel.SIMPLE_OPTIMIZATIONS,
+          "SIMPLE_OPTIMIZATIONS",
+          CompilationLevel.SIMPLE_OPTIMIZATIONS,
+          "ADVANCED",
+          CompilationLevel.ADVANCED_OPTIMIZATIONS,
+          "ADVANCED_OPTIMIZATIONS",
+          CompilationLevel.ADVANCED_OPTIMIZATIONS);
+
+  private static final Map<String, WarningLevel> WARNING_LEVEL_MAP =
+      ImmutableMap.of(
+          "QUIET",
+          WarningLevel.QUIET,
+          "DEFAULT",
+          WarningLevel.DEFAULT,
+          "VERBOSE",
+          WarningLevel.VERBOSE);
+
+  private final Compiler compiler;
+  private List<SourceFile> builtInExterns;
+
+  public GwtRunner() {
+    compiler = new Compiler();
+    compiler.disableThreads();
+  }
+
+  @JsType(namespace = JsPackage.GLOBAL, name = "Object", isNative = true)
+  private interface Flags {
+    @JsProperty
+    String getCompilationLevel();
+
+    @JsProperty
+    String getWarningLevel();
+
+    @JsProperty
+    String getLanguageIn();
+
+    @JsProperty
+    String getLanguageOut();
+
+    @JsProperty
+    boolean getChecksOnly();
+
+    @JsProperty
+    boolean getNewTypeInf();
+
+    @JsProperty
+    boolean getPreserveTypeAnnotations();
+
+    @JsProperty
+    boolean getRewritePolyfills();
+  }
+
+  private static void applyDefaultOptions(CompilerOptions options) {
+    CompilationLevel.SIMPLE_OPTIMIZATIONS.setOptionsForCompilationLevel(options);
+    WarningLevel.DEFAULT.setOptionsForWarningLevel(options);
+    options.setLanguageIn(LanguageMode.ECMASCRIPT6);
+    options.setLanguageOut(LanguageMode.ECMASCRIPT3);
+    options.setPrettyPrint(true);
+  }
+
+  // TODO(moz): Handle most compiler flags and report errors on invalid flags / values.
+  private static void applyOptionsFromFlags(CompilerOptions options, Flags flags) {
+    if (flags == null) {
+      return;
+    }
+
+    if (flags.getCompilationLevel() != null) {
+      CompilationLevel level =
+          COMPILATION_LEVEL_MAP.get(flags.getCompilationLevel().toUpperCase());
+      if (level != null) {
+        level.setOptionsForCompilationLevel(options);
+      }
+    }
+
+    if (flags.getWarningLevel() != null) {
+      WarningLevel level = WARNING_LEVEL_MAP.get(flags.getWarningLevel().toUpperCase());
+      if (level != null) {
+        level.setOptionsForWarningLevel(options);
+      }
+    }
+
+    if (flags.getLanguageIn() != null) {
+      CompilerOptions.LanguageMode languageIn =
+          CompilerOptions.LanguageMode.fromString(flags.getLanguageIn());
+      if (languageIn != null) {
+        options.setLanguageIn(languageIn);
+      }
+    }
+
+    if (flags.getLanguageOut() != null) {
+      CompilerOptions.LanguageMode languageOut =
+          CompilerOptions.LanguageMode.fromString(flags.getLanguageOut());
+      if (languageOut != null) {
+        options.setLanguageOut(languageOut);
+      }
+    }
+
+    options.setChecksOnly(flags.getChecksOnly());
+    options.setNewTypeInference(flags.getNewTypeInf());
+    options.setPreserveTypeAnnotations(flags.getPreserveTypeAnnotations());
+    options.setRewritePolyfills(flags.getRewritePolyfills());
+  }
+
+  private static void disableUnsupportedOptions(CompilerOptions options) {
+    options.getDependencyOptions().setDependencySorting(false);
+    options.getDependencyOptions().setEs6ModuleOrder(false);
+  }
+
+  // TODO(moz): Handle custom environment with CompilerOptions.Environment.
+  private static List<SourceFile> loadBuiltInExterns() {
+    List<SourceFile> builtInExterns = new ArrayList<>();
+    String pathPrefix = "externs/";
+    for (String key : BUILTIN_EXTERNS_LANG) {
+      String path = pathPrefix + key;
+      builtInExterns.add(
+          SourceFile.fromCode(path, ResourceLoader.loadTextResource(GwtRunner.class, path)));
+    }
+
+    for (String key : BUILTIN_EXTERNS_BROWSER_DEP_ORDER) {
+      String path = pathPrefix + key;
+      builtInExterns.add(
+          SourceFile.fromCode(path, ResourceLoader.loadTextResource(GwtRunner.class, path)));
+    }
+
+    for (String key : BUILTIN_EXTERNS_BROWSER_EXTRA) {
+      String path = pathPrefix + key;
+      builtInExterns.add(
+          SourceFile.fromCode(path, ResourceLoader.loadTextResource(GwtRunner.class, path)));
+    }
+    return builtInExterns;
+  }
+
+  public String compile(String js, Flags flags) {
+    CompilerOptions options = new CompilerOptions();
+    applyDefaultOptions(options);
+    applyOptionsFromFlags(options, flags);
+    disableUnsupportedOptions(options);
+    if (builtInExterns == null) {
+      builtInExterns = loadBuiltInExterns();
+    }
+    SourceFile src = SourceFile.fromCode("src.js", js);
+    compiler.compile(builtInExterns, ImmutableList.of(src), options);
+    return compiler.toSource();
+  }
+
+  @Override
+  public void onModuleLoad() {}
+}
--- src/com/google/javascript/jscomp/gwt/super/com/google/javascript/jscomp/ResourceLoader.java
+++ src/com/google/javascript/jscomp/gwt/super/com/google/javascript/jscomp/ResourceLoader.java
@@ -24,10 +24,94 @@ import com.google.gwt.resources.client.TextResource;
  * GWT compatible replacement for {@code ResourceLoader}
  *
  */
-final class ResourceLoader {
+public final class ResourceLoader {
   static interface Libraries extends ClientBundle {
     Libraries INSTANCE = GWT.create(Libraries.class);
 
+    @Source("externs/es3.js")
+    TextResource es3();
+    
+    @Source("externs/es5.js")
+    TextResource es5();
+    
+    @Source("externs/es6.js")
+    TextResource es6();
+
+    @Source("externs/es6_collections.js")
+    TextResource es6Collections();
+
+    @Source("externs/browser/intl.js")
+    TextResource intl();
+    
+    @Source("externs/browser/w3c_event.js")
+    TextResource w3cEvent();
+    
+    @Source("externs/browser/w3c_event3.js")
+    TextResource w3cEvent3();
+    
+    @Source("externs/browser/gecko_event.js")
+    TextResource geckoEvent();
+    
+    @Source("externs/browser/ie_event.js")
+    TextResource ieEvent();
+    
+    @Source("externs/browser/webkit_event.js")
+    TextResource webkitEvent();
+    
+    @Source("externs/browser/w3c_device_sensor_event.js")
+    TextResource w3cDeviceSensorEvent();
+    
+    @Source("externs/browser/w3c_dom1.js")
+    TextResource w3cDom1();
+    
+    @Source("externs/browser/w3c_dom2.js")
+    TextResource w3cDom2();
+    
+    @Source("externs/browser/w3c_dom3.js")
+    TextResource w3cDom3();
+
+    @Source("externs/browser/gecko_dom.js")
+    TextResource geckoDom();
+    
+    @Source("externs/browser/ie_dom.js")
+    TextResource ieDom();
+    
+    @Source("externs/browser/webkit_dom.js")
+    TextResource webkitDom();
+    
+    @Source("externs/browser/w3c_css.js")
+    TextResource w3cCss();
+    
+    @Source("externs/browser/gecko_css.js")
+    TextResource geckoCss();
+    
+    @Source("externs/browser/ie_css.js")
+    TextResource ieCss();
+    
+    @Source("externs/browser/webkit_css.js")
+    TextResource webkitCss();
+    
+    @Source("externs/browser/w3c_touch_event.js")
+    TextResource w3cTouchEvent();
+    
+    @Source("externs/browser/fileapi.js")
+    TextResource fileapi();
+    
+    @Source("externs/browser/html5.js")
+    TextResource html5();
+    
+    @Source("externs/browser/page_visibility.js")
+    TextResource pageVisibility();
+    
+    @Source("externs/browser/w3c_batterystatus.js")
+    TextResource w3cBatterystatus();
+    
+    @Source("externs/browser/w3c_range.js")
+    TextResource w3cRange();
+    
+    @Source("externs/browser/w3c_xml.js")
+    TextResource w3cXml();
+    
     @Source("js/base.js")
     TextResource base();
 
@@ -38,8 +122,64 @@ final class ResourceLoader {
     TextResource runtimeTypeCheck();
   }
 
-  static String loadTextResource(Class<?> clazz, String path) {
+  public static String loadTextResource(Class<?> clazz, String path) {
     switch (path) {
+      case "externs/es3.js":
+        return Libraries.INSTANCE.es3().getText();
+      case "externs/es5.js":
+        return Libraries.INSTANCE.es5().getText();
+      case "externs/es6.js":
+        return Libraries.INSTANCE.es6().getText();
+      case "externs/es6_collections.js":
+        return Libraries.INSTANCE.es6Collections().getText();
+      case "externs/browser/intl.js":
+        return Libraries.INSTANCE.intl().getText();
+      case "externs/browser/w3c_event.js":
+        return Libraries.INSTANCE.w3cEvent().getText();
+      case "externs/browser/w3c_event3.js":
+        return Libraries.INSTANCE.w3cEvent3().getText();
+      case "externs/browser/gecko_event.js":
+        return Libraries.INSTANCE.geckoEvent().getText();
+      case "externs/browser/ie_event.js":
+        return Libraries.INSTANCE.ieEvent().getText();
+      case "externs/browser/webkit_event.js":
+        return Libraries.INSTANCE.webkitEvent().getText();
+      case "externs/browser/w3c_device_sensor_event.js":
+        return Libraries.INSTANCE.w3cDeviceSensorEvent().getText();
+      case "externs/browser/w3c_dom1.js":
+        return Libraries.INSTANCE.w3cDom1().getText();
+      case "externs/browser/w3c_dom2.js":
+        return Libraries.INSTANCE.w3cDom2().getText();
+      case "externs/browser/w3c_dom3.js":
+        return Libraries.INSTANCE.w3cDom3().getText();
+      case "externs/browser/gecko_dom.js":
+        return Libraries.INSTANCE.geckoDom().getText();
+      case "externs/browser/ie_dom.js":
+        return Libraries.INSTANCE.ieDom().getText();
+      case "externs/browser/webkit_dom.js":
+        return Libraries.INSTANCE.webkitDom().getText();
+      case "externs/browser/w3c_css.js":
+        return Libraries.INSTANCE.w3cCss().getText();
+      case "externs/browser/gecko_css.js":
+        return Libraries.INSTANCE.geckoCss().getText();
+      case "externs/browser/ie_css.js":
+        return Libraries.INSTANCE.ieCss().getText();
+      case "externs/browser/webkit_css.js":
+        return Libraries.INSTANCE.webkitCss().getText();
+      case "externs/browser/w3c_touch_event.js":
+        return Libraries.INSTANCE.w3cTouchEvent().getText();
+      case "externs/browser/fileapi.js":
+        return Libraries.INSTANCE.fileapi().getText();
+      case "externs/browser/html5.js":
+        return Libraries.INSTANCE.html5().getText();
+      case "externs/browser/page_visibility.js":
+        return Libraries.INSTANCE.pageVisibility().getText();
+      case "externs/browser/w3c_batterystatus.js":
+        return Libraries.INSTANCE.w3cBatterystatus().getText();
+      case "externs/browser/w3c_range.js":
+        return Libraries.INSTANCE.w3cRange().getText();
+      case "externs/browser/w3c_xml.js":
+        return Libraries.INSTANCE.w3cXml().getText();
       case "js/base.js":
         return Libraries.INSTANCE.base().getText();
       case "js/es6_runtime.js":
