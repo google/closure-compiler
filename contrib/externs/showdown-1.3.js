f402ec30981a5614369dd2801f1760d0d19a93e6
Ben Schwartz <benschw@gmail.com>
2016-03-11 16:24:51 -0600

    adding showdown extern

---
+++ contrib/externs/showdown-1.3.js
@@ -0,0 +1,18 @@
+
+
+/**
+ * @const
+ */
+var showdown = {};
+
+/**
+ * @param {*} opts
+ * @constructor
+ */
+showdown.Converter = function(opts) {};
+
+/**
+ * @param {string} txt
+ * @return (string)
+ */
+showdown.Converter.prototype.makeHtml = function(txt) {};
