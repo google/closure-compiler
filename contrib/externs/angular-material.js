5e901a852eddd4d304f5333c45ef70bf13b03b16
jelbourn <jelbourn@google.com>
2016-03-22 11:27:57 -0700

    Add angular-material externs for $mdDialog callbacks. ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=117844258

--- contrib/externs/angular-material.js
+++ contrib/externs/angular-material.js
@@ -94,7 +94,10 @@ md.$dialog = function() {};
  *   locals: (Object|undefined),
  *   resolve: (Object|undefined),
  *   controllerAs: (string|undefined),
- *   parent: (angular.JQLite|Element|undefined)
+ *   parent: (angular.JQLite|Element|undefined),
+ *   onShowing: (Function|undefined),
+ *   onComplete: (Function|undefined),
+ *   onRemoving: (Function|undefined)
  * }}
  */
 md.$dialog.options;
