713759b4f55e6bdccb6cd43ea0ad3e202c3440b2
blickly <blickly@google.com>
2016-04-04 09:45:38 -0700

    Fix error message for new goog.module check ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=118948841

--- src/com/google/javascript/jscomp/ClosureCheckModule.java
+++ src/com/google/javascript/jscomp/ClosureCheckModule.java
@@ -66,7 +66,7 @@ public final class ClosureCheckModule implements Callback, HotSwapCompilerPass {
   static final DiagnosticType REFERENCE_TO_SHORT_IMPORT_BY_LONG_NAME =
       DiagnosticType.disabled(
           "JSC_REFERENCE_TO_SHORT_IMPORT_BY_LONG_NAME",
-          "Reference fully qualified import name ''{0}''. Please use the short name instead.");
+          "Reference to fully qualified import name ''{0}''. Please use the short name instead.");
 
   static final DiagnosticType REQUIRE_NOT_AT_TOP_LEVEL =
       DiagnosticType.error(
@@ -139,7 +139,7 @@ public final class ClosureCheckModule implements Callback, HotSwapCompilerPass {
         if (currentModuleName != null && n.matchesQualifiedName(currentModuleName)) {
           t.report(n, REFERENCE_TO_MODULE_GLOBAL_NAME);
         } else if (shortRequiredNamespaces.contains(n.getQualifiedName())) {
-          t.report(n, REFERENCE_TO_SHORT_IMPORT_BY_LONG_NAME);
+          t.report(n, REFERENCE_TO_SHORT_IMPORT_BY_LONG_NAME, n.getQualifiedName());
         }
         break;
       case Token.SCRIPT:
