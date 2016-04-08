e3a271536dc27e6507d170f45fa8b70b8d8351a4
Michael Zhou <zhoumotongxue008@gmail.com>
2016-04-08 14:04:25 -0400

    Add USELESS_USE_STRICT_DIRECTIVE to the "lintChecks" DiagnosticGroup
    I'm inclined to just remove that warning, but let's make it
    suppressible at least.
    
    Part of #1620.

--- src/com/google/javascript/jscomp/DiagnosticGroups.java
+++ src/com/google/javascript/jscomp/DiagnosticGroups.java
@@ -508,6 +508,7 @@ public class DiagnosticGroups {
           ClosureCheckModule.LET_GOOG_REQUIRE,
           ClosureCheckModule.REFERENCE_TO_SHORT_IMPORT_BY_LONG_NAME,
           ClosureCheckModule.REFERENCE_TO_SHORT_IMPORT_BY_LONG_NAME_INCLUDING_SHORT_NAME,
+          ClosureRewriteModule.USELESS_USE_STRICT_DIRECTIVE,
           RhinoErrorReporter.JSDOC_MISSING_BRACES_WARNING,
           RhinoErrorReporter.JSDOC_MISSING_TYPE_WARNING,
           RhinoErrorReporter.TOO_MANY_TEMPLATE_PARAMS,
