4ff1db85ab4dea06e76dff93227515ca95eea1de
Damien Engels <engelsdamien@gmail.com>
2016-04-27 16:36:36 -0700

    Fix #1262: Add correct implementation of NumberToString

--- src/com/google/javascript/jscomp/parsing/IRFactory.java
+++ src/com/google/javascript/jscomp/parsing/IRFactory.java
@@ -147,6 +147,7 @@ import com.google.javascript.rhino.Node.TypeDeclarationNode;
 import com.google.javascript.rhino.StaticSourceFile;
 import com.google.javascript.rhino.Token;
 import com.google.javascript.rhino.TokenStream;
+import com.google.javascript.rhino.dtoa.DToA;
 
 import java.util.ArrayList;
 import java.util.HashSet;
@@ -783,24 +784,13 @@ class IRFactory {
 
   Node transformNumberAsString(LiteralToken token) {
     double value = normalizeNumber(token);
-    Node irNode = newStringNode(getStringValue(value));
+    Node irNode = newStringNode(DToA.numberToString(value));
     JSDocInfo jsDocInfo = handleJsDoc(token);
     if (jsDocInfo != null) {
       irNode.setJSDocInfo(jsDocInfo);
     }
     setSourceInfo(irNode, token);
     return irNode;
-  }
-
-  private static String getStringValue(double value) {
-    long longValue = (long) value;
-
-    // Return "1" instead of "1.0"
-    if (longValue == value) {
-      return Long.toString(longValue);
-    } else {
-      return Double.toString(value);
-    }
   }
 
   static int lineno(ParseTree node) {
