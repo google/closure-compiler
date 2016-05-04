197c546c74d6e78b423baaa92ef93e02c17bde38
tbreisacher <tbreisacher@google.com>
2016-05-03 22:23:48 -0700

    Remove trailing spaces from the toString of record types. ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=121449943

--- test/com/google/javascript/rhino/jstype/RecordTypeTest.java
+++ test/com/google/javascript/rhino/jstype/RecordTypeTest.java
@@ -50,11 +50,11 @@ public class RecordTypeTest extends BaseJSTypeTestCase {
         .addProperty("number", NUMBER_TYPE, null)
         .addProperty("string", STRING_TYPE, null)
         .build();
-    assertEquals("{\n  loop: number, \n  number: number, \n  string: string\n}",
+    assertEquals("{\n  loop: number,\n  number: number,\n  string: string\n}",
         record.toString());
 
     loop.setReferencedType(record);
-    assertEquals("{\n  loop: {...}, \n  number: number, \n  string: string\n}",
+    assertEquals("{\n  loop: {...},\n  number: number,\n  string: string\n}",
         record.toString());
     assertEquals("{loop: ?, number: number, string: string}",
         record.toAnnotationString());
@@ -79,15 +79,15 @@ public class RecordTypeTest extends BaseJSTypeTestCase {
     assertEquals(
         LINE_JOINER.join(
             "{",
-            "  a01: number, ",
-            "  a02: number, ",
-            "  a03: number, ",
-            "  a04: number, ",
-            "  a05: number, ",
-            "  a06: number, ",
-            "  a07: number, ",
-            "  a08: number, ",
-            "  a09: number, ",
+            "  a01: number,",
+            "  a02: number,",
+            "  a03: number,",
+            "  a04: number,",
+            "  a05: number,",
+            "  a06: number,",
+            "  a07: number,",
+            "  a08: number,",
+            "  a09: number,",
             "  a10: number, ...",
             "}"),
         record.toString());
