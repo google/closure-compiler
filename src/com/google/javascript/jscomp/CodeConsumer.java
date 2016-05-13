39c75dd0ad83adafc2558e706e79ce2c005635e8
sdh <sdh@google.com>
2016-05-13 13:16:06 -0700

    Fix handling of negative zero by avoiding buggy GWT implementation of Math.copySign. ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=122288818

--- src/com/google/javascript/jscomp/CodeConsumer.java
+++ src/com/google/javascript/jscomp/CodeConsumer.java
@@ -284,7 +284,7 @@ abstract class CodeConsumer {
   }
 
   static boolean isNegativeZero(double x) {
-    return x == 0.0 && Math.copySign(1, x) == -1.0;
+    return x == 0.0 && 1 / x < 0;
   }
 
   static boolean isWordChar(char ch) {
