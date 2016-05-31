6aeff4d3ace6754d8b077d8a71534e0e23a4bfcb
johnlenz <johnlenz@google.com>
2016-05-31 14:57:41 -0700

    Improve the goog.base error message with directions as to how to use that alternate "base" method. ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=123686713

--- src/com/google/javascript/jscomp/ProcessClosurePrimitives.java
+++ src/com/google/javascript/jscomp/ProcessClosurePrimitives.java
@@ -146,7 +146,11 @@ class ProcessClosurePrimitives extends AbstractPostOrderCallback
 
   static final DiagnosticType USE_OF_GOOG_BASE = DiagnosticType.disabled(
       "JSC_USE_OF_GOOG_BASE",
-      "goog.base is not compatible with ES5 strict mode.");
+      "goog.base is not compatible with ES5 strict mode.\n"
+      + " Please use an alternative. \n"
+      + " For EcmaScript classes use the super keyword, for traditional Closure classes\n"
+      + " use the class specific base method instead. For example, for the constructor MyClass:\n"
+      + "   MyClass.base(this, 'constructor')");
 
   /** The root Closure namespace */
   static final String GOOG = "goog";
