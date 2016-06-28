e3745a62b80e4b7ddfe5e10f6f01d6b2dc2b1821
tbreisacher <tbreisacher@google.com>
2016-06-28 12:04:05 -0700

    Add strings so that error messages don't have {0}'s and {1}'s in them. ------------- Created by MOE: https://github.com/google/moe MOE_MIGRATED_REVID=126101524

--- src/com/google/javascript/jscomp/CheckJSDoc.java
+++ src/com/google/javascript/jscomp/CheckJSDoc.java
@@ -173,31 +173,32 @@ final class CheckJSDoc extends AbstractPostOrderCallback implements HotSwapCompi
 
     if (functionNode == null) {
       // @abstract annotation on a non-function
-      report(n, MISPLACED_ANNOTATION);
+      report(n, MISPLACED_ANNOTATION, "@abstract", "only functions or methods can be abstract");
       return;
     }
 
     if (NodeUtil.getFunctionBody(functionNode).hasChildren()) {
       // @abstract annotation on a function with a non-empty body
-      report(n, MISPLACED_ANNOTATION);
+      report(n, MISPLACED_ANNOTATION, "@abstract",
+          "function with a non-empty body cannot be abstract");
       return;
     }
 
     if (n.isMemberFunctionDef() && "constructor".equals(n.getString())) {
       // @abstract annotation on an ES6 constructor
-      report(n, MISPLACED_ANNOTATION);
+      report(n, MISPLACED_ANNOTATION, "@abstract", "constructors cannot be abstract");
       return;
     }
 
     if (!n.isMemberFunctionDef() && !NodeUtil.isPrototypeMethod(functionNode)) {
       // @abstract annotation on a non-method (or static method) in ES5
-      report(n, MISPLACED_ANNOTATION);
+      report(n, MISPLACED_ANNOTATION, "@abstract", "only functions or methods can be abstract");
       return;
     }
 
     if (n.isStaticMember()) {
       // @abstract annotation on a static method in ES6
-      report(n, MISPLACED_ANNOTATION);
+      report(n, MISPLACED_ANNOTATION, "@abstract", "static methods cannot be abstract");
       return;
     }
   }
