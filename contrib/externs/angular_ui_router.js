af9d7f8e39a4d82a4e4909fc85a36a0e5c109d40
dtarasiuk <dtarasiuk@google.com>
2016-08-01 15:19:57 -0700

    Fix ui-router externs.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=129033297

--- contrib/externs/angular_ui_router.js
+++ contrib/externs/angular_ui_router.js
@@ -33,118 +33,85 @@ var ui = {};
 
 
 /**
- * @type {Object}
  * @const
  */
 ui.router = {};
 
 
-/**
- * @typedef {{
- *   params: Object,
- *   current: !ui.router.State,
- *   transition: ?angular.$q.Promise,
- *   get: function(...),
- *   go: function(...),
- *   href: function(...),
- *   includes: function(...),
- *   is: function(...),
- *   reload: function(...),
- *   transitionTo: function(...)
- * }}
- */
-ui.router.$state;
+/** @typedef {!Object} */
+ui.router.StateParams;
 
 
-/**
- * @type {ui.router.State}
- */
-ui.router.$state.current;
-
-
-/**
- * @type {Object}
- */
-ui.router.$state.params;
-
-
-/**
- * @type {?angular.$q.Promise}
- */
-ui.router.$state.transition;
-
-
-/**
- * @param {?string|Object=} stateOrName
- * @param {?string|Object=} context
- * @return {Object|Array}
- */
-ui.router.$state.get = function(stateOrName, context) {};
-
-/**
- * @typedef {{
- *   location: (boolean|string|undefined),
- *   inherit: (boolean|undefined),
- *   relative: (Object|undefined),
- *   notify: (boolean|undefined),
- *   reload: (boolean|undefined)
- * }}
- */
-ui.router.$state.GoOptions_;
-
-/**
- * @param {string} to
- * @param {Object=} params
- * @param {(ui.router.$state.GoOptions_|Object)=} options
- * @return {angular.$q.Promise}
- */
-ui.router.$state.go = function(to, params, options) {};
-
-
-/**
- * @param {?string|Object} stateOrName
- * @param {Object=} params
- * @param {Object=} options
- * @return {string} compiled state url
- */
-ui.router.$state.href = function(stateOrName, params, options) {};
-
-
-/**
- * @param {?string} stateOrName
- * @param {Object=} params
- * @param {Object=} options
- */
-ui.router.$state.includes = function(stateOrName, params, options) {};
-
-
-/**
- * @param {?string|Object} stateOrName
- * @param {Object=} params
- * @param {Object=} options
- * @return {boolean}
- */
-ui.router.$state.is = function(stateOrName, params, options) {};
-
-
-/**
- * @return {angular.$q.Promise}
- */
-ui.router.$state.reload = function() {};
-
-
-/**
- * @param {string} to
- * @param {Object=} toParams
- * @param {Object=} options
- */
-ui.router.$state.transitionTo = function(to, toParams, options) {};
-
-
-/**
- * @typedef {Object.<string, string>}
- */
+/** @typedef {!ui.router.StateParams} */
 ui.router.$stateParams;
+
+
+/**
+ * This is the state configuration object that is getting passed to
+ * $stateProvider.state() method.
+ * @record
+ */
+ui.router.StateConfig = function() {};
+
+
+/** @type {boolean|undefined} */
+ui.router.StateConfig.prototype.abstract;
+
+
+/** @type {string|!angular.Injectable|undefined} */
+ui.router.StateConfig.prototype.controller;
+
+
+/** @type {string|undefined} */
+ui.router.StateConfig.prototype.controllerAs;
+
+
+/** @type {!Function|undefined} */
+ui.router.StateConfig.prototype.controllerProvider;
+
+
+/** @type {!Object|undefined} */
+ui.router.StateConfig.prototype.data;
+
+
+/** @type {!angular.Injectable|undefined} */
+ui.router.StateConfig.prototype.onEnter;
+
+
+/** @type {!angular.Injectable|undefined} */
+ui.router.StateConfig.prototype.onExit;
+
+
+/** @type {!Object|undefined} */
+ui.router.StateConfig.prototype.params;
+
+
+/** @type {boolean|undefined} */
+ui.router.StateConfig.prototype.reloadOnSearch;
+
+
+/** @type {!Object<string, (!angular.Injectable|string)>|undefined} */
+ui.router.StateConfig.prototype.resolve;
+
+
+/** @type {string|undefined} */
+ui.router.StateConfig.prototype.template;
+
+
+/** @type {string|undefined} */
+ui.router.StateConfig.prototype.templateUrl;
+
+
+/** @type {function(...): (!angular.$q.Promise<string>|string)|undefined} */
+ui.router.StateConfig.prototype.templateProvider;
+
+
+/** @type {string|undefined} */
+ui.router.StateConfig.prototype.url;
+
+
+/** @type {!Object<string, !ui.router.State>|undefined} */
+ui.router.StateConfig.prototype.views;
 
 
 /**
@@ -154,39 +121,212 @@ ui.router.$stateParams;
  * Example:
  * $rootScope.$on('$stateChangeStart', function(
  *     event, toState, toParams, fromState, fromParams){ ... });
- *
- * @typedef {{
- *     'abstract': (boolean|undefined),
- *     controller: (string|Function|undefined),
- *     controllerAs: (string|undefined),
- *     controllerProvider: (Function|undefined),
- *     data: (Object|undefined),
- *     name: string,
- *     onEnter: (Object|undefined),
- *     onExit: (Object|undefined),
- *     params: (Object|undefined),
- *     reloadOnSearch: (boolean|undefined),
- *     resolve: (Object.<string, !Function>|undefined),
- *     template: (string|Function|undefined),
- *     templateUrl: (string|Function|undefined),
- *     templateProvider: (Function|undefined),
- *     url: (string|undefined),
- *     views: (Object|undefined)
- * }}
+ * TODO(dtarasiuk): remove field duplication when @extends works for @record
+ * @record
  */
-ui.router.State;
+ui.router.State = function() {};
 
+
+/** @type {string} */
+ui.router.State.prototype.name;
+
+
+/** @type {boolean|undefined} */
+ui.router.State.prototype.abstract;
+
+
+/** @type {string|!angular.Injectable|undefined} */
+ui.router.State.prototype.controller;
+
+
+/** @type {string|undefined} */
+ui.router.State.prototype.controllerAs;
+
+
+/** @type {!Function|undefined} */
+ui.router.State.prototype.controllerProvider;
+
+
+/** @type {!Object|undefined} */
+ui.router.State.prototype.data;
+
+
+/** @type {!angular.Injectable|undefined} */
+ui.router.State.prototype.onEnter;
+
+
+/** @type {!angular.Injectable|undefined} */
+ui.router.State.prototype.onExit;
+
+
+/** @type {!Object|undefined} */
+ui.router.State.prototype.params;
+
+
+/** @type {boolean|undefined} */
+ui.router.State.prototype.reloadOnSearch;
+
+
+/** @type {!Object<string, (!angular.Injectable|string)>|undefined} */
+ui.router.State.prototype.resolve;
+
+
+/** @type {string|undefined} */
+ui.router.State.prototype.template;
+
+
+/** @type {string|undefined} */
+ui.router.State.prototype.templateUrl;
+
+
+/** @type {function(): string|undefined} */
+ui.router.State.prototype.templateProvider;
+
+
+/** @type {string|undefined} */
+ui.router.State.prototype.url;
+
+
+/** @type {!Object<string, !ui.router.State>|undefined} */
+ui.router.State.prototype.views;
+
+
+/** @record */
+ui.router.$state = function() {};
+
+
+/** @type {!ui.router.State} */
+ui.router.$state.prototype.current;
+
+
+/** @type {!Object} */
+ui.router.$state.prototype.params;
+
+
+/** @type {!Object} */
+ui.router.$state.prototype.options;
+
+
+/** @type {?angular.$q.Promise} */
+ui.router.$state.prototype.transition;
 
 
 /**
- * @constructor
+ * @param {VALUE=} opt_stateName
+ * @return {RESULT}
+ *
+ * @template VALUE
+ * @template RESULT :=
+ *     cond(isUnknown(VALUE),
+ *         type('Array', 'ui.router.State'),
+ *         'ui.router.State')
+ * =:
+ */
+ui.router.$state.prototype.get = function(opt_stateName) {};
+
+
+/** @record */
+ui.router.StateOptions = function() {};
+
+
+/** @type {boolean|string|undefined} */
+ui.router.StateOptions.prototype.location;
+
+
+/** @type {boolean|undefined} */
+ui.router.StateOptions.prototype.inherit;
+
+
+/** @type {!ui.router.State|undefined} */
+ui.router.StateOptions.prototype.relative;
+
+
+/** @type {boolean|undefined} */
+ui.router.StateOptions.prototype.notify;
+
+
+/** @type {boolean|undefined} */
+ui.router.StateOptions.prototype.reload;
+
+
+/**
+ * @param {string|!ui.router.State} to
+ * @param {?ui.router.StateParams=} opt_toParams
+ * @param {!ui.router.StateOptions=} opt_options
+ * @return {!angular.$q.Promise}
+ */
+ui.router.$state.prototype.go = function(to, opt_toParams, opt_options) {};
+
+
+/** @record */
+ui.router.HrefOptions = function() {};
+
+
+/** @type {boolean|undefined} */
+ui.router.HrefOptions.prototype.lossy;
+
+
+/** @type {boolean|undefined} */
+ui.router.HrefOptions.prototype.inherit;
+
+
+/** @type {!ui.router.State|undefined} */
+ui.router.HrefOptions.prototype.relative;
+
+
+/** @type {boolean|undefined} */
+ui.router.HrefOptions.prototype.absolute;
+
+
+/**
+ * @param {string|!ui.router.State} stateOrName
+ * @param {!ui.router.StateParams=} opt_params
+ * @param {!ui.router.HrefOptions=} opt_options
+ * @return {string} compiled state url
+ */
+ui.router.$state.prototype.href = function(
+    stateOrName, opt_params, opt_options) {};
+
+
+/**
+ * @param {string|!ui.router.State} stateOrName
+ * @param {!ui.router.StateParams=} opt_params
+ */
+ui.router.$state.prototype.includes = function(stateOrName, opt_params) {};
+
+
+/**
+ * @param {string|!ui.router.State} stateOrName
+ * @param {!ui.router.StateParams=} opt_params
+ * @return {boolean}
+ */
+ui.router.$state.prototype.is = function(stateOrName, opt_params) {};
+
+
+/**
+ * @param {string|!ui.router.State=} opt_stateOrName
+ * @return {!angular.$q.Promise}
+ */
+ui.router.$state.prototype.reload = function(opt_stateOrName) {};
+
+
+/**
+ * @param {string|!ui.router.State} to
+ * @param {!ui.router.StateParams=} opt_toParams
+ * @param {!ui.router.StateOptions=} opt_options
+ */
+ui.router.$state.prototype.transitionTo = function(
+    to, opt_toParams, opt_options) {};
+
+
+/**
+ * @interface
  */
 ui.router.$urlMatcherFactory = function() {};
 
 
-
 /**
- * @constructor
+ * @interface
  * @param {!ui.router.$urlMatcherFactory} $urlMatcherFactory
  */
 ui.router.$urlRouterProvider = function($urlMatcherFactory) {};
@@ -194,7 +334,7 @@ ui.router.$urlRouterProvider = function($urlMatcherFactory) {};
 
 /**
  * @param {string|RegExp} url
- * @param {string|function(...)|Array.<!Object>} route
+ * @param {string|function(...)|Array<!Object>} route
  */
 ui.router.$urlRouterProvider.prototype.when = function(url, route) {};
 
@@ -211,9 +351,8 @@ ui.router.$urlRouterProvider.prototype.otherwise = function(path) {};
 ui.router.$urlRouterProvider.prototype.rule = function(rule) {};
 
 
-
 /**
- * @constructor
+ * @interface
  * @param {!ui.router.$urlRouterProvider} $urlRouterProvider
  * @param {!ui.router.$urlMatcherFactory} $urlMatcherFactory
  * @param {!angular.$locationProvider} $locationProvider
@@ -224,7 +363,17 @@ ui.router.$stateProvider = function(
 
 /**
  * @param {!string} name
- * @param {Object} definition
+ * @param {!ui.router.StateConfig} stateConfig
  * @return {!ui.router.$stateProvider}
  */
-ui.router.$stateProvider.prototype.state = function(name, definition) {};
+ui.router.$stateProvider.prototype.state = function(name, stateConfig) {};
+
+
+/**
+ * @param {!string} name
+ * @param {(function(!ui.router.State, !Function):
+ *     Object<!ui.router.StateConfig>)=} opt_decoratorFn
+ * @return {!ui.router.$stateProvider}
+ */
+ui.router.$stateProvider.prototype.decorator = function(
+    name, opt_decoratorFn) {};
