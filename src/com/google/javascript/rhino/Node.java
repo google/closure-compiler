2b7413682869c73aafc2b2fdd355b408a14d1992
tdeegan <tdeegan@google.com>
2016-08-17 09:47:42 -0700

    Automated rollback of changelist 130461846.
    *** Reason for rollback ***
    
    This CL causes unexpected slowdowns in regression tests.
    
    *** Original change description ***
    
    Run pure function analysis in the optimization loop in J2CL code so that we can take advantage of more optimizations revealed by j2cl passes.
    
    Pure function analysis took: 379ms // This is the original pass
    Pure function analysis took: 155ms // Subsequent passes take less time becuase the AST is smaller
    Pure function analysis took: 84ms
    Pure function analysis took: 82ms
    Pure function analysis took: 59ms
    Pure function analysis took: 40ms
    Pure function analysis took: 33ms
    Pure function analysis to...
    
    ***
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=130529785

--- src/com/google/javascript/rhino/Node.java
+++ src/com/google/javascript/rhino/Node.java
@@ -2512,6 +2512,8 @@ public class Node implements Serializable {
   public static final int FLAG_NO_THROWS = 8;
   public static final int FLAG_LOCAL_RESULTS = 16;
 
+  public static final int SIDE_EFFECTS_FLAGS_MASK = 31;
+
   public static final int SIDE_EFFECTS_ALL = 0;
   public static final int NO_SIDE_EFFECTS =
     FLAG_GLOBAL_STATE_UNMODIFIED
@@ -2526,7 +2528,7 @@ public class Node implements Serializable {
    */
   public void setSideEffectFlags(int flags) {
     Preconditions.checkArgument(
-        this.isCall() || this.isNew(),
+        this.getToken() == Token.CALL || this.getToken() == Token.NEW,
         "setIsNoSideEffectsCall only supports CALL and NEW nodes, got %s",
         this.getToken());
 
