41920980d75e88da3fabee9cdee11bde33dbf2d4
Christian Gonzalez <chrisgo@microsoft.com>
2016-12-15 13:54:35 -0800

    Expose the RenamePrefix compiler option as a command line argument.
    Closes https://github.com/google/closure-compiler/pull/2200
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=142184507

--- src/com/google/javascript/jscomp/CommandLineRunner.java
+++ src/com/google/javascript/jscomp/CommandLineRunner.java
@@ -617,6 +617,10 @@ public class CommandLineRunner extends
         usage = "Checks for type errors using the new type inference algorithm.")
     private boolean useNewTypeInference = false;
 
+    @Option(name = "--rename_variable_prefix",
+        usage = "Specifies a prefix that will be prepended to all variables.")
+    private String renamePrefix = null;
+
     @Option(name = "--rename_prefix_namespace",
         usage = "Specifies the name of an object that will be used to store all "
         + "non-extern globals")
@@ -1600,6 +1604,8 @@ public class CommandLineRunner extends
             "Unknown J2clPassMode `" + flags.j2clPassMode + "' specified.");
       }
     }
+
+    options.renamePrefix = flags.renamePrefix;
 
     options.renamePrefixNamespace = flags.renamePrefixNamespace;
 
