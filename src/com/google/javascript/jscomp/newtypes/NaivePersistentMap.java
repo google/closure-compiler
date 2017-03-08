c922390d2c7696ee2150f7a2755d6f48794a5438
rluble <rluble@google.com>
2017-03-07 18:33:52 -0800

    Make NaivePersistentMap serializable.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=159905545

--- src/com/google/javascript/jscomp/newtypes/NaivePersistentMap.java
+++ src/com/google/javascript/jscomp/newtypes/NaivePersistentMap.java
@@ -16,12 +16,13 @@
 
 package com.google.javascript.jscomp.newtypes;
 
+import java.io.Serializable;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
 
 /** A naive persistent map that does too many copies */
-final class NaivePersistentMap<K, V> extends PersistentMap<K, V> {
+final class NaivePersistentMap<K, V> extends PersistentMap<K, V> implements Serializable {
   private final Map<K, V> map;
 
   private NaivePersistentMap(Map<K, V> m) {
