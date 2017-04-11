723d731ae27fcf2eeec1763cc40ff7f832110848
tomnguyen <tomnguyen@google.com>
2017-04-11 09:01:15 -0700

    Extends polyfill adds a private variable that should only be used by compiler.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=152818245

--- src/com/google/javascript/jscomp/js/es6/util/inherits.js
+++ src/com/google/javascript/jscomp/js/es6/util/inherits.js
@@ -54,6 +54,7 @@ $jscomp.inherits = function(childCtor, parentCtor) {
   /** @constructor */
   function tempCtor() {}
   tempCtor.prototype = parentCtor.prototype;
+  childCtor.superClass_ = parentCtor.prototype;
   childCtor.prototype = new tempCtor();
   /** @override */
     childCtor.prototype.constructor = childCtor;
