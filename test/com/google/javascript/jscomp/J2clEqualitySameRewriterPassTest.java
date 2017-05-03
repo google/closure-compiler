c661709eca4c023fd3b741bd751d4e87792fc89e
sdh <sdh@google.com>
2017-05-02 21:35:54 -0700

    [NTI] Convert J2clEqualitySameRewriterPassTest to TypeICompilerTestCase.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=154919812

--- test/com/google/javascript/jscomp/J2clEqualitySameRewriterPassTest.java
+++ test/com/google/javascript/jscomp/J2clEqualitySameRewriterPassTest.java
@@ -15,18 +15,18 @@
  */
 package com.google.javascript.jscomp;
 
-public class J2clEqualitySameRewriterPassTest extends CompilerTestCase {
+public class J2clEqualitySameRewriterPassTest extends TypeICompilerTestCase {
   private static final String EXTERN = "Equality.$same = function(a, b) {};";
 
   private boolean useTypesForOptimization = true;
 
   public J2clEqualitySameRewriterPassTest() {
-    super(EXTERN);
+    super(MINIMAL_EXTERNS + EXTERN);
   }
 
   @Override
   public void setUp() {
-    enableTypeCheck();
+    this.mode = TypeInferenceMode.BOTH;
   }
 
   @Override
