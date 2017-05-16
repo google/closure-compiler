3c60ea0a7fe2f6aea6cb1f8326775b46e44b70e7
tbreisacher <tbreisacher@google.com>
2017-05-15 19:06:48 -0700

    Open-source files which were mistakenly left out of ce7f6cd50e0d2c29e33fb05a3e0c63ef1c3c2a9e -- fixes https://github.com/google/closure-compiler/issues/1689
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=156134705

---
+++ travis_util/deploy_snapshot.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# see https://coderwall.com/p/9b_lfq
+
+set -e -u
+
+function mvn_deploy() {
+  mvn clean source:jar deploy \
+    --settings="$(dirname $0)/settings.xml" -DskipTests=true "$@"
+}
+
+# Restrict when snapshots are published
+# * Only for Google's repo, not any other forks
+# * Only deploy the build for the latest JDK version
+# * Not when Travis was run to check a pull request.
+# * Only for the master branch.
+if [ "$TRAVIS_REPO_SLUG" == "google/closure-compiler" ] && \
+   [ "$TRAVIS_JDK_VERSION" == "oraclejdk8" ] && \
+   [ "$TRAVIS_PULL_REQUEST" == "false" ] && \
+   [ "$TRAVIS_BRANCH" == "master" ]; then
+  echo "Publishing Maven snapshot..."
+
+  mvn_deploy
+
+  echo "Maven snapshot published."
+fi
