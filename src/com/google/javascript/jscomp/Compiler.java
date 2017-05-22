99c4661a447ef55201251c67d21acfdcb100b73e
rluble <rluble@google.com>
2017-05-22 10:21:22 -0700

    Do not mask compiler exceptions with deserialization errors.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=156752810

--- src/com/google/javascript/jscomp/Compiler.java
+++ src/com/google/javascript/jscomp/Compiler.java
@@ -3349,7 +3349,7 @@ public class Compiler extends AbstractCompiler implements ErrorHandler, SourceFi
   }
 
   @GwtIncompatible("ObjectInputStream")
-  public void restoreState(InputStream inputStream) throws Exception {
+  public void restoreState(InputStream inputStream) throws IOException  {
     try (final ObjectInputStream objectInputStream = new ObjectInputStream(inputStream)) {
       CompilerState compilerState = runInCompilerThread(new Callable<CompilerState>() {
         @Override
