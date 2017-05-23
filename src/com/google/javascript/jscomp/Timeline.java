1d73d5265a98ca3527c759a0ffe132e987add4fc
stalcup <stalcup@google.com>
2017-05-23 14:28:21 -0700

    Adds a timeline datastructure to enable running passes on just changed scopes.
    The Timeline class is an ordered set that moves values to the front when added
    (even if already contained) and which allows for the marking of times in the
    series from which point an accumulation of all following values can be
    (efficiently) requested.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=156911310

---
+++ src/com/google/javascript/jscomp/Timeline.java
@@ -0,0 +1,123 @@
+/*
+ * Copyright 2017 The Closure Compiler Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.javascript.jscomp;
+
+import com.google.common.base.Preconditions;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * An ordered set that moves values to the front when added (even if already contained) and which
+ * allows for the marking of named points in time in the series from which point an accumulation of
+ * all following values can be (efficiently) requested.
+ */
+// TODO(stalcup): save memory by unlinking events older than the oldest marked time.
+final class Timeline<T> {
+
+  private static class Event<T> {
+    Event<?> nextEvent;
+    Event<?> previousEvent;
+    T value;
+
+    Event(T value) {
+      Preconditions.checkNotNull(value);
+      this.value = value;
+    }
+  }
+
+  private static class Time {
+    final String name;
+
+    Time(String name) {
+      Preconditions.checkNotNull(name);
+      this.name = name;
+    }
+
+    @Override
+    public boolean equals(Object anObject) {
+      if (anObject instanceof Time) {
+        return name.equals(((Time) anObject).name);
+      }
+      return false;
+    }
+
+    @Override
+    public int hashCode() {
+      return name.hashCode();
+    }
+  }
+
+  private static <V> Event<V> addEvent(V value, Map<V, Event<V>> eventsByKey, Event<?> headEvent) {
+    Event<V> event = eventsByKey.get(value);
+
+    // If the event already exists and is already at the front, do nothing.
+    if (headEvent == event) {
+      return event;
+    }
+
+    // If the event already exists somewhere else in the history then...
+    if (event != null) {
+      // cut it out of the linked list...
+      event.previousEvent.nextEvent = event.nextEvent;
+      event.nextEvent.previousEvent = event.previousEvent;
+      event.nextEvent = null;
+    } else {
+      // Otherwise create and track an event for the given value.
+      event = new Event<V>(value);
+      eventsByKey.put(value, event);
+    }
+
+    // Regardless, stick the event at the front.
+    event.previousEvent = headEvent;
+    headEvent.nextEvent = event;
+    headEvent = event;
+
+    return event;
+  }
+
+  private final Map<Time, Event<Time>> eventsByTime = new HashMap<>();
+  private final Map<T, Event<T>> eventsByValue = new HashMap<>();
+  private Event<?> headEvent = new Event<>(new Time("-beginning-"));
+
+  void add(T value) {
+    headEvent = addEvent(value, eventsByValue, headEvent);
+  }
+
+  void mark(String timeName) {
+    headEvent = addEvent(new Time(timeName), eventsByTime, headEvent);
+  }
+
+  @SuppressWarnings("unchecked")
+  List<T> getSince(String timeName) {
+    List<T> values = new ArrayList<>();
+
+    Event<?> firstEvent = eventsByTime.get(new Time(timeName));
+    if (firstEvent == null) {
+      return null;
+    }
+
+    for (Event<?> event = firstEvent; event != null; event = event.nextEvent) {
+      // If the event contains a user provided value, accumulate it.
+      if (!(event.value instanceof Time)) {
+        values.add((T) event.value);
+      }
+    }
+
+    return values;
+  }
+}
---
+++ test/com/google/javascript/jscomp/TimelineTest.java
@@ -0,0 +1,140 @@
+/*
+ * Copyright 2017 The Closure Compiler Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.javascript.jscomp;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import junit.framework.TestCase;
+
+/** Tests {@link Timeline}. */
+public class TimelineTest extends TestCase {
+
+  private static class Event {
+    final int hashcode;
+
+    Event(int hashcode) {
+      this.hashcode = hashcode;
+    }
+
+    @Override
+    public int hashCode() {
+      return hashcode;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (obj instanceof Event) {
+        return ((Event) obj).hashcode == hashcode;
+      }
+      return false;
+    }
+  }
+
+  private static final Event ATE_GREEN_EGGS = new Event(1);
+  private static final Event ATE_HAM = new Event(2);
+  private Timeline<Event> timeline;
+
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
+    timeline = new Timeline<>();
+  }
+
+  public void testUnknownTimesReturnNull() {
+    assertNull(timeline.getSince("Monday"));
+  }
+
+  public void testDoesntReturnValuesBeforeTime() {
+    timeline.add(ATE_GREEN_EGGS);
+    timeline.mark("Monday");
+
+    assertThat(timeline.getSince("Monday")).isEmpty();
+  }
+
+  public void testReturnsValuesAfterTime() {
+    timeline.mark("Monday");
+    timeline.add(ATE_GREEN_EGGS);
+
+    assertThat(timeline.getSince("Monday")).containsExactly(ATE_GREEN_EGGS);
+  }
+
+  public void testMaintainsOrder1() {
+    timeline.mark("Monday");
+    timeline.add(ATE_GREEN_EGGS);
+    timeline.add(ATE_HAM);
+
+    assertThat(timeline.getSince("Monday")).containsExactly(ATE_GREEN_EGGS, ATE_HAM).inOrder();
+  }
+
+  public void testMaintainsOrder2() {
+    timeline.mark("Monday");
+    timeline.add(ATE_GREEN_EGGS);
+    timeline.add(ATE_GREEN_EGGS);
+    timeline.add(ATE_HAM);
+
+    assertThat(timeline.getSince("Monday")).containsExactly(ATE_GREEN_EGGS, ATE_HAM).inOrder();
+  }
+
+  public void testMaintainsOrder3() {
+    timeline.mark("Monday");
+    timeline.add(ATE_GREEN_EGGS);
+    timeline.add(ATE_HAM);
+    timeline.add(ATE_GREEN_EGGS);
+
+    assertThat(timeline.getSince("Monday")).containsExactly(ATE_HAM, ATE_GREEN_EGGS).inOrder();
+  }
+
+  public void testMaintainsOrder4() {
+    timeline.mark("Monday");
+    timeline.add(ATE_GREEN_EGGS);
+    timeline.add(ATE_HAM);
+    timeline.add(ATE_GREEN_EGGS);
+    timeline.add(ATE_HAM);
+
+    assertThat(timeline.getSince("Monday")).containsExactly(ATE_GREEN_EGGS, ATE_HAM).inOrder();
+  }
+
+  public void testUsesEqualityAndHashcode() {
+    // Are object and hashcode equal.
+    Event ateBerries = new Event(10);
+    Event ateGrapes = new Event(10);
+
+    timeline.mark("Monday");
+    timeline.add(ateBerries);
+    timeline.add(ateGrapes);
+
+    assertThat(timeline.getSince("Monday")).containsExactly(ateBerries).inOrder();
+    assertThat(timeline.getSince("Monday")).containsExactly(ateGrapes).inOrder();
+  }
+
+  public void testUpdatesExistingTimes() {
+    timeline.mark("Monday");
+    timeline.add(ATE_GREEN_EGGS);
+    timeline.mark("Monday");
+
+    assertThat(timeline.getSince("Monday")).isEmpty();
+  }
+
+  public void testManagesDifferentTimes() {
+    timeline.mark("Monday");
+    timeline.add(ATE_GREEN_EGGS);
+    timeline.mark("Thursday");
+    timeline.add(ATE_HAM);
+
+    assertThat(timeline.getSince("Monday")).containsExactly(ATE_GREEN_EGGS, ATE_HAM).inOrder();
+    assertThat(timeline.getSince("Thursday")).containsExactly(ATE_HAM);
+  }
+}
