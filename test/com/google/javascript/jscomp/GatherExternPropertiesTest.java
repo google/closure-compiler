3f0bd9f1887ccb6a29fa9d8ad531c163b9e0151d
bellashim <bellashim@google.com>
2017-06-06 14:23:22 -0700

    Use setUp() instead of constructor to do test setup.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=158189620

--- test/com/google/javascript/jscomp/GatherExternPropertiesTest.java
+++ test/com/google/javascript/jscomp/GatherExternPropertiesTest.java
@@ -24,8 +24,9 @@ import com.google.common.collect.ImmutableSet;
  * Test case for {@link GatherExternProperties}.
  */
 public final class GatherExternPropertiesTest extends CompilerTestCase {
-  public GatherExternPropertiesTest() {
-    super();
+  @Override
+  protected void setUp() throws Exception {
+    super.setUp();
     enableTypeCheck();
   }
 
