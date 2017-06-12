b76e7c82dc4bbb9e7d2b43a6ee1fd8b2f094a001
tbreisacher <tbreisacher@google.com>
2017-06-12 11:22:31 -0700

    Run ProcessClosurePrimitives even if modules are not transpiled.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=158738626

--- src/com/google/javascript/jscomp/DefaultPassConfig.java
+++ src/com/google/javascript/jscomp/DefaultPassConfig.java
@@ -1353,7 +1353,7 @@ public final class DefaultPassConfig extends PassConfig {
 
         @Override
         protected FeatureSet featureSet() {
-          return ES8;
+          return ES8_MODULES;
         }
       };
 
