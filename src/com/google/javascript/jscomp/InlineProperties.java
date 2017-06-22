5e5c94b4bb9a8225e02ad74b7a13ef919e9d3ac3
sdh <sdh@google.com>
2017-06-22 14:55:57 -0700

    Factor out the type-invalidation into InvalidatingTypes
    This logic is currently duplicated between InlineProperties and AmbiguateProperties, as well as partly in DisambiguateProperties.  This change only affects InlineProperties, since AmbiguateProperties has not yet been migrated to NTI.  These other passes will be migrated to InvalidatingTypes in a future change.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=159881081

--- src/com/google/javascript/jscomp/InlineProperties.java
+++ src/com/google/javascript/jscomp/InlineProperties.java
@@ -18,20 +18,15 @@ package com.google.javascript.jscomp;
 import static com.google.common.base.Preconditions.checkNotNull;
 import static com.google.common.base.Preconditions.checkState;
 
-import com.google.common.collect.ImmutableList;
 import com.google.javascript.jscomp.NodeTraversal.AbstractPostOrderCallback;
 import com.google.javascript.rhino.FunctionTypeI;
 import com.google.javascript.rhino.IR;
 import com.google.javascript.rhino.JSDocInfo;
 import com.google.javascript.rhino.Node;
-import com.google.javascript.rhino.ObjectTypeI;
 import com.google.javascript.rhino.TypeI;
-import com.google.javascript.rhino.TypeIRegistry;
 import com.google.javascript.rhino.jstype.JSTypeNative;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.Map;
-import java.util.Set;
 
 /**
  * InlineProperties attempts to find references to properties that are known to be constants and
@@ -63,45 +58,18 @@ final class InlineProperties implements CompilerPass {
 
   private final Map<String, PropertyInfo> props = new HashMap<>();
 
-  private final Set<TypeI> invalidatingTypes = new HashSet<>();
+  private final InvalidatingTypes invalidatingTypes;
 
   InlineProperties(AbstractCompiler compiler) {
     this.compiler = compiler;
-    buildInvalidatingTypeSet();
+    this.invalidatingTypes = new InvalidatingTypes.Builder(compiler.getTypeIRegistry())
+        .addTypesInvalidForPropertyRenaming()
+        // NOTE: Mismatches are less important to this pass than to (dis)ambiguate properties.
+        // This pass doesn't remove values (it only inlines them when the type is known), so
+        // it isn't necessary to invalidate due to implicit interface uses.
+        .addAllTypeMismatches(compiler.getTypeMismatches())
+        .build();
     invalidateExternProperties();
-  }
-
-  // TODO(johnlenz): this is a direct copy of the invalidation code
-  // from AmbiguateProperties, if in the end we don't need to modify it
-  // we should move it to a common location.
-  private void buildInvalidatingTypeSet() {
-    TypeIRegistry registry = compiler.getTypeIRegistry();
-    invalidatingTypes.addAll(
-        ImmutableList.of(
-            registry.getNativeType(JSTypeNative.ALL_TYPE),
-            registry.getNativeType(JSTypeNative.NO_OBJECT_TYPE),
-            registry.getNativeType(JSTypeNative.NO_TYPE),
-            registry.getNativeType(JSTypeNative.NULL_TYPE),
-            registry.getNativeType(JSTypeNative.VOID_TYPE),
-            registry.getNativeType(JSTypeNative.FUNCTION_FUNCTION_TYPE),
-            registry.getNativeType(JSTypeNative.FUNCTION_INSTANCE_TYPE),
-            registry.getNativeType(JSTypeNative.FUNCTION_PROTOTYPE),
-            registry.getNativeType(JSTypeNative.GLOBAL_THIS),
-            registry.getNativeType(JSTypeNative.OBJECT_TYPE),
-            registry.getNativeType(JSTypeNative.OBJECT_PROTOTYPE),
-            registry.getNativeType(JSTypeNative.OBJECT_FUNCTION_TYPE),
-            registry.getNativeType(JSTypeNative.TOP_LEVEL_PROTOTYPE),
-            registry.getNativeType(JSTypeNative.UNKNOWN_TYPE)));
-
-    for (TypeMismatch mis : compiler.getTypeMismatches()) {
-      addInvalidatingType(mis.typeA);
-      addInvalidatingType(mis.typeB);
-    }
-
-    // NOTE: Mismatches are less important to this pass than to disabmiguate/ambiguate
-    // properties. This pass doesn't remove values (it only inlines them when the type
-    // is known), so it isn't necessary to record the invalidation from
-    // "getImplicitInterfaceUses".
   }
 
   private void invalidateExternProperties() {
@@ -109,46 +77,6 @@ final class InlineProperties implements CompilerPass {
     for (String name : compiler.getExternProperties()) {
       props.put(name, INVALIDATED);
     }
-  }
-
-  /** Invalidates the given type, so that no properties on it will be inlined. */
-  private void addInvalidatingType(TypeI type) {
-    type = type.restrictByNotNullOrUndefined();
-    if (type.isUnionType()) {
-      for (TypeI alt : type.getUnionMembers()) {
-        addInvalidatingType(alt);
-      }
-    }
-
-    invalidatingTypes.add(type);
-    ObjectTypeI objType = type.toMaybeObjectType();
-    if (objType != null && objType.isInstanceType()) {
-      invalidatingTypes.add(objType.getPrototypeObject());
-    }
-  }
-
-  /** Returns true if properties on this type should not be inlined. */
-  private boolean isInvalidatingType(TypeI type) {
-    if (type.isUnionType()) {
-      type = type.restrictByNotNullOrUndefined();
-      if (type.isUnionType()) {
-        for (TypeI alt : type.getUnionMembers()) {
-          if (isInvalidatingType(alt)) {
-            return true;
-          }
-        }
-        return false;
-      }
-    }
-    ObjectTypeI objType = type.toMaybeObjectType();
-    return objType == null
-        || invalidatingTypes.contains(objType)
-        || objType.isUnknownObject()
-        || objType.isUnknownType()
-        || objType.isBottom()
-        || objType.isEnumObject()
-        || objType.isBoxableScalar()
-        || !(type.isConstructor() || objType.isInstanceType());
   }
 
   /** This method gets the JSType from the Node argument and verifies that it is present. */
@@ -258,7 +186,7 @@ final class InlineProperties implements CompilerPass {
     private boolean maybeStoreCandidateValue(TypeI type, String propName, Node value) {
       checkNotNull(value);
       if (!props.containsKey(propName)
-          && !isInvalidatingType(type)
+          && !invalidatingTypes.isInvalidating(type)
           && NodeUtil.isImmutableValue(value)
           && NodeUtil.isExecutedExactlyOnce(value)) {
         props.put(propName, new PropertyInfo(type, value));
@@ -300,7 +228,7 @@ final class InlineProperties implements CompilerPass {
     private boolean isMatchingType(Node n, TypeI src) {
       src = src.restrictByNotNullOrUndefined();
       TypeI dest = getTypeI(n).restrictByNotNullOrUndefined();
-      if (!isInvalidatingType(dest)) {
+      if (!invalidatingTypes.isInvalidating(dest)) {
         if (dest.isConstructor() || src.isConstructor()) {
           // Don't inline constructor properties referenced from
           // subclass constructor references. This would be appropriate
---
+++ src/com/google/javascript/jscomp/InvalidatingTypes.java
@@ -0,0 +1,110 @@
+/*
+ * Copyright 2017 The Closure Compiler Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.google.javascript.jscomp;
+
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import com.google.javascript.rhino.ObjectTypeI;
+import com.google.javascript.rhino.TypeI;
+import com.google.javascript.rhino.TypeIRegistry;
+import com.google.javascript.rhino.jstype.JSTypeNative;
+
+/**
+ * Keeps track of "invalidating types" that force type-based
+ * optimizations to back off, specifically for {@link InlineProperties}
+ * and {@link AmbiguateProperties}.
+ */
+final class InvalidatingTypes {
+
+  private final ImmutableSet<TypeI> types;
+
+  private InvalidatingTypes(ImmutableSet<TypeI> types) {
+    this.types = types;
+  }
+
+  boolean isInvalidating(TypeI type) {
+    if (type.isUnionType()) {
+      type = type.restrictByNotNullOrUndefined();
+      if (type.isUnionType()) {
+        for (TypeI alt : type.getUnionMembers()) {
+          if (isInvalidating(alt)) {
+            return true;
+          }
+        }
+        return false;
+      }
+    }
+    ObjectTypeI objType = type.toMaybeObjectType();
+    return objType == null
+        || types.contains(objType)
+        || objType.isUnknownObject()
+        || objType.isUnknownType()
+        || objType.isBottom()
+        || objType.isEnumObject()
+        || objType.isBoxableScalar();
+  }
+
+  static final class Builder {
+    private final ImmutableSet.Builder<TypeI> types = ImmutableSet.builder();
+    private final TypeIRegistry registry;
+
+    Builder(TypeIRegistry registry) {
+      this.registry = registry;
+    }
+
+    InvalidatingTypes build() {
+      return new InvalidatingTypes(types.build());
+    }
+
+    Builder addAllTypeMismatches(Iterable<TypeMismatch> mismatches) {
+      for (TypeMismatch mis : mismatches) {
+        addType(mis.typeA);
+        addType(mis.typeB);
+      }
+      return this;
+    }
+
+    Builder addTypesInvalidForPropertyRenaming() {
+      types.addAll(
+          ImmutableList.of(
+              registry.getNativeType(JSTypeNative.FUNCTION_FUNCTION_TYPE),
+              registry.getNativeType(JSTypeNative.FUNCTION_INSTANCE_TYPE),
+              registry.getNativeType(JSTypeNative.FUNCTION_PROTOTYPE),
+              registry.getNativeType(JSTypeNative.GLOBAL_THIS),
+              registry.getNativeType(JSTypeNative.OBJECT_TYPE),
+              registry.getNativeType(JSTypeNative.OBJECT_PROTOTYPE),
+              registry.getNativeType(JSTypeNative.OBJECT_FUNCTION_TYPE),
+              registry.getNativeType(JSTypeNative.TOP_LEVEL_PROTOTYPE)));
+      return this;
+    }
+
+    /** Invalidates the given type, so that no properties on it will be inlined. */
+    Builder addType(TypeI type) {
+      type = type.restrictByNotNullOrUndefined();
+      if (type.isUnionType()) {
+        for (TypeI alt : type.getUnionMembers()) {
+          addType(alt);
+        }
+      }
+      types.add(type);
+      ObjectTypeI objType = type.toMaybeObjectType();
+      if (objType != null && objType.isInstanceType()) {
+        types.add(objType.getPrototypeObject());
+      }
+      return this;
+    }
+  }
+}
