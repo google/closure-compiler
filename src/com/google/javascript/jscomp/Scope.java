8942cd00827a5e62a070c1d93961f32caa47e65d
johnlenz <johnlenz@google.com>
2017-06-26 12:54:22 -0700

    Rollback of "Gives the name value when checkState fails"
    providing the name is not useful when it is only going to be null or empty
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=160182055

--- src/com/google/javascript/jscomp/Scope.java
+++ src/com/google/javascript/jscomp/Scope.java
@@ -118,7 +118,7 @@ public class Scope implements StaticScope, Serializable {
    * @param input the input in which this variable is defined.
    */
   Var declare(String name, Node nameNode, CompilerInput input) {
-    checkState(name != null && !name.isEmpty(), name);
+    checkState(name != null && !name.isEmpty());
     // Make sure that it's declared only once
     checkState(vars.get(name) == null);
     Var var = new Var(name, nameNode, this, vars.size(), input);
