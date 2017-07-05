5ce7f428f108a70d7138c5eba4c9aed4e3cb8264
tbreisacher <tbreisacher@google.com>
2017-07-05 12:26:21 -0700

    ES6 super() calls are now transpiled to use $jscomp.construct when the super class is unknown.
    Most of the changes in this CL were originally by lillymliu@ and joeltine@
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=160990288

--- src/com/google/javascript/jscomp/js/es6/util/construct.js
+++ src/com/google/javascript/jscomp/js/es6/util/construct.js
@@ -39,11 +39,11 @@ $jscomp.construct = /** @type {function(): !Function} */ (function() {
   // Check for https://github.com/Microsoft/ChakraCore/issues/3217
   /** @return {boolean} */
   function reflectConstructWorks() {
-    /** @constructor */ function Base() {}
-    /** @constructor */ function Derived() {}
-    new Base();
-    Reflect.construct(Base, [], Derived);
-    return new Base() instanceof Base;
+    /** @constructor */ var BaseTestClass = function() {};
+    /** @constructor */ var DerivedTestClass = function() {};
+    new BaseTestClass();
+    Reflect.construct(BaseTestClass, [], DerivedTestClass);
+    return new BaseTestClass() instanceof BaseTestClass;
   }
 
   if (typeof Reflect != 'undefined' && Reflect.construct) {
--- test/com/google/javascript/jscomp/Es6ToEs3ConverterTest.java
+++ test/com/google/javascript/jscomp/Es6ToEs3ConverterTest.java
@@ -277,7 +277,7 @@ public final class Es6ToEs3ConverterTest extends CompilerTestCase {
             " * @param {...?} var_args",
             " */",
             "var testcode$classdecl$var0 = function(var_args) {",
-            "  return D.apply(this,arguments) || this; ",
+            "  return $jscomp.construct(D, arguments, this.constructor); ",
             "};",
             "$jscomp.inherits(testcode$classdecl$var0, D);",
             "testcode$classdecl$var0.prototype.f = function() { D.prototype.g.call(this); };",
@@ -568,7 +568,8 @@ public final class Es6ToEs3ConverterTest extends CompilerTestCase {
             " */",
             "var C = function(var_args) { D.apply(this, arguments); };",
             "$jscomp.inherits(C, D);"));
-    assertThat(getLastCompiler().injected).containsExactly("es6/util/inherits");
+    assertThat(getLastCompiler().injected)
+        .containsExactly("es6/util/inherits", "es6/util/construct");
 
     test(
         "class D {} class C extends D { constructor() { super(); } }",
@@ -600,7 +601,7 @@ public final class Es6ToEs3ConverterTest extends CompilerTestCase {
             " * @param {...?} var_args",
             " */",
             "var C = function(var_args) {",
-            " return ns.D.apply(this, arguments) || this;",
+            " return $jscomp.construct(ns.D, arguments, this.constructor);",
             "};",
             "$jscomp.inherits(C, ns.D);"));
 
@@ -945,7 +946,7 @@ public final class Es6ToEs3ConverterTest extends CompilerTestCase {
             "/** @constructor @struct @extends {D} */",
             "var C = function(str, n) {",
             "  var $jscomp$super$this;",
-            "  ($jscomp$super$this = D.call(this,str) || this).n = n;",
+            "  ($jscomp$super$this = $jscomp.construct(D, [str], this.constructor)).n = n;",
             "  return $jscomp$super$this;", // Duplicate because of existing return statement.
             "  return $jscomp$super$this;",
             "}",
@@ -1014,13 +1015,31 @@ public final class Es6ToEs3ConverterTest extends CompilerTestCase {
             "var C = function(str, n) {",
             "  var $jscomp$super$this;",
             "  if (n >= 0) {",
-            "    $jscomp$super$this = D.call(this, 'positive: ' + str) || this;",
+            "    $jscomp$super$this = $jscomp.construct(D,[\"positive: \"+str],this.constructor);",
             "  } else {",
-            "    $jscomp$super$this = D.call(this, 'negative: ' + str) || this;",
+            "    $jscomp$super$this = $jscomp.construct(D,[\"negative: \"+str],this.constructor);",
             "  }",
             "  $jscomp$super$this.n = n;",
             "  return $jscomp$super$this;",
             "}",
+            "$jscomp.inherits(C, D);"));
+  }
+
+  public void testTranspiledExtendsNative() {
+    // transpiled class extends a native ES6 class
+    allowExternsChanges();
+    test(
+        "class D {}", // externs
+        "class C extends D {}",
+        LINE_JOINER.join(
+            "/**",
+            " * @constructor @struct",
+            " * @extends {D}",
+            " * @param {...?} var_args",
+            " */",
+            "var C = function(var_args) {",
+            "  return $jscomp.construct(D, arguments, this.constructor);",
+            "};",
             "$jscomp.inherits(C, D);"));
   }
 
@@ -1505,7 +1524,7 @@ public final class Es6ToEs3ConverterTest extends CompilerTestCase {
             "};",
             "$jscomp.inherits(C,D);"));
     assertThat(getLastCompiler().injected)
-        .containsExactly("es6/util/arrayfromiterable", "es6/util/inherits");
+        .containsExactly("es6/util/arrayfromiterable", "es6/util/inherits", "es6/util/construct");
   }
 
   public void testSuperCallNonConstructor() {
@@ -1615,7 +1634,7 @@ public final class Es6ToEs3ConverterTest extends CompilerTestCase {
             " * @param {...?} var_args",
             " */",
             "var CodeClass = function(var_args) {",
-            "  return ExternsClass.apply(this,arguments) || this;",
+            "  return $jscomp.construct(ExternsClass, arguments, this.constructor);",
             "};",
             "$jscomp.inherits(CodeClass,ExternsClass)"));
   }
