78678832573088a5962d15e556acd066268e90a7
tdeegan <tdeegan@google.com>
2017-07-05 11:09:12 -0700

    Fixes leaking file reader.
    See: https://groups.google.com/forum/#!topic/closure-compiler-discuss/S1Q5Icai9vU
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=160979830

--- src/com/google/javascript/jscomp/SourceFile.java
+++ src/com/google/javascript/jscomp/SourceFile.java
@@ -613,7 +613,9 @@ public class SourceFile implements StaticSourceFile, Serializable {
 
       if (cachedCode == null) {
         try {
-          cachedCode = CharStreams.toString(getCodeReader());
+          try (Reader r = getCodeReader()) {
+            cachedCode = CharStreams.toString(r);
+          }
         } catch (java.nio.charset.MalformedInputException e) {
           throw new IOException("Failed to read: " + path + ", is this input UTF-8 encoded?", e);
         }
