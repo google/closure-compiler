9fbdc81ff64410cb03911302ba023f74e06b92c5
claywoolam <claywoolam@google.com>
2017-08-01 10:15:40 -0700

    Add the ServiceWorkerClients.get() call to w3c_serviceworker.js as described in here: https://developer.mozilla.org/en-US/docs/Web/API/Clients/get
    Also change the @constructor annotation to @interface on ServiceWorkerClient, because @interface is the correct one.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=163846439

--- externs/browser/w3c_serviceworker.js
+++ externs/browser/w3c_serviceworker.js
@@ -349,7 +349,7 @@ ServiceWorkerGlobalScope.prototype.indexedDB;
 
 /**
  * @see http://www.w3.org/TR/service-workers/#service-worker-client-interface
- * @constructor
+ * @interface
  */
 function ServiceWorkerClient() {}
 
@@ -419,6 +419,12 @@ ServiceWorkerClients.prototype.claim = function() {};
  * @return {!Promise<!ServiceWorkerClient>}
  */
 ServiceWorkerClients.prototype.openWindow = function(url) {};
+
+/**
+ * @param {string} id
+ * @return {!Promise<!ServiceWorkerClient|undefined>}
+ */
+ServiceWorkerClients.prototype.get = function(id) {};
 
 /** @typedef {{includeUncontrolled: (boolean|undefined)}} */
 var ServiceWorkerClientQueryOptions;
