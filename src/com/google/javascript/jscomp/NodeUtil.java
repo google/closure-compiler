8a4db33dcd6dcbfbfd984c9fc6d08c5932e7c732
blickly <blickly@google.com>
2017-08-10 12:29:17 -0700

    Remove an unused case from NodeUtil.removeChild()
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=164887564

--- src/com/google/javascript/jscomp/NodeUtil.java
+++ src/com/google/javascript/jscomp/NodeUtil.java
@@ -2783,11 +2783,6 @@ public final class NodeUtil {
       parent.detach();
     } else if (parent.isParamList()) {
       parent.removeChild(node);
-    } else if (parent.isDefaultValue()) {
-      parent.removeChild(node);
-      if (!parent.hasChildren()) {
-        removeChild(parent.getParent(), parent);
-      }
     } else {
       throw new IllegalStateException("Invalid attempt to remove node: " + node + " of " + parent);
     }
