eee7c10f19db40791a3f06a611eb25321bcb33a7
heeba <heeba@google.com>
2017-08-10 10:53:48 -0700

    Transferring TODOs.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=164874216

--- test/com/google/javascript/jscomp/VariableReferenceCheckTest.java
+++ test/com/google/javascript/jscomp/VariableReferenceCheckTest.java
@@ -422,7 +422,7 @@ public final class VariableReferenceCheckTest extends CompilerTestCase {
 
   public void testUnusedImport() {
     enableUnusedLocalAssignmentCheck = true;
-    // TODO(heeba): This test should give an UNUSED_LOCAL_ASSIGNMENT error for x.
+    // TODO(b/64566470): This test should give an UNUSED_LOCAL_ASSIGNMENT error for x.
     testSame("import x from 'Foo';");
   }
 
