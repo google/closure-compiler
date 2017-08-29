8777c0882b68f70d662d21b862b0db35cf50826d
Chad Killingsworth <chadkillingsworth@gmail.com>
2017-08-29 11:10:23 -0700

    When an invalid source map state is encountered, throw a SourceMapParseException instead of IllegalStateException so that existing error handling properly recognizes this case.
    Closes https://github.com/google/closure-compiler/pull/2631
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=166873243

--- src/com/google/debugging/sourcemap/SourceMapConsumerV3.java
+++ src/com/google/debugging/sourcemap/SourceMapConsumerV3.java
@@ -243,7 +243,7 @@ public final class SourceMapConsumerV3 implements SourceMapConsumer,
       this.content = new StringCharIterator(lineMap);
     }
 
-    void build() {
+    void build() throws SourceMapParseException {
       int [] temp = new int[MAX_ENTRY_VALUES];
       ArrayList<Entry> entries = new ArrayList<>();
       while (content.hasNext()) {
@@ -309,7 +309,7 @@ public final class SourceMapConsumerV3 implements SourceMapConsumer,
      * @param entryValues The number of entries in the array.
      * @return The entry object.
      */
-    private Entry decodeEntry(int[] vals, int entryValues) {
+    private Entry decodeEntry(int[] vals, int entryValues) throws SourceMapParseException {
       Entry entry;
       switch (entryValues) {
         // The first values, if present are in the following order:
@@ -363,7 +363,7 @@ public final class SourceMapConsumerV3 implements SourceMapConsumer,
           return entry;
 
         default:
-          throw new IllegalStateException(
+          throw new SourceMapParseException(
               "Unexpected number of values for entry:" + entryValues);
       }
     }
