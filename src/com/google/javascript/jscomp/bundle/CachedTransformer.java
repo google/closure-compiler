304578868a400ed7226a437400b5301f35a9ab8d
sdh <sdh@google.com>
2017-09-08 11:49:08 -0700

    Refactor Transpiler into a Source.Transformer.
    This should provide a cleaner API for a number of uses, such as single-file transpilation, bundling, and coverage instrumentation.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=168025536

---
+++ src/com/google/javascript/jscomp/bundle/CachedTransformer.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2017 The Closure Compiler Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.javascript.jscomp.bundle;
+
+import com.google.common.annotations.GwtIncompatible;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.LoadingCache;
+
+/** A Transformer that caches output from a delegate transformer. */
+@GwtIncompatible
+public class CachedTransformer implements Source.Transformer {
+
+  private static final String DEFAULT_CACHE_SPEC = "maximumSize=10000";
+
+  private final LoadingCache<Source, Source> cache;
+
+  public CachedTransformer(
+      Source.Transformer delegate, CacheBuilder<? super Source, ? super Source> builder) {
+    this.cache = builder.build(CacheLoader.from(source -> delegate.transform(source)));
+  }
+
+  public CachedTransformer(Source.Transformer delegate, String spec) {
+    this(delegate, CacheBuilder.from(spec));
+  }
+
+  public CachedTransformer(Source.Transformer delegate) {
+    this(delegate, DEFAULT_CACHE_SPEC);
+  }
+
+  @Override
+  public Source transform(Source input) {
+    return cache.getUnchecked(input);
+  }
+}
---
+++ test/com/google/javascript/jscomp/bundle/SourceTest.java
@@ -0,0 +1,91 @@
+/*
+ * Copyright 2017 The Closure Compiler Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.javascript.jscomp.bundle;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import com.google.common.annotations.GwtIncompatible;
+import com.google.common.base.Suppliers;
+import com.google.common.collect.ImmutableMap;
+import com.google.common.collect.ImmutableSet;
+import com.google.common.testing.EqualsTester;
+import java.nio.file.Paths;
+import junit.framework.TestCase;
+
+/** Tests for {@link Source} and its nested classes. */
+@GwtIncompatible
+public final class SourceTest extends TestCase {
+
+  public void testEquals() {
+    Source foo = Source.builder().setCode("foo").build();
+    new EqualsTester()
+        .addEqualityGroup(Source.builder().build(), Source.builder().build())
+        .addEqualityGroup(
+            Source.builder().setCode(Suppliers.ofInstance("foo")).build(),
+            Source.builder().setCode(Suppliers.ofInstance("foo")).build(),
+            Source.builder().setCode("foo").build(),
+            Source.builder().setCode("foo").build())
+        .addEqualityGroup(Source.builder().setCode("bar").build())
+        .addEqualityGroup(
+            foo.toBuilder().setPath(Paths.get("/x")).build(),
+            foo.toBuilder().setPath(Paths.get("/x")).build())
+        .addEqualityGroup(
+            foo.toBuilder().setOriginalCode("bar").build(),
+            foo.toBuilder().setOriginalCode("bar").build())
+        .addEqualityGroup(foo.toBuilder().setSourceUrl("foo").build())
+        .addEqualityGroup(foo.toBuilder().setSourceMap("foo").build())
+        .addEqualityGroup(foo.toBuilder().setSourceMappingUrl("foo").build())
+        .addEqualityGroup(foo.toBuilder().setEstimatedSize(12).build())
+        .addEqualityGroup(foo.toBuilder().setLoadFlags(ImmutableMap.of("x", "y")).build())
+        .addEqualityGroup(foo.toBuilder().setRuntimes(ImmutableSet.of("x")).build())
+        .testEquals();
+  }
+
+  public void testCode() {
+    assertThat(Source.builder().setCode("foo").build().code()).isEqualTo("foo");
+    assertThat(Source.builder().setCode(Suppliers.ofInstance("foo")).build().code())
+        .isEqualTo("foo");
+  }
+
+  public void testOriginal() {
+    assertThat(Source.builder().setCode("foo").build().originalCode()).isEqualTo("foo");
+    assertThat(Source.builder().setCode(Suppliers.ofInstance("foo")).build().originalCode())
+        .isEqualTo("foo");
+
+    assertThat(Source.builder().setCode("foo").setOriginalCode("bar").build().originalCode())
+        .isEqualTo("bar");
+    assertThat(
+            Source.builder()
+                .setCode(Suppliers.ofInstance("foo"))
+                .setOriginalCode("bar")
+                .build()
+                .originalCode())
+        .isEqualTo("bar");
+  }
+
+  public void testRuntimes() {
+    Source foo = Source.builder().addRuntime("foo").build();
+    assertThat(foo.runtimes()).containsExactly("foo");
+    assertThat(foo.toBuilder().addRuntime("bar").build().runtimes()).containsExactly("foo", "bar");
+    assertThat(foo.toBuilder().addRuntime("bar").addRuntime("baz").build().runtimes())
+        .containsExactly("foo", "bar", "baz")
+        .inOrder();
+    assertThat(foo.toBuilder().addRuntime("bar").addRuntime("foo").build().runtimes())
+        .containsExactly("foo", "bar")
+        .inOrder();
+  }
+}
