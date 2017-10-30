a933e16b609a6fc865371d3527dce0e79d69358b
johnlenz <johnlenz@google.com>
2017-10-30 13:27:46 -0700

    Small NodeUtil changes:
     - Added NodeUtil.isNamedFunctionExpression
     - deprecated isVarArgsFunction in favor of doesFunctionReferenceOwnArgumentsObject
     - small cleanup of isExpressionResultUsed
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=173940187

--- test/com/google/javascript/jscomp/NodeUtilTest.java
+++ test/com/google/javascript/jscomp/NodeUtilTest.java
@@ -2927,13 +2927,38 @@ public final class NodeUtilTest extends TestCase {
   }
 
   public void testIsVarArgs() {
-    assertTrue(NodeUtil.isVarArgsFunction(getNode("function() {return () => arguments}")));
-    assertFalse(NodeUtil.isVarArgsFunction(getNode("() => arguments")));
+    assertTrue(NodeUtil.doesFunctionReferenceOwnArgumentsObject(
+        getNode("function() {return () => arguments}")));
+    assertFalse(NodeUtil.doesFunctionReferenceOwnArgumentsObject(
+        getNode("() => arguments")));
+  }
+
+  /**
+   * When the left side is a destructuring pattern, generally it's not possible to identify the
+   * RHS for a specific name on the LHS.
+   */
+  public void testIsExpressionResultUsed() {
+    assertThat(NodeUtil.isExpressionResultUsed(getNameNodeFrom("for (x in y) z", "x"))).isTrue();
+    assertThat(NodeUtil.isExpressionResultUsed(getNameNodeFrom("for (x in y) z", "y"))).isTrue();
+    assertThat(NodeUtil.isExpressionResultUsed(getNameNodeFrom("for (x in y) z", "z"))).isFalse();
+
+    assertThat(NodeUtil.isExpressionResultUsed(getNameNodeFrom("for (x of y) z", "x"))).isTrue();
+    assertThat(NodeUtil.isExpressionResultUsed(getNameNodeFrom("for (x of y) z", "y"))).isTrue();
+    assertThat(NodeUtil.isExpressionResultUsed(getNameNodeFrom("for (x of y) z", "z"))).isFalse();
+
+    assertThat(NodeUtil.isExpressionResultUsed(getNameNodeFrom("for (x; y; z) a", "x"))).isFalse();
+    assertThat(NodeUtil.isExpressionResultUsed(getNameNodeFrom("for (x; y; z) a", "y"))).isTrue();
+    assertThat(NodeUtil.isExpressionResultUsed(getNameNodeFrom("for (x; y; z) a", "z"))).isFalse();
+  }
+
+  private Node getNameNodeFrom(String code, String name) {
+    Node ast = parse(code);
+    Node nameNode = getNameNode(ast, name);
+    return nameNode;
   }
 
   private boolean executedOnceTestCase(String code) {
-    Node ast = parse(code);
-    Node nameNode = getNameNode(ast, "x");
+    Node nameNode = getNameNodeFrom(code, "x");
     return NodeUtil.isExecutedExactlyOnce(nameNode);
   }
 
