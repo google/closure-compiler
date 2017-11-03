6ae88df0307fbf9902dd1b3b796b7bcda52fb1f0
tbreisacher <tbreisacher@google.com>
2017-11-03 11:44:09 -0700

    A few small ES6 changes for CollapseAnonymousFunctions:
    * Allow collapsing if the left side is a let or const.
    * Don't allow collapsing if the left side is a destructuring pattern.
    * Don't allow collapsing if the right side is an arrow function.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=174491433

--- test/com/google/javascript/jscomp/CollapseAnonymousFunctionsTest.java
+++ test/com/google/javascript/jscomp/CollapseAnonymousFunctionsTest.java
@@ -47,18 +47,49 @@ public final class CollapseAnonymousFunctionsTest extends CompilerTestCase {
          "function f(){ function x(){} return x }");
   }
 
+  public void testLet() {
+    test(
+        "let f = function() {};",
+        "function f() {}");
+  }
+
+  public void testConst() {
+    test(
+        "let f = function() {};",
+        "function f() {}");
+  }
+
+  public void testArrow() {
+    testSame("function f() { var g = () => this; }");
+    // It would be safe to collapse this one because it doesn't reference 'this' or 'arguments'
+    // but (at least for now) we don't.
+    testSame("let f = () => { console.log(0); };");
+  }
+
+  public void testDestructuring() {
+    testSame("var {name} = function() {};");
+    testSame("let {name} = function() {};");
+    testSame("const {name} = function() {};");
+
+    testSame("var [x] = function() {};");
+    testSame("let [x] = function() {};");
+    testSame("const [x] = function() {};");
+  }
+
   public void testVarNotImmediatelyBelowScriptOrBlock1() {
     testSame("if (x) var f = function(){}");
   }
 
   public void testVarNotImmediatelyBelowScriptOrBlock2() {
-    testSame("var x = 1;" +
-             "if (x == 1) {" +
-             "  var f = function () { alert('b')}" +
-             "} else {" +
-             "  f = function() { alert('c')}" +
-             "}" +
-             "f();");
+    testSame(
+        LINE_JOINER.join(
+            "var x = 1;",
+            "if (x == 1) {",
+            "  var f = function () { alert('b')}",
+            "} else {",
+            "  f = function() { alert('c')}",
+            "}",
+            "f();"));
   }
 
   public void testVarNotImmediatelyBelowScriptOrBlock3() {
@@ -129,11 +160,17 @@ public final class CollapseAnonymousFunctionsTest extends CompilerTestCase {
 
   public void testInnerFunction1() {
     test(
-        "function f() { " +
-        "  var x = 3; var y = function() { return 4; }; return x + y();" +
-        "}",
-        "function f() { " +
-        "  function y() { return 4; } var x = 3; return x + y();" +
-        "}");
+        LINE_JOINER.join(
+            "function f() { ",
+            "  var x = 3;",
+            "  var y = function() { return 4; };",
+            "  return x + y();",
+            "}"),
+        LINE_JOINER.join(
+            "function f() { ",
+            "  function y() { return 4; }",
+            "  var x = 3;",
+            "  return x + y();",
+            "}"));
   }
 }
