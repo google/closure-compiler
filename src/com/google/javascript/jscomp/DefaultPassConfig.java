250fbfc22a51a16ddf23efb5134dd2dcdb1dcb4e
bradfordcsmith <bradfordcsmith@google.com>
2017-12-11 15:42:52 -0800

    enable CrossModuleMethodMotion for ES_2017 output
    The pass still needs to be updated so it can actually move ES6 class methods.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=178684979

--- src/com/google/javascript/jscomp/DefaultPassConfig.java
+++ src/com/google/javascript/jscomp/DefaultPassConfig.java
@@ -2925,7 +2925,7 @@ public final class DefaultPassConfig extends PassConfig {
 
         @Override
         protected FeatureSet featureSet() {
-          return ES5;
+          return ES8_MODULES;
         }
       };
 
