8ca49102c21f505c6e733c1e15d0cf93a21764ce
tbreisacher <tbreisacher@google.com>
2017-12-11 10:15:12 -0800

    Fix a minor code-health-ish TODO.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=178634431

--- src/com/google/javascript/rhino/Node.java
+++ src/com/google/javascript/rhino/Node.java
@@ -2343,7 +2343,7 @@ public class Node implements Serializable {
    * #getDeclaredTypeExpression()} which returns the syntactically specified type.
    */
   @Nullable
-  public JSType getJSType() {  // TODO(johnlenz): make this final
+  public final JSType getJSType() {
     return typei instanceof JSType ? (JSType) typei : null;
   }
 
