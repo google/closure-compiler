4edb896b75294373368dd9838822d36bf37a7f9e
dylandavidson <dylandavidson@google.com>
2018-01-31 10:23:47 -0800

    Add switch case for NTI_SUPPORTED FeatureSet.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=184010090

--- src/com/google/javascript/jscomp/parsing/parser/FeatureSet.java
+++ src/com/google/javascript/jscomp/parsing/parser/FeatureSet.java
@@ -318,6 +318,8 @@ public final class FeatureSet implements Serializable {
       case "es6-impl":
       case "es6":
         return ES6;
+      case "ntiSupported":
+        return NTI_SUPPORTED;
       case "es7":
         return ES7;
       case "es8":
