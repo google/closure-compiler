3481ba5c45cb8075e9b26f087ce18304153a2a7e
blickly <blickly@google.com>
2018-02-14 16:49:37 -0800

    Consider @typedef annotations as having annotated type
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=185766887

--- test/com/google/javascript/jscomp/ijs/ConvertToTypedInterfaceTest.java
+++ test/com/google/javascript/jscomp/ijs/ConvertToTypedInterfaceTest.java
@@ -731,6 +731,15 @@ public final class ConvertToTypedInterfaceTest extends CompilerTestCase {
         });
   }
 
+  public void testGoogModulesWithTypedefExports() {
+    testSame(
+        lines(
+            "goog.module('x.y.z');",
+            "",
+            "/** @typedef {number} */",
+            "exports.Foo;"));
+  }
+
   public void testGoogModulesWithUndefinedExports() {
     testWarning(
         lines(
