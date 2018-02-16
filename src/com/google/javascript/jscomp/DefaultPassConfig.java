892f5aa3649b1c207dc067699d04c196706a6af4
lharker <lharker@google.com>
2018-02-16 12:00:42 -0800

    Enable FlowSensitiveInlineVariables in ES6 output
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=186027319

--- src/com/google/javascript/jscomp/DefaultPassConfig.java
+++ src/com/google/javascript/jscomp/DefaultPassConfig.java
@@ -2844,7 +2844,7 @@ public final class DefaultPassConfig extends PassConfig {
 
         @Override
         public FeatureSet featureSet() {
-          return ES5;
+          return ES8_MODULES;
         }
       };
 
