f12e60b98316e45658c7b7b550366bd571b88010
lharker <lharker@google.com>
2018-04-12 10:45:49 -0700

    Automated g4 rollback of changelist 192534714.
    *** Reason for rollback ***
    
    Rollforward after fix in []
    
    New TGP: []
    
    *** Original change description ***
    
    Automated g4 rollback of changelist 192513895.
    
    *** Reason for rollback ***
    
    Broke some builds
    [][][]htt...
    
    ***
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=192633835

--- src/com/google/javascript/jscomp/TypeInference.java
+++ src/com/google/javascript/jscomp/TypeInference.java
@@ -1200,9 +1200,9 @@ class TypeInference
           && iArgument.isFunction()
           && iArgumentType.isFunctionType()) {
         FunctionType argFnType = iArgumentType.toMaybeFunctionType();
-        boolean declared = iArgument.getJSDocInfo() != null;
-        iArgument.setJSType(
-            matchFunction(restrictedParameter, argFnType, declared));
+        JSDocInfo argJsdoc = iArgument.getJSDocInfo();
+        boolean declared = argJsdoc != null && argJsdoc.containsDeclaration();
+        iArgument.setJSType(matchFunction(restrictedParameter, argFnType, declared));
       }
       i++;
     }
