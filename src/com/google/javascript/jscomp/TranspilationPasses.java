ae0e9d990bb114b4e1ef747249eb4e5b13506285
lharker <lharker@google.com>
2018-04-30 13:30:30 -0700

    Actually disable let/const transpilation in checksOnly
    (the first run at this had a bug in it)
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=194835757

--- src/com/google/javascript/jscomp/TranspilationPasses.java
+++ src/com/google/javascript/jscomp/TranspilationPasses.java
@@ -99,7 +99,6 @@ public class TranspilationPasses {
     passes.add(lateConvertEs6ToEs3);
     passes.add(es6ForOf);
     passes.add(rewriteBlockScopedFunctionDeclaration);
-    passes.add(rewriteBlockScopedDeclaration);
     if (!options.checksOnly) {
       // Don't run these passes in checksOnly mode since all the OTI typechecking & checks passes
       // support the transpiled features.
