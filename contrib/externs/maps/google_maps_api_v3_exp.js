74d4eaae851bda08daae61d4b38642f3bfe47641
blickly <blickly@google.com>
2018-05-18 10:55:33 -0700

    Make a few remaining Maps API externs strictCheckTypes compatible
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=197169635

--- contrib/externs/maps/google_maps_api_v3_exp.js
+++ contrib/externs/maps/google_maps_api_v3_exp.js
@@ -22,7 +22,7 @@
 
 /**
  * @const
- * @suppress {const|duplicate}
+ * @suppress {const,duplicate,strictMissingProperties}
  */
 var google = {};
 
@@ -6539,4 +6539,3 @@ google.maps.visualization.WeightedLocation.prototype.location;
  * @type {number}
  */
 google.maps.visualization.WeightedLocation.prototype.weight;
-
