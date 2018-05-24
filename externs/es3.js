6e05dcca0341df55881ead3d7d92743626e81813
johnlenz <johnlenz@google.com>
2018-05-23 17:26:27 -0700

    Modify the externs now that the type system handles 'symbol' natively
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=197817780

--- externs/es3.js
+++ externs/es3.js
@@ -27,12 +27,8 @@
 
 // START ES6 RETROFIT CODE
 // symbol, Symbol and Symbol.iterator are actually ES6 types but some
-// Some types require them to be part of their definition (such as Array).
+// base types require them to be part of their definition (such as Array).
 
-
-// TODO(johnlenz): symbol should be a primitive type.
-/** @typedef {?} */
-var symbol;
 
 /**
  * @param {string=} opt_description
