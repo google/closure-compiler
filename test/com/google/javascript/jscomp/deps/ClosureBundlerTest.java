07bd5b67efb5c400fb704172df32b2cd94fe090c
johnplaisted <johnplaisted@google.com>
2018-06-15 16:13:16 -0700

    Roll forward: "Allow the transpiler to be configured with a feature set output rather than to ES5."
    Allow ClosureBundler to be configured later with a different transpiler.
    When bundling only remove modules using the transpiler.
    
    NEW: Have the internal ES5 transpiler always transpile to ES5.
    Go back to ClosureBundler having 2 transpilers so that ES6 modules are always transformed.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=200790289

--- test/com/google/javascript/jscomp/deps/ClosureBundlerTest.java
+++ test/com/google/javascript/jscomp/deps/ClosureBundlerTest.java
@@ -20,6 +20,8 @@ import static org.mockito.Mockito.RETURNS_SMART_NULLS;
 import static org.mockito.Mockito.when;
 
 import com.google.common.collect.ImmutableMap;
+import com.google.javascript.jscomp.bundle.TranspilationException;
+import com.google.javascript.jscomp.transpile.BaseTranspiler;
 import com.google.javascript.jscomp.transpile.TranspileResult;
 import com.google.javascript.jscomp.transpile.Transpiler;
 import java.io.IOException;
@@ -140,9 +142,10 @@ public final class ClosureBundlerTest extends TestCase {
     String input =
         "import {x} from './other.js';\n"
             + "export {x as y};"
-            + "var local;\n"
+            + "let local;\n"
             + "export function foo() { return local; }\n";
-    ClosureBundler bundler = new ClosureBundler().withPath("nested/path/foo.js");
+    ClosureBundler bundler =
+        new ClosureBundler(BaseTranspiler.LATEST_TRANSPILER).withPath("nested/path/foo.js");
     StringBuilder sb = new StringBuilder();
     bundler.appendRuntimeTo(sb);
     bundler.appendTo(
@@ -163,10 +166,61 @@ public final class ClosureBundlerTest extends TestCase {
                 + "    return module$nested$path$other.x;\n"
                 + "  }}});\n"
                 + "  var module$nested$path$other = $$require(\"nested/path/other.js\");\n"
-                + "  var local;\n"
+                + "  let local;\n"
                 + "  function foo() {\n"
                 + "    return local;\n"
                 + "  }\n"
                 + "}, \"nested/path/foo.js\", [\"nested/path/other.js\"]);\n");
+  }
+
+  public void testPassThroughIfNoTranspilationNeeded() throws Exception {
+    String input = "/** Hello Comments! */ const s = 0;\n  let intended;";
+    ClosureBundler bundler = new ClosureBundler(BaseTranspiler.LATEST_TRANSPILER);
+    StringBuilder sb = new StringBuilder();
+    bundler.appendTo(
+        sb,
+        SimpleDependencyInfo.builder("", "").build(),
+        input);
+    assertThat(sb.toString()).isEqualTo(input);
+  }
+
+  public void testCommentsAndFormattingRemovedWithTranspilation() throws Exception {
+    String input = "/** Hello Comments! */ const s = 0;\n  let intended;";
+    ClosureBundler bundler = new ClosureBundler(BaseTranspiler.ES5_TRANSPILER);
+    StringBuilder sb = new StringBuilder();
+    bundler.appendTo(
+        sb,
+        SimpleDependencyInfo.builder("", "").build(),
+        input);
+    assertThat(sb.toString()).isEqualTo("var s = 0;\nvar intended;\n");
+  }
+
+  public void testFullWidthLowLineWithDefaultTranspilerIsOkay() throws Exception {
+    // The last character is something the compiler doesn't handle correctly
+    String input = "var ａｅｓｔｈｅｔｉｃ＿";
+    ClosureBundler bundler = new ClosureBundler();
+    StringBuilder sb = new StringBuilder();
+    bundler.appendTo(
+        sb,
+        SimpleDependencyInfo.builder("", "").build(),
+        input);
+    assertThat(sb.toString()).isEqualTo(input);
+  }
+
+  // TODO(johnplaisted): If / when the compiler can parse full width low line in identifiers
+  // this should be okay to be transpiled.
+  public void testFullWidthLowLineInTranspiledCodeIsError() throws Exception {
+    // The last character is something the compiler doesn't handle correctly
+    String input = "let ａｅｓｔｈｅｔｉｃ＿";
+    ClosureBundler bundler = new ClosureBundler(BaseTranspiler.ES5_TRANSPILER);
+    StringBuilder sb = new StringBuilder();
+    try {
+      bundler.appendTo(sb, SimpleDependencyInfo.builder("", "").build(), input);
+      fail("Expected an exception");
+    } catch (TranspilationException e) {
+      assertThat(e)
+          .hasMessageThat()
+          .contains("Parse error. Character '＿' (U+FF3F) is not a valid identifier start char");
+    }
   }
 }
