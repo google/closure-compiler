25a54dec67fce3749813e09c03daba1c5b0c5a98
Bradford C. Smith <bradfordcsmith@google.com>
2018-07-25 08:33:04 -0700

    Rollfoward "Inline require'd symbols when rewriting ES6 modules."
    This is needed as some later stages of the compiler specifically check for well-known Closure properties, such as goog.asserts.assert. So `const {assert} = goog.require('goog.asserts'); assert(0);` must be rewritten to `goog.asserts.assert(0);` to be recognized correctly later.
    
    NEW: Don't detach the variable statement if there is one so that it can be re-exported.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=205889853

--- test/com/google/javascript/jscomp/Es6RewriteModulesWithGoogInteropTest.java
+++ test/com/google/javascript/jscomp/Es6RewriteModulesWithGoogInteropTest.java
@@ -117,17 +117,38 @@ public final class Es6RewriteModulesWithGoogInteropTest extends CompilerTestCase
 
   public void testGoogRequireForProvide() {
     testModules(
-        "const y = goog.require('closure.provide');\nexport {};",
+        lines("const y = goog.require('closure.provide');", "use(y, y.x)", "export {};"),
         lines(
             "const y$$module$testcode = closure.provide;",
+            "use(closure.provide, closure.provide.x);",
+            "/** @const */ var module$testcode = {};"));
+  }
+
+  public void testGoogRequireForProvideWithDestructure() {
+    testModules(
+        lines("const {a, b:c} = goog.require('closure.provide');", "use(a, a.z, c)", "export {};"),
+        lines(
+            "const {a:a$$module$testcode, b:c$$module$testcode} = closure.provide;",
+            "use(closure.provide.a, closure.provide.a.z, closure.provide.b);",
             "/** @const */ var module$testcode = {};"));
   }
 
   public void testGoogRequireForGoogModule() {
     testModules(
-        "const y = goog.require('closure.module');\nexport {};",
+        lines("const y = goog.require('closure.module');", "use(y, y.x)", "export {};"),
         lines(
             "const y$$module$testcode = module$exports$closure$module;",
+            "use(module$exports$closure$module, module$exports$closure$module.x);",
+            "/** @const */ var module$testcode = {};"));
+  }
+
+  public void testGoogRequireForGoogModuleWithDestructure() {
+    testModules(
+        lines("const {a, b:c} = goog.require('closure.module');", "use(a, a.z, c)", "export {};"),
+        lines(
+            "const {a:a$$module$testcode, b:c$$module$testcode} = module$exports$closure$module;",
+            "use(module$exports$closure$module.a, module$exports$closure$module.a.z,",
+            "  module$exports$closure$module.b);",
             "/** @const */ var module$testcode = {};"));
   }
 
@@ -141,9 +162,22 @@ public final class Es6RewriteModulesWithGoogInteropTest extends CompilerTestCase
 
   public void testGoogRequireForLegacyGoogModule() {
     testModules(
-        "const y = goog.require('closure.legacy.module');\nexport {};",
+        lines("const y = goog.require('closure.legacy.module');", "use(y, y.x)", "export {};"),
         lines(
-            "const y$$module$testcode = closure.legacy.module;",
+            "const y$$module$testcode=closure.legacy.module;",
+            "use(closure.legacy.module, closure.legacy.module.x);",
+            "/** @const */ var module$testcode = {};"));
+  }
+
+  public void testGoogRequireForLegacyGoogModuleWithDestructure() {
+    testModules(
+        lines(
+            "const {a, b:c} = goog.require('closure.legacy.module');",
+            "use(a, a.z, c)",
+            "export {};"),
+        lines(
+            "const {a:a$$module$testcode,b:c$$module$testcode} = closure.legacy.module;",
+            "use(closure.legacy.module.a, closure.legacy.module.a.z, closure.legacy.module.b);",
             "/** @const */ var module$testcode = {};"));
   }
 
@@ -195,19 +229,6 @@ public final class Es6RewriteModulesWithGoogInteropTest extends CompilerTestCase
     testModulesError(
         "export var x;\nvar bar = goog.require('closure.provide');",
         LHS_OF_GOOG_REQUIRE_MUST_BE_CONST);
-  }
-
-  public void testGoogRequiresDestructuring_rewrite() {
-    testModules(
-        lines(
-            "export {};", "const {foo, bar} = goog.require('closure.provide');", "use(foo, bar);"),
-        lines(
-            "const {",
-            "  foo: foo$$module$testcode,",
-            "  bar: bar$$module$testcode,",
-            "} = closure.provide;",
-            "use(foo$$module$testcode, bar$$module$testcode);",
-            "/** @const */ var module$testcode = {};"));
 
     testModulesError(
         lines("export {};", "var {foo, bar} = goog.require('closure.provide');", "use(foo, bar);"),
@@ -264,5 +285,16 @@ public final class Es6RewriteModulesWithGoogInteropTest extends CompilerTestCase
     testModulesError("goog.module.get('closure.module'); export {}", MODULE_USES_GOOG_MODULE_GET);
     testModulesError(
         "{ goog.module.get('closure.module'); } export {}", MODULE_USES_GOOG_MODULE_GET);
+  }
+
+  public void testExportSpecRequiredSymbol() {
+    testModules(
+        lines(
+            "const {a} = goog.require('closure.provide');",
+            "export {a};"),
+        lines(
+            "const {a:a$$module$testcode} = closure.provide",
+            "/** @const */ var module$testcode={};",
+            "/** @const */ module$testcode.a = a$$module$testcode;"));
   }
 }
