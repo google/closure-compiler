de799ceb65c37776fc2b629f1fcc9af44c119909
lharker <lharker@google.com>
2018-09-06 10:58:56 -0700

    Enable JSC_INEXISTENT_PROPERTY_WITH_SUGGESTION as a warning by default
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=211834406

--- src/com/google/javascript/jscomp/TypeCheck.java
+++ src/com/google/javascript/jscomp/TypeCheck.java
@@ -110,7 +110,7 @@ public final class TypeCheck implements NodeTraversal.Callback, CompilerPass {
           "Property {0} never defined on {1}");
 
   static final DiagnosticType INEXISTENT_PROPERTY_WITH_SUGGESTION =
-      DiagnosticType.disabled(
+      DiagnosticType.warning(
           "JSC_INEXISTENT_PROPERTY_WITH_SUGGESTION",
           "Property {0} never defined on {1}. Did you mean {2}?");
 
