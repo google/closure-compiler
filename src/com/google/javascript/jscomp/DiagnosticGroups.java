707f45e3d5bbd84e6ba6563f82b9fafc355d46fe
Maikel Steneker <maikel.steneker@tiobe.com>
2018-10-19 11:49:05 -0700

    Added missing space.
    Closes https://github.com/google/closure-compiler/pull/3124.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=217906145

--- src/com/google/javascript/jscomp/DiagnosticGroups.java
+++ src/com/google/javascript/jscomp/DiagnosticGroups.java
@@ -146,7 +146,7 @@ public class DiagnosticGroups {
           + "newCheckTypes, "
           + "nonStandardJsDocs, "
           + "missingSourcesWarnings, "
-          + "polymer,"
+          + "polymer, "
           + "reportUnknownTypes, "
           + "suspiciousCode, "
           + "strictCheckTypes, "
