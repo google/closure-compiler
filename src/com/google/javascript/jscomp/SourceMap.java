google/closure-compiler

commit c143b8c7f50b6ce86e6e1944eae50d6f664019e2

Keep original identifier when it's missing in original source maps.
Closes https://github.com/google/closure-compiler/pull/3100.

-------------
Created by MOE: https://github.com/google/moe
MOE_MIGRATED_REVID=221644447

src/com/google/javascript/jscomp/SourceMap.java
@@ -179,7 +179,10 @@ public final class SourceMap {
         sourceFile = sourceMapping.getOriginalFile();
         lineNo = sourceMapping.getLineNumber();
         charNo = sourceMapping.getColumnPosition();
-        originalName = sourceMapping.getIdentifier();
+        String identifier = sourceMapping.getIdentifier();
+        if (sourceMapping != null && !identifier.isEmpty()) {
+          originalName = identifier;
+        }
       }
     }
 

test/com/google/javascript/jscomp/CommandLineRunnerTest.java
@@ -2282,10 +2282,10 @@ public final class CommandLineRunnerTest {
                 + "\"src\":\"function log(a){console.log(a)}log(\\\"one.js\\\");\\n\","
                 + "\"path\":\"bar.js\","
                 + "\"source_map\":\"{\\n\\\"version\\\":3,\\n\\\"file\\\":\\\"bar.js\\\",\\n"
-                + "\\\"lineCount\\\":1,\\n\\\"mappings\\\":\\\"AAAAA,QAASA,IAAGC,CAACC,CAADD,CAAIA,"
-                + "CACdE,OAAAA,IAAAA,CAAYD,CAAZC,CADcF,CAGhBD,GAAAA,CAAIC,QAAJD;\\\",\\n"
-                + "\\\"sources\\\":[\\\"one.js\\\"],\\n\\\"names\\\":[\\\"log\\\",\\\"\\\","
-                + "\\\"a\\\",\\\"console\\\"]\\n}\\n\"}]");
+                + "\\\"lineCount\\\":1,\\n\\\"mappings\\\":\\\"AAAAA,QAASA,IAAG,CAACC,CAAD,CAAI,CACdC,"
+                + "OAAAA,IAAAA,CAAYD,CAAZC,CADc,CAGhBF,GAAAA,CAAI,QAAJA;\\\",\\n"
+                + "\\\"sources\\\":[\\\"one.js\\\"],\\n"
+                + "\\\"names\\\":[\\\"log\\\",\\\"a\\\",\\\"console\\\"]\\n}\\n\"}]");
   }
 
   @Test

commit cfaf272ae6e3037e589c738df264e74cc766c759

Fix a NPE crash in the gwt compiler introduced by c143b8c7f50b6ce86e6e1944eae50d6f664019e2

src/com/google/javascript/jscomp/SourceMap.java
@@ -180,7 +180,7 @@ public final class SourceMap {
         lineNo = sourceMapping.getLineNumber();
         charNo = sourceMapping.getColumnPosition();
         String identifier = sourceMapping.getIdentifier();
-        if (sourceMapping != null && !identifier.isEmpty()) {
+        if (sourceMapping != null && identifier != null && !identifier.isEmpty()) {
           originalName = identifier;
         }
       }