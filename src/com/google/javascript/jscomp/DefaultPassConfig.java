c8db81681867f39f46e99837752ceb007fe5619c
lharker <lharker@google.com>
2018-12-13 16:35:17 -0800

    Enable InlineProperties for ES2017 output
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=225457648

--- src/com/google/javascript/jscomp/DefaultPassConfig.java
+++ src/com/google/javascript/jscomp/DefaultPassConfig.java
@@ -2634,7 +2634,7 @@ public final class DefaultPassConfig extends PassConfig {
 
         @Override
         protected FeatureSet featureSet() {
-          return ES5;
+          return ES8_MODULES;
         }
       };
 
