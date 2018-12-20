39eb9c1febc01dafbaa478f4da7a1d2bf62bee83
nickreid <nickreid@google.com>
2018-12-19 16:04:20 -0800

    Enable `DevirtualizePrototypeProperties` for ES8 + modules featureset.
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=226249580

--- src/com/google/javascript/jscomp/DefaultPassConfig.java
+++ src/com/google/javascript/jscomp/DefaultPassConfig.java
@@ -2430,7 +2430,7 @@ public final class DefaultPassConfig extends PassConfig {
 
         @Override
         protected FeatureSet featureSet() {
-          return ES5;
+          return ES8_MODULES;
         }
       };
 
