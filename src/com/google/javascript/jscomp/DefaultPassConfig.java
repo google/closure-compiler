a86eb380935e7bc14f77d72ae67ad92d38a89abb
nickreid <nickreid@google.com>
2019-01-09 09:46:34 -0800

    Automated g4 rollback.
    *** Original change description ***
    
    Enable `DevirtualizePrototypeProperties` for ES8_MODULES.
    
    ***
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=228534117

--- src/com/google/javascript/jscomp/DefaultPassConfig.java
+++ src/com/google/javascript/jscomp/DefaultPassConfig.java
@@ -2416,7 +2416,7 @@ public final class DefaultPassConfig extends PassConfig {
 
         @Override
         protected FeatureSet featureSet() {
-          return ES8_MODULES;
+          return ES5;
         }
       };
 
