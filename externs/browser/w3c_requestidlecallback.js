5807c0f206ed5d4edbb0a002f6f76cc124efecc6
Peter Donald <peter@realityforge.org>
2019-02-08 16:54:26 -0800

    Make sure that callback returns void to match WebIDL
    Closes https://github.com/google/closure-compiler/pull/3227.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=233151134

--- externs/browser/w3c_requestidlecallback.js
+++ externs/browser/w3c_requestidlecallback.js
@@ -33,7 +33,7 @@ var IdleCallbackOptions;
 
 /**
  * Schedules a callback to run when the browser is idle.
- * @param {function(!IdleDeadline)} callback Called when the browser is idle.
+ * @param {function(!IdleDeadline): void} callback Called when the browser is idle.
  * @param {number|IdleCallbackOptions=} opt_options If set, gives the browser a time in ms by which
  *     it must execute the callback. No timeout enforced otherwise.
  * @return {number} A handle that can be used to cancel the scheduled callback.
