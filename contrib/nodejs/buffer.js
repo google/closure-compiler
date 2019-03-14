a62381d91abd19e12ede0281f31045194d71c123
Mikolaj Konarski <mikolaj.konarski@gmail.com>
2019-03-14 19:31:28 +0100

    Add Buffer.alloc to nodejs externs

--- contrib/nodejs/buffer.js
+++ contrib/nodejs/buffer.js
@@ -55,3 +55,11 @@ buffer.SlowBuffer.prototype.slice;
  * @return {string}
  */
 buffer.SlowBuffer.prototype.toString;
+
+/**
+ * @param {number} size
+ * @param {(string|Buffer|number)=} fill
+ * @param {number=} encoding
+ * @return {Buffer}
+ */
+buffer.Buffer.alloc;
