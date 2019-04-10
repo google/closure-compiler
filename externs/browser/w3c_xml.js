b334f060b03f3881bba5f67d5b357303d3c504ed
bradfordcsmith <bradfordcsmith@google.com>
2019-04-09 19:01:06 -0700

    Add `timeout` property to XMLHttpRequest externs definition
    Also update the links to the XMLHttpRequest spec to the current version.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=242785010

--- externs/browser/w3c_xml.js
+++ externs/browser/w3c_xml.js
@@ -25,8 +25,7 @@
  *    XMLHttpRequest - Fully type annotated
  *
  * @see http://www.w3.org/TR/DOM-Level-3-XPath/xpath.html
- * @see http://www.w3.org/TR/XMLHttpRequest/
- * @see http://www.w3.org/TR/XMLHttpRequest2/
+ * @see https://xhr.spec.whatwg.org/
  *
  * @externs
  * @author stevey@google.com (Steve Yegge)
@@ -281,7 +280,7 @@ XPathNamespace.prototype.ownerElement;
 XPathNamespace.XPATH_NAMESPACE_NODE = 13;
 
 /**
- * From http://www.w3.org/TR/XMLHttpRequest/
+ * From https://xhr.spec.whatwg.org/#xmlhttprequest
  *
  * (Draft)
  *
@@ -291,7 +290,7 @@ XPathNamespace.XPATH_NAMESPACE_NODE = 13;
  *
  * @constructor
  * @implements {EventTarget}
- * @see http://www.w3.org/TR/XMLHttpRequest/#xmlhttprequest-object
+ * @see https://xhr.spec.whatwg.org/#xmlhttprequest
  */
 function XMLHttpRequest() {}
 
@@ -313,7 +312,7 @@ XMLHttpRequest.prototype.dispatchEvent = function(evt) {};
  * @param {?string=} opt_user
  * @param {?string=} opt_password
  * @return {undefined}
- * @see http://www.w3.org/TR/XMLHttpRequest/#the-open()-method
+ * @see https://xhr.spec.whatwg.org/#the-open()-method
  */
 XMLHttpRequest.prototype.open = function(method, url, opt_async, opt_user,
     opt_password) {};
@@ -322,112 +321,118 @@ XMLHttpRequest.prototype.open = function(method, url, opt_async, opt_user,
  * @param {string} header
  * @param {string} value
  * @return {undefined}
- * @see http://www.w3.org/TR/XMLHttpRequest/#the-setrequestheader()-method
+ * @see https://xhr.spec.whatwg.org/#the-setrequestheader()-method
  */
 XMLHttpRequest.prototype.setRequestHeader = function(header, value) {};
 
 /**
  * @param {ArrayBuffer|ArrayBufferView|Blob|Document|FormData|string=} opt_data
  * @return {undefined}
- * @see http://www.w3.org/TR/XMLHttpRequest/#the-send()-method
+ * @see https://xhr.spec.whatwg.org/#the-send()-method
  */
 XMLHttpRequest.prototype.send = function(opt_data) {};
 
 /**
  * @return {undefined}
- * @see http://www.w3.org/TR/XMLHttpRequest/#the-abort()-method
+ * @see https://xhr.spec.whatwg.org/#the-abort()-method
  */
 XMLHttpRequest.prototype.abort = function() {};
 
 /**
  * @return {string}
- * @see http://www.w3.org/TR/XMLHttpRequest/#the-getallresponseheaders()-method
+ * @see https://xhr.spec.whatwg.org/#the-getallresponseheaders()-method
  */
 XMLHttpRequest.prototype.getAllResponseHeaders = function() {};
 
 /**
  * @param {string} header
  * @return {string}
- * @see http://www.w3.org/TR/XMLHttpRequest/#the-getresponseheader()-method
+ * @see https://xhr.spec.whatwg.org/#the-getresponseheader()-method
  */
 XMLHttpRequest.prototype.getResponseHeader = function(header) {};
 
 /**
  * @type {string}
- * @see http://www.w3.org/TR/XMLHttpRequest/#the-responsetext-attribute
+ * @see https://xhr.spec.whatwg.org/#the-responsetext-attribute
  */
 XMLHttpRequest.prototype.responseText;
 
 /**
  * This is not supported in any IE browser (as of August 2016).
  * @type {string}
- * @see https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/responseURL
+ * @see https://xhr.spec.whatwg.org/#the-responseurl-attribute
  */
 XMLHttpRequest.prototype.responseURL;
 
 /**
  * @type {Document}
- * @see http://www.w3.org/TR/XMLHttpRequest/#the-responsexml-attribute
+ * @see https://xhr.spec.whatwg.org/#the-responsexml-attribute
  */
 XMLHttpRequest.prototype.responseXML;
 
 /**
  * @type {number}
- * @see http://www.w3.org/TR/XMLHttpRequest/#the-readystate-attribute
+ * @see https://xhr.spec.whatwg.org/#dom-xmlhttprequest-readystate
  */
 XMLHttpRequest.prototype.readyState;
 
 /**
  * @type {number}
- * @see http://www.w3.org/TR/XMLHttpRequest/#the-status-attribute
+ * @see https://xhr.spec.whatwg.org/#the-status-attribute
  */
 XMLHttpRequest.prototype.status;
 
 /**
  * @type {string}
- * @see http://www.w3.org/TR/XMLHttpRequest/#the-statustext-attribute
+ * @see https://xhr.spec.whatwg.org/#the-statustext-attribute
  */
 XMLHttpRequest.prototype.statusText;
 
 /**
+ * @type {number}
+ * @see https://xhr.spec.whatwg.org/#the-timeout-attribute
+ */
+XMLHttpRequest.prototype.timeout;
+
+/**
  * @type {?function(!Event)}
- * @see http://www.w3.org/TR/XMLHttpRequest/#handler-xhr-onreadystatechange
+ * @see https://xhr.spec.whatwg.org/#event-handlers
  */
 XMLHttpRequest.prototype.onreadystatechange;
 
 /**
  * @type {?function(!Event)}
- * @see http://www.w3.org/TR/XMLHttpRequest/#handler-xhr-onerror
+ * @see https://xhr.spec.whatwg.org/#event-handlers
  */
 XMLHttpRequest.prototype.onerror;
 
 /**
  * @type {number}
- * @see https://www.w3.org/TR/XMLHttpRequest/#states
+ * @see https://xhr.spec.whatwg.org/#states
  */
 XMLHttpRequest.UNSENT;
 
 /**
  * @type {number}
- * @see https://www.w3.org/TR/XMLHttpRequest/#states
+ * @see https://xhr.spec.whatwg.org/#states
  */
 XMLHttpRequest.OPENED;
 
 /**
  * @type {number}
- * @see https://www.w3.org/TR/XMLHttpRequest/#states
+ * @see https://xhr.spec.whatwg.org/#states
  */
 XMLHttpRequest.HEADERS_RECEIVED;
 
 /**
  * @type {number}
- * @see https://www.w3.org/TR/XMLHttpRequest/#states
+ * @see https://xhr.spec.whatwg.org/#states
  */
 XMLHttpRequest.LOADING;
 
 /**
  * @type {number}
- * @see https://www.w3.org/TR/XMLHttpRequest/#states
+ * @see https://xhr.spec.whatwg.org/#states
  */
 XMLHttpRequest.DONE;
 
@@ -438,7 +443,7 @@ XMLHttpRequest.DONE;
  * @param {?Element=} opt_form An optional form to use for constructing the form
  *     data set.
  * @constructor
- * @see http://www.w3.org/TR/XMLHttpRequest2/#the-formdata-interface
+ * @see https://xhr.spec.whatwg.org/#interface-formdata
  */
 function FormData(opt_form) {}
 
