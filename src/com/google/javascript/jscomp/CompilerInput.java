02061ed510c465c746dfc9a93d94085aba8d506a
Dmitrii Tikhomirov <chani@me.com>
2019-04-25 18:24:34 +0200

    restore PersistentInputStore

--- src/com/google/javascript/jscomp/CompilerInput.java
+++ src/com/google/javascript/jscomp/CompilerInput.java
@@ -110,6 +110,15 @@ public class CompilerInput extends DependencyInfo.Base implements SourceAst {
     this(new JsAst(file), isExtern);
   }
 
+  /**
+   * Using the RecoverableJsAst, creates a CompilerInput that can be reset() to be safe to reuse
+   * in multiple compiler invocations.
+   */
+  public static CompilerInput makePersistentInput(SourceFile file) {
+    SourceAst ast = new RecoverableJsAst(new JsAst(file), true);
+    return new CompilerInput(ast, file.isExtern());
+  }
+
   /** Returns a name for this input. Must be unique across all inputs. */
   @Override
   public InputId getInputId() {
