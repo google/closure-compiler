1d61d33ca660e4e059b5e79077e9c89c54640112
rishipal <rishipal@google.com>
2019-07-29 11:41:03 -0700

    Enable preserving comments when applying fixes via JSFlume
    JSFlume refactoring deletes comments present in the JS source code. Hence enabling the JSCompiler option to preserve all comments. This option currently preserves all comments but serializes only the comments associated with import statements.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=260545388

--- src/com/google/javascript/refactoring/RefactoringDriver.java
+++ src/com/google/javascript/refactoring/RefactoringDriver.java
@@ -94,7 +94,7 @@ public final class RefactoringDriver {
 
     options.setChecksOnly(true);
     options.setContinueAfterErrors(true);
-    options.setParseJsDocDocumentation(Config.JsDocParsing.INCLUDE_DESCRIPTIONS_NO_WHITESPACE);
+    options.setParseJsDocDocumentation(Config.JsDocParsing.INCLUDE_ALL_COMMENTS);
     options.setCheckSuspiciousCode(true);
     options.setCheckSymbols(true);
     options.setCheckTypes(true);
