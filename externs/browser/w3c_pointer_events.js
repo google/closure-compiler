9feebeb9723066fcee3f77d1244a219431722497
Marcel Prestel <admin@marci4.de>
2019-07-31 22:18:17 -0700

    [FIX] Element.hasPointerCapture is not supported
    Fixes #3445
    
    Closes https://github.com/google/closure-compiler/pull/3446.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=261061135

--- externs/browser/w3c_pointer_events.js
+++ externs/browser/w3c_pointer_events.js
@@ -54,6 +54,13 @@ Element.prototype.setPointerCapture = function(pointerId) {};
  */
 Element.prototype.releasePointerCapture = function(pointerId) {};
 
+/**
+ * @param {number} pointerId
+ * @see https://www.w3.org/TR/pointerevents/#dom-element-haspointercapture
+ * @return {boolean}
+ */
+Element.prototype.hasPointerCapture = function(pointerId) {};
+
 
 /**
  * @record
