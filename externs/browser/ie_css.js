e4651500c0bb13e7debe79000c8f8dc545a895e3
jubi <jubi@google.com>
2020-01-03 14:23:22 -0800

    Change CSSProperties.MsUserSelect => CSSProperties.msUserSelect
    It appears based on @leewilliams test at https://jsfiddle.net/sfbw4pay/24/ that this should be msUserSelect.
    
    -------------
    Created by MOE: https://github.com/google/moe
    MOE_MIGRATED_REVID=288049171

--- externs/browser/ie_css.js
+++ externs/browser/ie_css.js
@@ -253,7 +253,7 @@ CSSProperties.prototype.MsWritingMode;
 CSSProperties.prototype.MsZoom;
 
 /** @type {string} */
-CSSProperties.prototype.MsUserSelect;
+CSSProperties.prototype.msUserSelect;
 
 // See: http://msdn.microsoft.com/en-us/library/windows/apps/Hh702466.aspx
 
