03f43c652a336bf4f187fbe2028e2bfd18af5a47
Closure Team <noreply@google.com>
2020-06-03 13:32:52 +0000

    Create a polyfill for TypedArray#copyWithin.
    PiperOrigin-RevId: 314519873

---
+++ test/com/google/javascript/jscomp/runtime_tests/polyfill_tests/typed_array_copywithin_test.js
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2020 The Closure Compiler Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+goog.module('jscomp.runtime_tests.polyfill_tests.typedarray_copywithin_test');
+goog.setTestOnly();
+
+const testSuite = goog.require('goog.testing.testSuite');
+
+testSuite({
+  testCopyWithin_uint8() {
+    assertObjectEquals(
+        new Uint8Array([1, 3, 3]), new Uint8Array([1, 2, 3]).copyWithin(1, 2));
+  },
+
+  testCopyWithin_uint8Clamped() {
+    assertObjectEquals(
+        new Uint8ClampedArray([1, 3, 3]),
+        new Uint8ClampedArray([1, 2, 3]).copyWithin(1, 2));
+  },
+
+  testCopyWithin_int8() {
+    assertObjectEquals(
+        new Int8Array([1, 3, 3]), new Int8Array([1, 2, 3]).copyWithin(1, 2));
+  },
+
+  testCopyWithin_uint16() {
+    assertObjectEquals(
+        new Uint16Array([1, 3, 3]),
+        new Uint16Array([1, 2, 3]).copyWithin(1, 2));
+  },
+
+  testCopyWithin_int16() {
+    assertObjectEquals(
+        new Int16Array([1, 3, 3]), new Int16Array([1, 2, 3]).copyWithin(1, 2));
+  },
+
+  testCopyWithin_uint32() {
+    assertObjectEquals(
+        new Uint32Array([1, 3, 3]),
+        new Uint32Array([1, 2, 3]).copyWithin(1, 2));
+  },
+
+  testCopyWithin_int32() {
+    assertObjectEquals(
+        new Int32Array([1, 3, 3]), new Int32Array([1, 2, 3]).copyWithin(1, 2));
+  },
+
+  testCopyWithin_float32() {
+    assertObjectEquals(
+        new Float32Array([1, 3, 3]),
+        new Float32Array([1, 2, 3]).copyWithin(1, 2));
+  },
+
+  testCopyWithin_float64() {
+    assertObjectEquals(
+        new Float64Array([1, 3, 3]),
+        new Float64Array([1, 2, 3]).copyWithin(1, 2));
+  },
+});
