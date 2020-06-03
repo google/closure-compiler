61dd381d39c52a9dd00f6f4ea90c45d65a1cd486
Closure Team <noreply@google.com>
2020-06-03 20:03:13 +0000

    Rollback of changelist 314368658: Create a polyfill for TypedArray#fill.
    PiperOrigin-RevId: 314588706

--- src/com/google/javascript/jscomp/js/es6_runtime.js
+++ src/com/google/javascript/jscomp/js/es6_runtime.js
@@ -1,5 +1,5 @@
 /*
- * Copyright 2020 The Closure Compiler Authors.
+ * Copyright 2016 The Closure Compiler Authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -32,7 +32,6 @@
 'require es6/set';
 'require es6/string';
 'require es6/symbol';
-'require es6/typed_array';
 'require es6/util/createtemplatetagfirstarg';
 'require es6/util/arrayfromiterable';
 'require es6/util/arrayfromiterator';
