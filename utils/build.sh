2f69388db7dabae4bb7d03bc1ebaa1e1e158a9e0
Christian Lewis <ctjl96@gmail.com>
2020-08-12 17:15:32 -0500

    Removed debugging utils/ directory

--- utils/build.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# Build the compiler without GWT.
-mvn -DskipTests -pl externs/pom.xml,pom-main.xml,pom-main-shaded.xml -e
--- utils/run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# Run the local build of CC in target/. Make sure to run build.sh before running
-# this script.
-java -jar target/closure-compiler-1.0-SNAPSHOT.jar $@
