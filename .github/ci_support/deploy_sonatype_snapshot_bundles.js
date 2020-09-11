e0d1fcf4b0fa1ed6441e6ceeb5004be33283f615
nickreid <nickreid@google.com>
2020-09-11 09:32:34 -0700

    Stop using Travis in favour of GHA.
    This CL deletes all the files that only Travis was using, as well as the config, which should be enough to disable Travis builds.
    
    PiperOrigin-RevId: 331163165

--- .github/ci_support/deploy_sonatype_snapshot_bundles.js
+++ .github/ci_support/deploy_sonatype_snapshot_bundles.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
 /*
- * Copyright 2020 Google LLC
+ * Copyright 2020 The Closure Compiler Authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *     https://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
