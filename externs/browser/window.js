cf2aeba83c91df53e3ed364edeae78957d135844
Patrick Dähne <pdaehne@gmail.com>
2020-10-14 16:17:23 +0200

    Fixed comment

--- externs/browser/window.js
+++ externs/browser/window.js
@@ -173,7 +173,7 @@ function setTimeout(callback, opt_delay, callbackParams) {}
  */
 function hasOwnProperty (propertyName) {}
 
-/*
+/**
  * @param {function()} callback
  * @return {undefined}
  * @see https://developer.mozilla.org/en-US/docs/Web/API/WindowOrWorkerGlobalScope/queueMicrotask
