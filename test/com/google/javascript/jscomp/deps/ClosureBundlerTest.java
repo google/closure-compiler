83ce3f1011434185ab46320a1b1b4237a125b00b
Closure Team <noreply@google.com>
2020-11-09 14:37:46 -0800

    Closure allows Trusted Types in loading resources for test/workaround functions. RELNOTES: Allow Trusted Types in loading resources.
    PiperOrigin-RevId: 341488098

--- test/com/google/javascript/jscomp/deps/ClosureBundlerTest.java
+++ test/com/google/javascript/jscomp/deps/ClosureBundlerTest.java
@@ -107,8 +107,7 @@ public final class ClosureBundlerTest {
     new ClosureBundler()
         .useEval(true)
         .appendTo(sb, TRADITIONAL, "\"a string\"");
-    assertThat(sb.toString())
-        .isEqualTo("eval(this.CLOSURE_EVAL_PREFILTER(\"\\x22a string\\x22\"));\n");
+    assertThat(sb.toString()).isEqualTo("eval(\"\\x22a string\\x22\");\n");
   }
 
   @Test
@@ -119,10 +118,7 @@ public final class ClosureBundlerTest {
         .withSourceUrl("URL")
         .appendTo(sb, TRADITIONAL, "\"a string\"");
     assertThat(sb.toString())
-        .isEqualTo(
-            "eval(this.CLOSURE_EVAL_PREFILTER(\"\\x22a string\\x22\\n"
-                + "//# sourceURL\\x3dURL\\n"
-                + "\"));\n");
+        .isEqualTo("eval(\"\\x22a string\\x22\\n//# sourceURL\\x3dURL\\n\");\n");
   }
 
   @Test
