bd7a627d8681143669d978153632c96206fdbdcc
nickreid <nickreid@google.com>
2020-12-09 13:03:11 -0800

    Apply jscomp_java_library macro to //third_party/java_src/jscomp/java/com/google/debugging/sourcemap
    PiperOrigin-RevId: 346618927

--- src/com/google/debugging/sourcemap/super/com/google/debugging/sourcemap/SourceMapObjectParser.java
+++ src/com/google/debugging/sourcemap/super/com/google/debugging/sourcemap/SourceMapObjectParser.java
@@ -16,13 +16,109 @@
 
 package com.google.debugging.sourcemap;
 
-/** Delegates to SouceMapObjectParserJs */
+import com.google.common.collect.ImmutableList;
+import elemental2.core.JSONType;
+import java.util.Collections;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import jsinterop.annotations.JsPackage;
+import jsinterop.annotations.JsProperty;
+import jsinterop.annotations.JsType;
+import jsinterop.base.Js;
+import jsinterop.base.JsPropertyMap;
+
+/**
+ * ClientSide implementation of the source map parser using jsinterop.
+ *
+ * <p>Note: This is invoked through super sourcing of SourceMapObjectParser.
+ */
 public class SourceMapObjectParser {
+
   public static SourceMapObject parse(String contents) throws SourceMapParseException {
-    // Do not implement lots of logic in super sourcing, rather delegate back to proper Java,
-    // since this means that tooling will run on the source.
-    return SourceMapObjectParserJs.parse(contents);
+    JsonMap sourceMap = Js.uncheckedCast(getJSON().parse(contents));
+    SourceMapObject.Builder builder = SourceMapObject.builder();
+
+    builder.setVersion(sourceMap.version);
+    builder.setFile(sourceMap.file);
+    // Line count is no longer part of the source map spec. -1 is the magic "not provided" value
+    builder.setLineCount(-1);
+    builder.setMappings(sourceMap.mappings);
+    builder.setSourceRoot(sourceMap.sourceRoot);
+
+    if (sourceMap.sections != null) {
+      ImmutableList.Builder<SourceMapSection> listBuilder = ImmutableList.builder();
+      for (Section section : sourceMap.sections) {
+        listBuilder.add(buildSection(section));
+      }
+      builder.setSections(listBuilder.build());
+    } else {
+      builder.setSections(null);
+    }
+
+    builder.setSources(sourceMap.sources);
+    builder.setNames(sourceMap.names);
+
+    Map<String, Object> extensions = new LinkedHashMap<>();
+    sourceMap.forEach(
+        (key) -> {
+          if (key.startsWith("x_")) {
+            extensions.put(key, sourceMap.get(key));
+          }
+        });
+    builder.setExtensions(Collections.unmodifiableMap(extensions));
+    return builder.build();
   }
 
-  private SourceMapObjectParser() {}
+  private static SourceMapSection buildSection(Section section) throws SourceMapParseException {
+    int line = section.offset.line;
+    int column = section.offset.column;
+
+    if (section.map != null && section.url != null) {
+      throw new SourceMapParseException(
+          "Invalid map format: section may not have both 'map' and 'url'");
+    } else if (section.url != null) {
+      return SourceMapSection.forURL(section.url, line, column);
+    } else if (section.map != null) {
+      return SourceMapSection.forMap(String.valueOf(section.map), line, column);
+    }
+
+    throw new SourceMapParseException(
+        "Invalid map format: section must have either 'map' or 'url'");
+  }
+
+  @JsProperty(namespace = JsPackage.GLOBAL)
+  private static native JSONType getJSON();
+
+  @JsType(
+      isNative = true,
+      namespace = JsPackage.GLOBAL,
+      name = "com_google_debugging_sourcemap_SourceMapObjectParserJs$JsonMap")
+  private abstract static class JsonMap implements JsPropertyMap<Object> {
+    int version;
+    String file;
+    String mappings;
+    String sourceRoot;
+    Section[] sections;
+    String[] sources;
+    String[] names;
+  }
+
+  @JsType(
+      isNative = true,
+      namespace = JsPackage.GLOBAL,
+      name = "com_google_debugging_sourcemap_SourceMapObjectParserJs$Section")
+  private abstract static class Section {
+    Offset offset;
+    String url;
+    String map;
+  }
+
+  @JsType(
+      isNative = true,
+      namespace = JsPackage.GLOBAL,
+      name = "com_google_debugging_sourcemap_SourceMapObjectParserJs$Offset")
+  private abstract static class Offset {
+    int line;
+    int column;
+  }
 }
