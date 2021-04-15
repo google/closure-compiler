5b4a9e86b73e30b953d77674af04a80d68e3d478
lharker <lharker@google.com>
2021-04-15 14:20:23 -0700

    Double MAX_STEPS for data flow analysis
    PiperOrigin-RevId: 368716735

--- src/com/google/javascript/jscomp/DataFlowAnalysis.java
+++ src/com/google/javascript/jscomp/DataFlowAnalysis.java
@@ -96,7 +96,7 @@ abstract class DataFlowAnalysis<N, L extends LatticeElement> {
    * If you just have a special case, consider calling
    * {@link #analyze(int)} instead.
    */
-  public static final int MAX_STEPS = 1000000;
+  public static final int MAX_STEPS = 2000000;
 
   /**
    * Constructs a data flow analysis.
