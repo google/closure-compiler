db7fde193964ae103d54f2d72d73e322c1a39d4c
Christina Zhang <christinazhang@google.com>
2021-07-29 14:19:46 -0700

    Add simple integrations tests with checks-only/with transpilation mode for logical assignment operators.
    Move logical assignment feature to ES_NEXT_IN. Perform integration tests with checks only and with transpilation mode.
    
    PiperOrigin-RevId: 387667272

---
+++ test/com/google/javascript/jscomp/integration/ES2021IntegrationTest.java
@@ -0,0 +1,144 @@
+/*
+ * Copyright 2021 The Closure Compiler Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.javascript.jscomp.integration;
+
+import static com.google.javascript.jscomp.base.JSCompStrings.lines;
+
+import com.google.common.collect.ImmutableList;
+import com.google.javascript.jscomp.CompilationLevel;
+import com.google.javascript.jscomp.CompilerOptions;
+import com.google.javascript.jscomp.CompilerOptions.DevMode;
+import com.google.javascript.jscomp.CompilerOptions.LanguageMode;
+import com.google.javascript.jscomp.GoogleCodingConvention;
+import com.google.javascript.jscomp.WarningLevel;
+import com.google.javascript.jscomp.testing.TestExternsBuilder;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+/** Integration tests related to ES2021 features like logical assignments */
+@RunWith(JUnit4.class)
+public final class ES2021IntegrationTest extends IntegrationTestCase {
+
+  /** Creates a CompilerOptions object with google coding conventions. */
+  @Override
+  protected CompilerOptions createCompilerOptions() {
+    CompilerOptions options = new CompilerOptions();
+    options.setLanguageIn(LanguageMode.ECMASCRIPT_NEXT);
+    options.setDevMode(DevMode.EVERY_PASS);
+    options.setCodingConvention(new GoogleCodingConvention());
+    WarningLevel.VERBOSE.setOptionsForWarningLevel(options);
+    return options;
+  }
+
+  private CompilerOptions checksOnlyCompilerOptions() {
+    CompilerOptions options = createCompilerOptions();
+    options.setChecksOnly(true);
+    return options;
+  }
+
+  private CompilerOptions fullyOptimizedCompilerOptions() {
+    CompilerOptions options = createCompilerOptions();
+    CompilationLevel.ADVANCED_OPTIMIZATIONS.setOptionsForCompilationLevel(options);
+    options.setLanguageOut(LanguageMode.ECMASCRIPT_2020);
+    return options;
+  }
+
+  @Test
+  public void logicalAssignmentSimpleInlineChecksOnly() {
+    CompilerOptions options = checksOnlyCompilerOptions();
+
+    testNoWarnings(
+        options,
+        lines(
+            "function f() {", //
+            "  let x = '';",
+            "  x ||= '1';",
+            "  x &&= '2';",
+            "  x ??= '3'",
+            "  return x;",
+            "}",
+            "window.f = f;"));
+  }
+
+  @Test
+  public void logicalAssignmentSimpleChecksOnly1() {
+    CompilerOptions options = checksOnlyCompilerOptions();
+
+    testNoWarnings(options, "let x, y, z; alert(x ??= (y &&= z))");
+  }
+
+  @Test
+  public void logicalAssignmentSimpleInlineTranspiledOutput() {
+    CompilerOptions options = fullyOptimizedCompilerOptions();
+
+    test(
+        options,
+        lines(
+            "function f() {", //
+            "  let x = '';",
+            "  x ||= '1';",
+            "  x &&= '2';",
+            "  x ??= '3'",
+            "  return x;",
+            "}",
+            "window.f = f;"),
+        lines(
+            "window.a = function() {", //
+            "  return '2';",
+            "};"));
+  }
+
+  @Test
+  public void logicalAssignmentSimpleTranspiledOutput1() {
+    CompilerOptions options = fullyOptimizedCompilerOptions();
+
+    test(
+        options,
+        lines(
+            "let x = 0, y = {}", //
+            "alert(x ??= y)"),
+        lines(
+            "let a = 0, b = {}", //
+            "alert(a ?? (a = b));"));
+  }
+
+  @Test
+  public void logicalAssignmentSimpleTranspiledOutput2() {
+    CompilerOptions options = fullyOptimizedCompilerOptions();
+
+    test(
+        options,
+        lines(
+            "let w, x, y, z;", //
+            "alert(w ||= (x &&= (y ??= z)))"),
+        lines(
+            "let a, b, c;", //
+            "alert(a || (a = b && (b = c ?? (c = void 0))));"));
+  }
+
+  @Test
+  public void logicalAssignmentSimpleTranspiledOutput3() {
+    CompilerOptions options = fullyOptimizedCompilerOptions();
+
+    externs =
+        ImmutableList.of(
+            new TestExternsBuilder().addExtra("let w, x, y, z").buildExternsFile("externs"));
+
+    test(options, "w ||= (x &&= (y ??= z))", "w || (w = x && (x = y ?? (y = z)));");
+  }
+}
