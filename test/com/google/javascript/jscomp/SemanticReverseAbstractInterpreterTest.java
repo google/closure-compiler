1af7aa6f6751dc46e36cc431fb8aee938d0a55f0
kak <kak@google.com>
2022-03-08 07:19:06 -0800

    Remove unused expressions (which will soon be compile errors).
    PiperOrigin-RevId: 433204278

--- test/com/google/javascript/jscomp/SemanticReverseAbstractInterpreterTest.java
+++ test/com/google/javascript/jscomp/SemanticReverseAbstractInterpreterTest.java
@@ -33,7 +33,7 @@ import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public final class SemanticReverseAbstractInterpreterTest extends CompilerTypeTestCase {
-{ new GoogleCodingConvention(); }  private ReverseAbstractInterpreter interpreter;
+  private ReverseAbstractInterpreter interpreter;
   private TypedScope functionScope;
 
   @Override
