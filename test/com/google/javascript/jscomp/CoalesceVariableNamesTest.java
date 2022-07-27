594ee5c4c1664beb14a3fd5943b8f394cc298cce
Delina Kiflom <delinak@google.com>
2022-07-27 15:57:51 -0700

    Add tests to shows current behavior of var replacement in CoalesceVariableNames for static blocks
    PiperOrigin-RevId: 463699877

--- test/com/google/javascript/jscomp/CoalesceVariableNamesTest.java
+++ test/com/google/javascript/jscomp/CoalesceVariableNamesTest.java
@@ -372,6 +372,55 @@ public final class CoalesceVariableNamesTest extends CompilerTestCase {
   }
 
   @Test
+  public void testCoalesceVarinStaticBlock() {
+    // TODO(b/235871861): Wrong replacement of var names.
+    test(
+        lines(
+            "function foo() {",
+            "   var x = 0;",
+            "   var y = 2;",
+            "   const C = class{",
+            "      static{",
+            "       print(x);",
+            "      }",
+            "   }",
+            "}"),
+        lines(
+            "function foo() {",
+            "   var x = 0;",
+            "   x = 2;",
+            "   x = class{",
+            "     static{",
+            "      print(x);",
+            "     }",
+            "   }",
+            "}"));
+
+    // test var replacment for inner functions as comparison for class static blocks
+    test(
+        lines(
+            "function foo() {",
+            "   var x = 0;",
+            "   var y = 2;",
+            "   const C = class {",
+            "      function(){",
+            "       print(x);",
+            "      }",
+            "   }",
+            "}"),
+        lines(
+            "function foo() {",
+            "   var x = 0;",
+            "   var y = 2;",
+            "   y = class {",
+            "     function(){",
+            "      print(x);",
+            "     }",
+            "  }",
+            "}"));
+  }
+
+  @Test
   public void testBug65688660() {
     test(
         lines(
@@ -996,8 +1045,9 @@ public final class CoalesceVariableNamesTest extends CompilerTestCase {
   @Test
   public void testUsePseudoNames() {
     usePseudoName = true;
-    inFunction("var x   = 0; print(x  ); var   y = 1; print(  y)",
-               "var x_y = 0; print(x_y);     x_y = 1; print(x_y)");
+    inFunction(
+        "var x   = 0; print(x  ); var   y = 1; print(  y)",
+        "var x_y = 0; print(x_y);     x_y = 1; print(x_y)");
 
     inFunction(
         "var x_y = 1; var x    = 0; print(x   ); var     y = 1; print(   y); print(x_y);",
