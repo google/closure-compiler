c248583975a3b0d236da02ebce7d3a5194b16f3a
blickly <blickly@google.com>
2022-09-15 14:59:05 -0700

    Use `instanceof` instead of `getClass` inside `equals`
    PiperOrigin-RevId: 474664924

--- src/com/google/javascript/rhino/InputId.java
+++ src/com/google/javascript/rhino/InputId.java
@@ -64,8 +64,9 @@ public class InputId implements Serializable {
   @Override
   public boolean equals(Object obj) {
     if (this == obj) return true;
-    if (obj == null) return false;
-    if (getClass() != obj.getClass()) return false;
+    if (!(obj instanceof InputId)) {
+      return false;
+    }
     return id.equals(((InputId) obj).id);
   }
 
