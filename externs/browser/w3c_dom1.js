6470e5b40453490c5759061cb84e6b7de1554a7f
Closure Team <noreply@google.com>
2024-12-04 14:51:34 -0800

    Support TrustedScript in setAttribute
    PiperOrigin-RevId: 702870662

--- externs/browser/w3c_dom1.js
+++ externs/browser/w3c_dom1.js
@@ -708,7 +708,7 @@ Element.prototype.removeAttributeNode = function(oldAttr) {};
 
 /**
  * @param {string} name
- * @param {string|number|boolean|!TrustedHTML|!TrustedScriptURL}
+ * @param {string|number|boolean|!TrustedHTML|!TrustedScriptURL|!TrustedScript}
  *     value Values are converted to strings with ToString, so we accept number
  *     and boolean since both convert easily to strings.
  * @return {undefined}
