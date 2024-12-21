c546bc7f8c2452086d1b3825daa3a1331950b535
lharker <lharker@google.com>
2024-12-20 16:18:07 -0800

    [Rollback] Add StringBuilder.repeat from Java 21
    https://docs.oracle.com/en/java/javase/21/docs/api/java.base/java/lang/StringBuilder.html#repeat(int,int)
    
    PiperOrigin-RevId: 708454194

--- src/com/google/javascript/jscomp/LightweightMessageFormatter.java
+++ src/com/google/javascript/jscomp/LightweightMessageFormatter.java
@@ -339,7 +339,7 @@ public final class LightweightMessageFormatter extends AbstractMessageFormatter 
 
         // nice spaces for the line number
         int spaces = numberLength - Integer.toString(lineNumber).length();
-        builder.repeat(" ", spaces);
+        builder.append(" ".repeat(spaces));
         builder.append(lineNumber);
         builder.append("| ");
 
