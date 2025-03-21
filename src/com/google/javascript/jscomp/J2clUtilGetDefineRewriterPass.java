5ab717546982c538044740a348525a2d4fdc7093
kevinoconnor <kevinoconnor@google.com>
2025-03-21 08:22:44 -0700

    Add correctness checks to J2clUtilGetDefineRewriterPass
    This will enforce constraints on how we expect these calls to appear in the code
    in order to properly optimize them. The primary thing we'll be looking for is
    a corresponding `addSystemPropertyFromGoogDefine` call which help ensure that
    the user wired things up correctly and we'll be replacing the code with the
    correct define value.
    
    PiperOrigin-RevId: 739187942

--- src/com/google/javascript/jscomp/J2clUtilGetDefineRewriterPass.java
+++ src/com/google/javascript/jscomp/J2clUtilGetDefineRewriterPass.java
@@ -19,6 +19,9 @@ import com.google.common.collect.ImmutableSet;
 import com.google.javascript.jscomp.NodeTraversal.AbstractPostOrderCallback;
 import com.google.javascript.rhino.IR;
 import com.google.javascript.rhino.Node;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.jspecify.annotations.Nullable;
 
 /** A normalization pass to re-write Util.$getDefine calls to make them work in compiled mode. */
 public class J2clUtilGetDefineRewriterPass extends AbstractPostOrderCallback
@@ -36,6 +39,11 @@ public class J2clUtilGetDefineRewriterPass extends AbstractPostOrderCallback
       return;
     }
     defines = compiler.getDefineNames();
+
+    var checkGetDefineCalls = new CheckGetDefineCalls();
+    NodeTraversal.traverse(compiler, root, checkGetDefineCalls);
+    checkGetDefineCalls.validateRegisteredDefines();
+
     NodeTraversal.traverse(compiler, root, this);
   }
 
@@ -81,5 +89,92 @@ public class J2clUtilGetDefineRewriterPass extends AbstractPostOrderCallback
   private static boolean isUtilGetDefineMethodName(String fnName) {
     // TODO: Switch this to the filename + property name heuristic which is less brittle.
     return fnName != null && fnName.endsWith(".$getDefine") && fnName.contains("Util");
+  }
+
+  static final DiagnosticType J2CL_SYSTEM_GET_PROPERTY_CONSTANT_NAME =
+      DiagnosticType.error(
+          "JSC_J2CL_SYSTEM_GET_PROPERTY_CONSTANT_NAME",
+          "Calls to System.getProperty must use a String literal for the property name");
+
+  static final DiagnosticType J2CL_SYSTEM_GET_PROPERTY_UNKNOWN_PROPERTY =
+      DiagnosticType.error(
+          "JSC_J2CL_SYSTEM_GET_PROPERTY_UNKNOWN_PROPERTY",
+          "Unknown system property \"{0}\", are you missing a call to"
+              + " jre.addSystemPropertyFromGoogDefine?");
+
+  static final DiagnosticType J2CL_ADD_SYSTEM_PROPERTY_CONSTANT_NAME =
+      DiagnosticType.error(
+          "JSC_J2CL_ADD_SYSTEM_PROPERTY_CONSTANT_NAME",
+          "Calls to addSystemPropertyFromGoogDefine must use a String literal for the define"
+              + " name");
+
+  static final DiagnosticType J2CL_ADD_SYSTEM_PROPERTY_UNKNOWN_DEFINE =
+      DiagnosticType.error(
+          "JSC_J2CL_ADD_SYSTEM_PROPERTY_UNKNOWN_DEFINE", "Unknown goog.define with name: {0}");
+
+  private final class CheckGetDefineCalls extends AbstractPostOrderCallback {
+    private final Map<String, Node> defineNameToAddSystemPropertyCall = new LinkedHashMap<>();
+    private final Map<String, Node> defineNameToGetDefineCall = new LinkedHashMap<>();
+
+    @Override
+    public void visit(NodeTraversal t, Node n, @Nullable Node parent) {
+      if (isAddSystemPropertyFromGoogDefine(n)) {
+        String defineName = getDefineNameParameter(n);
+        if (defineName != null) {
+          defineNameToAddSystemPropertyCall.put(defineName, n);
+        } else {
+          compiler.report(JSError.make(n, J2CL_ADD_SYSTEM_PROPERTY_CONSTANT_NAME));
+        }
+      } else if (isUtilGetDefineCall(n)) {
+        String defineName = getDefineNameParameter(n);
+        if (defineName != null) {
+          defineNameToGetDefineCall.put(defineName, n);
+        } else {
+          compiler.report(JSError.make(n, J2CL_SYSTEM_GET_PROPERTY_CONSTANT_NAME));
+        }
+      }
+    }
+
+    private void validateRegisteredDefines() {
+      // Validate that all addSystemPropertyFromGoogDefine calls reference a known define.
+      for (var entry : defineNameToAddSystemPropertyCall.entrySet()) {
+        if (!defines.contains(entry.getKey())) {
+          compiler.report(
+              JSError.make(
+                  entry.getValue(), J2CL_ADD_SYSTEM_PROPERTY_UNKNOWN_DEFINE, entry.getKey()));
+        }
+      }
+
+      // Validate that Util.$getDefine() calls reference a define registered with
+      // addSystemPropertyFromGoogDefine.
+      for (var entry : defineNameToGetDefineCall.entrySet()) {
+        // Tolerate System.getProperty() calls that reference something that is never goog.define'd.
+        // This provides compatibility with code that never intended reference a define (ex. third
+        // party code).
+        if (!defines.contains(entry.getKey())) {
+          continue;
+        }
+
+        if (!defineNameToAddSystemPropertyCall.containsKey(entry.getKey())) {
+          compiler.report(
+              JSError.make(
+                  entry.getValue(), J2CL_SYSTEM_GET_PROPERTY_UNKNOWN_PROPERTY, entry.getKey()));
+        }
+      }
+    }
+
+    private static @Nullable String getDefineNameParameter(Node callNode) {
+      Node nameNode = callNode.getSecondChild();
+      if (nameNode == null || !nameNode.isString()) {
+        return null;
+      }
+      return nameNode.getString();
+    }
+
+    private static boolean isAddSystemPropertyFromGoogDefine(Node n) {
+      return n.isCall()
+          && n.getFirstChild().getQualifiedName() != null
+          && n.getFirstChild().getQualifiedName().endsWith("addSystemPropertyFromGoogDefine");
+    }
   }
 }
--- test/com/google/javascript/jscomp/J2clUtilGetDefineRewriterPassTest.java
+++ test/com/google/javascript/jscomp/J2clUtilGetDefineRewriterPassTest.java
@@ -15,6 +15,11 @@
  */
 package com.google.javascript.jscomp;
 
+import static com.google.javascript.jscomp.J2clUtilGetDefineRewriterPass.J2CL_ADD_SYSTEM_PROPERTY_CONSTANT_NAME;
+import static com.google.javascript.jscomp.J2clUtilGetDefineRewriterPass.J2CL_ADD_SYSTEM_PROPERTY_UNKNOWN_DEFINE;
+import static com.google.javascript.jscomp.J2clUtilGetDefineRewriterPass.J2CL_SYSTEM_GET_PROPERTY_CONSTANT_NAME;
+import static com.google.javascript.jscomp.J2clUtilGetDefineRewriterPass.J2CL_SYSTEM_GET_PROPERTY_UNKNOWN_PROPERTY;
+
 import com.google.javascript.jscomp.ProcessDefines.Mode;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -48,6 +53,7 @@ public class J2clUtilGetDefineRewriterPassTest extends CompilerTestCase {
         var a = {};
         a.b = {}
         /** @define {boolean} */ a.b.c = goog.define('a.b.c', true);
+        jre.addSystemPropertyFromGoogDefine('a.b.c', a.b.c);
         nativebootstrap.Util.$getDefine('a.b.c', 'def');
         """,
         """
@@ -55,6 +61,7 @@ public class J2clUtilGetDefineRewriterPassTest extends CompilerTestCase {
         a.b = {}
         /** @define {boolean} */ a.b.c = true;
         var jscomp$defines$a$b$c = a.b.c;
+        jre.addSystemPropertyFromGoogDefine('a.b.c', a.b.c);
         ('def', String(jscomp$defines$a$b$c));
         """);
     test(
@@ -62,6 +69,7 @@ public class J2clUtilGetDefineRewriterPassTest extends CompilerTestCase {
         var a = {};
         a.b = {}
         /** @define {boolean} */ a.b.c = goog.define('a.b.c', true);
+        jre.addSystemPropertyFromGoogDefine('a.b.c', a.b.c);
         nativebootstrap.Util.$getDefine('a.b.c');
         """,
         """
@@ -69,29 +77,84 @@ public class J2clUtilGetDefineRewriterPassTest extends CompilerTestCase {
         a.b = {}
         /** @define {boolean} */ a.b.c = true;
         var jscomp$defines$a$b$c = a.b.c;
+        jre.addSystemPropertyFromGoogDefine('a.b.c', a.b.c);
         (null, String(jscomp$defines$a$b$c));
         """);
     test(
         """
         /** @define {boolean} */ var x = goog.define('x', 1);
+        jre.addSystemPropertyFromGoogDefine('x', x);
         /** @define {boolean} */ var y = goog.define('y', x);
+        jre.addSystemPropertyFromGoogDefine('y', y);
         nativebootstrap.Util.$getDefine('x');
         nativebootstrap.Util.$getDefine('y');
         """,
         """
         /** @define {boolean} */ var x = 1;
         var jscomp$defines$x = x;
+        jre.addSystemPropertyFromGoogDefine('x', x);
         /** @define {boolean} */ var y = x;
         var jscomp$defines$y = y;
+        jre.addSystemPropertyFromGoogDefine('y', y);
         (null, String(jscomp$defines$x));
         (null, String(jscomp$defines$y));
         """);
-    test("nativebootstrap.Util.$getDefine('COMPILED');", "(null, String(COMPILED));");
+    test(
+        """
+        jre.addSystemPropertyFromGoogDefine('COMPILED', COMPILED);
+        nativebootstrap.Util.$getDefine('COMPILED');
+        """,
+        """
+        jre.addSystemPropertyFromGoogDefine('COMPILED', COMPILED);
+        (null, String(COMPILED));
+        """);
   }
 
   @Test
   public void testUtilGetDefine_notDefined() {
     test("nativebootstrap.Util.$getDefine('not.defined');", "null;");
     test("nativebootstrap.Util.$getDefine('not.defined', 'def');", "'def';");
+  }
+
+  @Test
+  public void testUtilGetDefine_missingAddSystemProperty() {
+    testError(
+        """
+        /** @define {boolean} */ var a = goog.define('a', true);
+        nativebootstrap.Util.$getDefine('a');
+        """,
+        J2CL_SYSTEM_GET_PROPERTY_UNKNOWN_PROPERTY);
+  }
+
+  @Test
+  public void testUtilGetDefine_notAStringLiteral() {
+    testError(
+        """
+        var defineName = 'a';
+        /** @define {boolean} */ var a = goog.define('a', true);
+        nativebootstrap.Util.$getDefine(defineName);
+        """,
+        J2CL_SYSTEM_GET_PROPERTY_CONSTANT_NAME);
+  }
+
+  @Test
+  public void testAddSystemProperty_notDefined() {
+    testError(
+        """
+        var notADefine = true;
+        jre.addSystemPropertyFromGoogDefine('not_a_define', notADefine);
+        """,
+        J2CL_ADD_SYSTEM_PROPERTY_UNKNOWN_DEFINE);
+  }
+
+  @Test
+  public void testAddSystemProperty_notAStringLiteral() {
+    testError(
+        """
+        var defineName = 'someDefine';
+        /** @define {boolean} */ var someDefine = goog.define('someDefine', true);
+        jre.addSystemPropertyFromGoogDefine(defineName, someDefine);
+        """,
+        J2CL_ADD_SYSTEM_PROPERTY_CONSTANT_NAME);
   }
 }
