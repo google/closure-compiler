caaa930dc09b60fd5488d5310c5eac492dfe81c8
tflo <tflo@google.com>
2025-12-11 14:10:10 -0800

    Remove unneeded `this.` prefixes from `FeatureCollector.java`
    PiperOrigin-RevId: 843368949

--- src/com/google/javascript/jscomp/parsing/FeatureCollector.java
+++ src/com/google/javascript/jscomp/parsing/FeatureCollector.java
@@ -32,12 +32,12 @@ public final class FeatureCollector {
   private FeatureSet scriptFeatures = FeatureSet.BARE_MINIMUM;
 
   public FeatureSet allFeatures() {
-    return this.scriptFeatures;
+    return scriptFeatures;
   }
 
   public @Nullable FeatureContext visitSingleNode(@Nullable FeatureContext context, Node node) {
     if (context != null) {
-      this.recordScriptFeatures(context, node);
+      recordScriptFeatures(context, node);
     }
     return contextFor(context, node);
   }
@@ -46,111 +46,110 @@ public final class FeatureCollector {
     switch (node.getToken()) {
       case FUNCTION -> {
         if (node.isAsyncGeneratorFunction()) {
-          this.addScriptFeature(Feature.ASYNC_GENERATORS);
+          addScriptFeature(Feature.ASYNC_GENERATORS);
         }
         if (node.isArrowFunction()) {
-          this.addScriptFeature(Feature.ARROW_FUNCTIONS);
+          addScriptFeature(Feature.ARROW_FUNCTIONS);
         }
         if (node.isAsyncFunction()) {
-          this.addScriptFeature(Feature.ASYNC_FUNCTIONS);
+          addScriptFeature(Feature.ASYNC_FUNCTIONS);
         }
         if (node.isGeneratorFunction()) {
-          this.addScriptFeature(Feature.GENERATORS);
+          addScriptFeature(Feature.GENERATORS);
         }
 
         if (context.equals(FeatureContext.BLOCK_SCOPE)) {
-          this.scriptFeatures = this.scriptFeatures.with(Feature.BLOCK_SCOPED_FUNCTION_DECLARATION);
+          scriptFeatures = scriptFeatures.with(Feature.BLOCK_SCOPED_FUNCTION_DECLARATION);
         }
       }
       case PARAM_LIST, CALL, NEW -> {}
       case STRING_KEY -> {
         if (node.isShorthandProperty() && context.equals(FeatureContext.OBJECT_LITERAL)) {
-          this.addScriptFeature(Feature.SHORTHAND_OBJECT_PROPERTIES);
+          addScriptFeature(Feature.SHORTHAND_OBJECT_PROPERTIES);
         }
       }
       case DEFAULT_VALUE -> {
         if (context.equals(FeatureContext.PARAM_LIST)) {
-          this.addScriptFeature(Feature.DEFAULT_PARAMETERS);
+          addScriptFeature(Feature.DEFAULT_PARAMETERS);
         }
       }
       case GETTER_DEF -> {
-        this.addScriptFeature(Feature.GETTER);
+        addScriptFeature(Feature.GETTER);
         if (context.equals(FeatureContext.CLASS_MEMBERS)) {
-          this.addScriptFeature(Feature.CLASS_GETTER_SETTER);
+          addScriptFeature(Feature.CLASS_GETTER_SETTER);
         }
       }
-      case REGEXP -> this.addScriptFeature(Feature.REGEXP_SYNTAX);
+      case REGEXP -> addScriptFeature(Feature.REGEXP_SYNTAX);
       case SETTER_DEF -> {
-        this.addScriptFeature(Feature.SETTER);
+        addScriptFeature(Feature.SETTER);
         if (context.equals(FeatureContext.CLASS_MEMBERS)) {
-          this.addScriptFeature(Feature.CLASS_GETTER_SETTER);
+          addScriptFeature(Feature.CLASS_GETTER_SETTER);
         }
       }
       case BLOCK -> {
         if (context.equals(FeatureContext.CLASS_MEMBERS)) {
-          this.addScriptFeature(Feature.CLASS_STATIC_BLOCK);
+          addScriptFeature(Feature.CLASS_STATIC_BLOCK);
         }
       }
       case EMPTY -> {
         if (context.equals(FeatureContext.CATCH)) {
-          this.addScriptFeature(Feature.OPTIONAL_CATCH_BINDING);
+          addScriptFeature(Feature.OPTIONAL_CATCH_BINDING);
         }
       }
       case ITER_REST -> {
         if (context.equals(FeatureContext.PARAM_LIST)) {
-          this.addScriptFeature(Feature.REST_PARAMETERS);
+          addScriptFeature(Feature.REST_PARAMETERS);
         } else {
-          this.addScriptFeature(Feature.ARRAY_PATTERN_REST);
+          addScriptFeature(Feature.ARRAY_PATTERN_REST);
         }
       }
-      case ITER_SPREAD -> this.addScriptFeature(Feature.SPREAD_EXPRESSIONS);
-      case OBJECT_REST -> this.addScriptFeature(Feature.OBJECT_PATTERN_REST);
-      case OBJECT_SPREAD -> this.addScriptFeature(Feature.OBJECT_LITERALS_WITH_SPREAD);
-      case BIGINT -> this.addScriptFeature(Feature.BIGINT);
-      case EXPONENT, ASSIGN_EXPONENT -> this.addScriptFeature(Feature.EXPONENT_OP);
-      case TAGGED_TEMPLATELIT, TEMPLATELIT -> this.addScriptFeature(Feature.TEMPLATE_LITERALS);
-      case NEW_TARGET -> this.addScriptFeature(Feature.NEW_TARGET);
+      case ITER_SPREAD -> addScriptFeature(Feature.SPREAD_EXPRESSIONS);
+      case OBJECT_REST -> addScriptFeature(Feature.OBJECT_PATTERN_REST);
+      case OBJECT_SPREAD -> addScriptFeature(Feature.OBJECT_LITERALS_WITH_SPREAD);
+      case BIGINT -> addScriptFeature(Feature.BIGINT);
+      case EXPONENT, ASSIGN_EXPONENT -> addScriptFeature(Feature.EXPONENT_OP);
+      case TAGGED_TEMPLATELIT, TEMPLATELIT -> addScriptFeature(Feature.TEMPLATE_LITERALS);
+      case NEW_TARGET -> addScriptFeature(Feature.NEW_TARGET);
       case COMPUTED_PROP -> {
-        this.addScriptFeature(Feature.COMPUTED_PROPERTIES);
+        addScriptFeature(Feature.COMPUTED_PROPERTIES);
         boolean isGetter = node.getBooleanProp(Node.COMPUTED_PROP_GETTER);
         boolean isSetter = node.getBooleanProp(Node.COMPUTED_PROP_SETTER);
         boolean isClassMember = context.equals(FeatureContext.CLASS_MEMBERS);
         if (isGetter) {
-          this.addScriptFeature(Feature.GETTER);
+          addScriptFeature(Feature.GETTER);
         } else if (isSetter) {
-          this.addScriptFeature(Feature.SETTER);
+          addScriptFeature(Feature.SETTER);
         }
         if ((isGetter || isSetter) && isClassMember) {
-          this.addScriptFeature(Feature.CLASS_GETTER_SETTER);
+          addScriptFeature(Feature.CLASS_GETTER_SETTER);
         }
       }
       case OPTCHAIN_GETPROP, OPTCHAIN_CALL, OPTCHAIN_GETELEM ->
-          this.addScriptFeature(Feature.OPTIONAL_CHAINING);
-      case COALESCE -> this.addScriptFeature(Feature.NULL_COALESCE_OP);
-      case DYNAMIC_IMPORT -> this.addScriptFeature(Feature.DYNAMIC_IMPORT);
-      case ASSIGN_OR, ASSIGN_AND -> this.addScriptFeature(Feature.LOGICAL_ASSIGNMENT);
+          addScriptFeature(Feature.OPTIONAL_CHAINING);
+      case COALESCE -> addScriptFeature(Feature.NULL_COALESCE_OP);
+      case DYNAMIC_IMPORT -> addScriptFeature(Feature.DYNAMIC_IMPORT);
+      case ASSIGN_OR, ASSIGN_AND -> addScriptFeature(Feature.LOGICAL_ASSIGNMENT);
       case ASSIGN_COALESCE -> {
-        this.addScriptFeature(Feature.NULL_COALESCE_OP);
-        this.addScriptFeature(Feature.LOGICAL_ASSIGNMENT);
+        addScriptFeature(Feature.NULL_COALESCE_OP);
+        addScriptFeature(Feature.LOGICAL_ASSIGNMENT);
       }
-      case FOR_OF -> this.addScriptFeature(Feature.FOR_OF);
-      case FOR_AWAIT_OF -> this.addScriptFeature(Feature.FOR_AWAIT_OF);
-      case IMPORT, EXPORT -> this.addScriptFeature(Feature.MODULES);
-      case CONST -> this.addScriptFeature(Feature.CONST_DECLARATIONS);
-      case LET -> this.addScriptFeature(Feature.LET_DECLARATIONS);
-      case CLASS -> this.addScriptFeature(Feature.CLASSES);
-      case MEMBER_FUNCTION_DEF -> this.addScriptFeature(Feature.MEMBER_DECLARATIONS);
-      case MEMBER_FIELD_DEF, COMPUTED_FIELD_DEF ->
-          this.addScriptFeature(Feature.PUBLIC_CLASS_FIELDS);
-      case SUPER -> this.addScriptFeature(Feature.SUPER);
-      case ARRAY_PATTERN -> this.addScriptFeature(Feature.ARRAY_DESTRUCTURING);
-      case OBJECT_PATTERN -> this.addScriptFeature(Feature.OBJECT_DESTRUCTURING);
+      case FOR_OF -> addScriptFeature(Feature.FOR_OF);
+      case FOR_AWAIT_OF -> addScriptFeature(Feature.FOR_AWAIT_OF);
+      case IMPORT, EXPORT -> addScriptFeature(Feature.MODULES);
+      case CONST -> addScriptFeature(Feature.CONST_DECLARATIONS);
+      case LET -> addScriptFeature(Feature.LET_DECLARATIONS);
+      case CLASS -> addScriptFeature(Feature.CLASSES);
+      case MEMBER_FUNCTION_DEF -> addScriptFeature(Feature.MEMBER_DECLARATIONS);
+      case MEMBER_FIELD_DEF, COMPUTED_FIELD_DEF -> addScriptFeature(Feature.PUBLIC_CLASS_FIELDS);
+      case SUPER -> addScriptFeature(Feature.SUPER);
+      case ARRAY_PATTERN -> addScriptFeature(Feature.ARRAY_DESTRUCTURING);
+      case OBJECT_PATTERN -> addScriptFeature(Feature.OBJECT_DESTRUCTURING);
       default -> {}
     }
   }
 
   private void addScriptFeature(Feature x) {
-    this.scriptFeatures = this.scriptFeatures.with(x);
+    scriptFeatures = scriptFeatures.with(x);
   }
 
   /**
