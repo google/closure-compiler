ca3576a8ec64afab0fe1fe78acd7dc1cbae90562
johnlenz <johnlenz@google.com>
2026-09-09 15:03:19 -0700

    Fix RewriteSimpleDestructuringAliases to single-evaluate RHS and preserve empty patterns.
    PiperOrigin-RevId: 978773147

--- test/com/google/javascript/jscomp/InlineAndCollapsePropertiesTest.java
+++ test/com/google/javascript/jscomp/InlineAndCollapsePropertiesTest.java
@@ -3276,8 +3276,6 @@ public final class InlineAndCollapsePropertiesTest extends CompilerTestCase {
         PARTIAL_NAMESPACE_WARNING);
   }
 
-  // TODO(b/538122483): InlineAndCollapseProperties: handle destructuring patterns without creating
-  // extra variables or losing side effects
   @Test
   public void testDestructuringQualifiedNameMultipleKeysEvaluatesOnce() {
     test(
@@ -3286,8 +3284,9 @@ public final class InlineAndCollapsePropertiesTest extends CompilerTestCase {
         use(a, b);
         """,
         """
-        const a = this.validated.a;
-        const b = this.validated.b;
+        const destructuring$m1146332801$0 = this.validated;
+        const a = destructuring$m1146332801$0.a;
+        const b = destructuring$m1146332801$0.b;
         use(a, b);
         """);
   }
@@ -3305,16 +3304,12 @@ public final class InlineAndCollapsePropertiesTest extends CompilerTestCase {
         """);
   }
 
-  // TODO(b/538122483): InlineAndCollapseProperties: handle destructuring patterns without creating
-  // extra variables or losing side effects
   @Test
   public void testEmptyDestructuringPatternNotStripped() {
-    test("const {} = this.validated;", "");
-    test("const {} = obj;", "");
+    testSame("const {} = this.validated;");
+    testSame("const {} = obj;");
   }
 
-  // TODO(b/538122483): InlineAndCollapseProperties: handle destructuring patterns without creating
-  // extra variables or losing side effects
   @Test
   public void testDestructuringQualifiedNameWithCollapsibleNamespace() {
     test(
@@ -3331,6 +3326,7 @@ public final class InlineAndCollapsePropertiesTest extends CompilerTestCase {
         """
         var ns$sub$a = 1;
         var ns$sub$b = 2;
+        const destructuring$m1146332801$0 = null;
         const a = null;
         const b = null;
         use(ns$sub$a, ns$sub$b);
